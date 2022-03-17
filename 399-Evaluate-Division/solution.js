684e22d5cc3af123f74c324e05a1e4e2a5cacc52
dinesh_kumar1 <dineshkumar13506@gmail.com>
2022-03-17 12:54:00 +0530

    hustle is real

---
+++ 399-Evaluate-Division/solution.js
@@ -0,0 +1,70 @@
+/**
+ * @param {string[][]} equations
+ * @param {number[]} values
+ * @param {string[][]} queries
+ * @return {number[]}
+ */
+ var calcEquation = function(equations, values, queries) {
+    try {
+    let adjacencyMap = {}
+    let length = equations.length
+    for(let i = 0; i<length;i++) {
+        let equation = equations[i]
+        let dividend = equation[0]
+        let divisor = equation[1]
+        let value = values[i]
+        if(!adjacencyMap[dividend]) {
+            adjacencyMap[dividend] = {}
+        }
+        if(!adjacencyMap[divisor]) {
+            adjacencyMap[divisor] = {}
+        }
+        adjacencyMap[dividend][divisor] = value
+        adjacencyMap[divisor][dividend] = 1 / value
+    }
+    
+    //console.log(adjacencyMap)
+    let result = new Array(queries.length)
+    for(let i = 0; i < queries.length; i++) {
+        let query = queries[i]
+        let dividend = query[0]
+        let divisor = query[1]
+        
+        if(!adjacencyMap[dividend] || !adjacencyMap[divisor]) {
+            result[i] = -1.0
+        } else if(dividend===divisor) {
+            result[i] = 1.0
+        } else {
+            let visited = {}
+            let product = 1
+            result[i] = dfsutil(dividend,divisor,product,visited,adjacencyMap)
+        }
+    }
+    
+    return result
+    } catch(e) {
+        console.error(e)
+    }
+    
+};
+
+let dfsutil = function(start,end,product,visited,adjacencyMap) {
+        let result = -1.0
+        visited[start] = true
+        let neighbours = adjacencyMap[start]
+        if(neighbours[end]) {
+            result = product * neighbours[end]
+        } else {
+            let keys = Object.keys(neighbours)
+            for(let neighbour of keys) {
+                if(!visited[neighbour]) {
+                    result = dfsutil(neighbour,end,product*neighbours[neighbour],visited,adjacencyMap)
+                    if(result!= -1) {
+                        break
+                    }
+                }
+            }
+        }
+        //visited[start] = false
+        return result
+}
