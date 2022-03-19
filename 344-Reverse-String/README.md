949e4a1b7ae0fb646977d622fa1db6352ea9461f
dinesh_kumar1 <dineshkumar13506@gmail.com>
2022-03-19 19:35:56 +0530

    hustle is real

---
+++ 344-Reverse-String/README.md
@@ -0,0 +1,21 @@
+### 344. Reverse String
+[344. Reverse String](https://leetcode.com/problems/reverse-string/)
+
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+```
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+```
+
+```
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+```
+
+Constraints:
+
+1. 1 <= s.length <= 105
+2. s[i] is a printable ascii character.
---
+++ 344-Reverse-String/solution.js
@@ -0,0 +1,44 @@
+// solution 1 using recursion
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+ var reverseString1 = function(s) {
+    
+    let reverse = function(i,j) {
+        if(i > j) {
+            return
+        }
+        if(i === j) {
+            return 
+        }
+        
+        let temp = s[i]
+        s[i] = s[j]
+        s[j] = temp
+        reverse(i+1,j-1)
+    }
+    let i = 0
+    let j = s.length - 1
+    reverse(i,j)
+    return s
+};
+
+
+//without recursion
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+ var reverseString = function(s) {
+    let i = 0
+    let j = s.length - 1
+    while(i < j) {
+        [s[i],s[j]] = [s[j],s[i]]
+        i++
+        j--
+    }
+    return s
+};
