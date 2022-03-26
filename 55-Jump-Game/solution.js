7a88e5aeaef0eb91f3aaa03de4a458122b56ad18
dinesh_kumar1 <dineshkumar13506@gmail.com>
2022-03-26 09:52:05 +0530

    hustle is real

---
+++ 55-Jump-Game/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ var canJump = function(nums) {
+    let lastPos = nums.length - 1
+    for(let i = nums.length-1; i>=0; i--) {
+        if(i + nums[i] >= lastPos) {
+            lastPos = i
+        }
+    }
+    
+    return lastPos === 0 ? true : false
+};
