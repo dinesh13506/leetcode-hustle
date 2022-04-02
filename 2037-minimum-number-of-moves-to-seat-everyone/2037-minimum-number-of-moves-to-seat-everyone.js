7d6c9e354e63e44a91a58369935f1fe90993da35
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-02 22:45:28 +0530

    Time: 111 ms (31.05%), Space: 44 MB (83.47%) - LeetHub

---
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} seats
+ * @param {number[]} students
+ * @return {number}
+ */
+var minMovesToSeat = function(seats, students) {
+    
+    seats.sort(function(a,b) { return a - b})
+    students.sort(function(a,b) { return a - b})
+    let moves = 0
+    for(let i = 0; i < seats.length; i++) {
+        moves = moves + Math.abs(seats[i]- students[i])
+    }
+    return moves
+};
