fcfd534d8d049c73cb31da497e2b87c32b9b328a
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-02 23:28:39 +0530

    Time: 90 ms (45.16%), Space: 42.1 MB (85.01%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canMakeArithmeticProgression = function(arr) {
+    arr.sort(function(a,b) { return a - b})
+    let isArithmatic = function(nums) {
+        if(nums.length<=1) return false
+        let diff = nums[1] - nums[0]
+        for(let i = 2; i < nums.length; i++) {
+            if(nums[i] - nums[i-1] != diff) return false
+        }
+        return true
+    }
+    return isArithmatic(arr)
+};
