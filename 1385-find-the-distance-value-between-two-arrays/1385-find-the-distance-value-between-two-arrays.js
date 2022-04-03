5ac1c4ddc455d8704078fdedd62927878fb78bfd
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-03 14:45:12 +0530

    Time: 60 ms (97.87%), Space: 44.8 MB (20.21%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.js
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.js
@@ -5,17 +5,35 @@
  * @return {number}
  */
 var findTheDistanceValue = function(arr1, arr2, d) {
-    
     let ans = 0
-    for(let num1 of arr1) {
-        let c = 1
-        for( let num2 of arr2) {
-            if(Math.abs(num1-num2) <= d) {
-                c = 0
-                break
+    let binarysearch = function(target,start,end) {
+        while(start<= end) {
+            let mid = parseInt((start+end)/2)
+            if(arr2[mid] == target) {
+                return 0
+            } else if(target < arr2[mid]) {
+                end = mid - 1
+            } else {
+                start = mid + 1
             }
         }
-        ans = ans + c
+        let diff1 = Infinity
+        if(start < arr2.length) {
+             //console.log(arr2[start])
+             diff1 = Math.abs(arr2[start]-target)
+        }
+        let diff2 = Infinity
+        if(end >= 0) {
+            //console.log(arr2[end])
+            diff2 = Math.abs(arr2[end]-target)
+        }
+        return Math.min(diff1,diff2)
+    }
+    arr2.sort(function(a,b) { return a - b})
+    for(let num of arr1) {
+        if(binarysearch(num,0,arr2.length-1) > d) {
+            ans++
+        }
     }
     return ans
 };
