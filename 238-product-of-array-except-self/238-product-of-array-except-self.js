daf67a6ef1c3281ab55f53cff33418597d2d3544
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-04 16:45:21 +0530

    Time: 179 ms (23.93%), Space: 51.4 MB (99.35%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    
+    let product = 1, length = nums.length,answer = new Array(length), c = 0
+    for(let num of nums) {
+        product *= (num != 0 ? num : 1)
+        if(num ==0) c++
+    }
+    for(let i = 0; i < length; i++) {
+        if(nums[i] == 0 && c == 1) {
+            answer[i] = product
+        } else if(nums[i] == 0 && c >= 1) {
+            answer[i] = 0
+        } else if(nums[i] != 0 && c >= 1) {
+            answer[i] = 0
+        } else {
+            answer[i] = product/ nums[i]
+        }
+    }
+    return answer
+};
