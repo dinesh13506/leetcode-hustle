48cdbcbb23e47a20ac01295ca76c858c56492e94
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-08 12:26:20 +0530

    Time: 63 ms (93.25%), Space: 41.9 MB (93.98%) - LeetHub

--- 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.js
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.js
@@ -3,17 +3,10 @@
  * @return {number}
  */
 var countEven = function(num) {
-    let count = 0
-    for(let i = 1; i <= num; i++) {
-        let x = i
-        let sum = 0
-        while(x) {
-            sum += (x%10)
-            x = parseInt(x/10)
-        }
-        if( (sum&1) == 0) {
-            count++
-        }
+    let orig = num, sum = 0
+    while(num) {
+        sum += (num%10)
+        num = parseInt(num/10)
     }
-    return count
+    return (sum&1) == 0 ?  parseInt(orig / 2) : parseInt((orig-1)/2)
 };
