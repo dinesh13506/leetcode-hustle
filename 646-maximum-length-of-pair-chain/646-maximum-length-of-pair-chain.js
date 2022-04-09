baead30ccd5f5d9d4a5243f84800c3cd4d97a2fe
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-09 14:07:24 +0530

    Time: 72 ms (98.61%), Space: 45.7 MB (84.72%) - LeetHub

--- 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.js
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.js
@@ -3,19 +3,16 @@
  * @return {number}
  */
 var findLongestChain = function(pairs) {
-    
-    pairs.sort(function(p1,p2) { return p1[0] - p2[0]})
-    let lis = new Array(pairs.length)
-    lis[0] = 1
-    let ans = 1
-    for(let i =1; i < pairs.length; i++) {
-        lis[i] = 1
-        for(j = 0; j < i; j++) {
-            if(pairs[j][1] < pairs[i][0]) {
-                lis[i] = Math.max(lis[i],lis[j] + 1)
-            }
+    pairs.sort(function(a,b) { return a[1] - b[1]})
+    let c = 1
+    let prevEnd = pairs[0][1]
+    for(let i = 1; i < pairs.length; i++) {
+        let start = pairs[i][0]
+        let end =  pairs[i][1]
+        if(start > prevEnd) {
+            c++
+            prevEnd = end
         }
-        ans = Math.max(lis[i],ans)
     }
-    return ans
+    return c
 };
