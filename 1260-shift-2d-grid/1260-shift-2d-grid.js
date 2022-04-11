c6edd36e9de18964ead13fe600cc19f84a0e2e48
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-11 08:56:13 +0530

    Time: 182 ms (9.09%), Space: 46.9 MB (93.51%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.js
+++ 1260-shift-2d-grid/1260-shift-2d-grid.js
@@ -5,25 +5,16 @@
  */
 var shiftGrid = function(grid, k) {
     
-    let m = grid.length, n = grid[0].length
-    while(k > 0) {
-        let newGrid = new Array(m)
+    const m = grid.length, n = grid[0].length
+    while(k>0) {
+        let prev = grid[m-1][n-1]
         for(let i = 0; i < m; i++) {
-            newGrid[i] = new Array(n)
-        }
-        //case1 
-        for(let i = 0; i < m; i++) {
-            for(let j = 0; j < n-1; j++) {
-                newGrid[i][j+1] = grid[i][j]
+            for(let j = 0; j < n ; j++) {
+                let temp = grid[i][j]
+                grid[i][j] = prev
+                prev = temp
             }
         }
-        //case2 
-        for(let i = 0; i < m-1; i++) {
-            newGrid[i+1][0] = grid[i][n-1]
-        }
-        //case3 
-        newGrid[0][0] = grid[m-1][n-1]
-        grid = newGrid 
         k--
     }
     return grid
