c305b600150eff19ce64ac5502b34f635527700e
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-11 09:11:14 +0530

    Time: 140 ms (38.96%), Space: 47.6 MB (62.34%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.js
+++ 1260-shift-2d-grid/1260-shift-2d-grid.js
@@ -6,16 +6,17 @@
 var shiftGrid = function(grid, k) {
     
     const m = grid.length, n = grid[0].length
-    while(k>0) {
-        let prev = grid[m-1][n-1]
-        for(let i = 0; i < m; i++) {
-            for(let j = 0; j < n ; j++) {
-                let temp = grid[i][j]
-                grid[i][j] = prev
-                prev = temp
-            }
-        }
-        k--
+    let ans = new Array(m)
+    for(let i = 0; i < m; i++) {
+        ans[i] = new Array(n)
     }
-    return grid
+    for(let i =0; i < m; i++) {
+        for(let j = 0; j < n; j++) {
+            let c = (j + k) % n
+            let iterations = parseInt((j+k) / n)
+            let r = (i+iterations) % m
+            ans[r][c] = grid[i][j]
+        }
+    }
+    return ans
 };
