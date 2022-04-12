c76b51d1311ed345274395740cc5db6c2d3582b6
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-12 07:46:08 +0530

    Time: 68 ms (79.74%), Space: 41.6 MB (98.68%) - LeetHub

--- 289-game-of-life/289-game-of-life.js
+++ 289-game-of-life/289-game-of-life.js
@@ -4,39 +4,38 @@
  */
 var gameOfLife = function(board) {
     let m = board.length, n = board[0].length
-    let future = new Array(m)
-    for(let i = 0; i < m; i++) {
-        future[i] = new Array(n)
-        future[i].fill(0)
-    }
     for(let i = 0; i < m; i++) {
         for(let j = 0; j < n; j++) {
             //calculate neighbours of [i,j]
             let aliveNeighbours = 0
             for(let x = -1; x<=1; x++) {
                 for(let y = -1; y<=1; y++) {
-                    if( (i+x >=0 && i+x < m) && (j+y>=0 && j+y < n)) {
-                        aliveNeighbours = aliveNeighbours + board[i+x][j+y]
+                    if( (i+x >=0 && i+x < m) && (j+y>=0 && j+y < n) && Math.abs(board[i+x][j+y]) === 1) {
+                        aliveNeighbours = aliveNeighbours + 1
                     }
                 }
             }
             aliveNeighbours = aliveNeighbours - board[i][j]
             if( (board[i][j] == 1) && (aliveNeighbours < 2)) {
-                future[i][j] = 0
+                board[i][j] = -1
             }
             else if( (board[i][j] == 1) && (aliveNeighbours > 3)) {
-                future[i][j] = 0
+                board[i][j] = -1
             }
             else if( (board[i][j] == 0) && (aliveNeighbours == 3)) {
-                future[i][j] = 1
+                board[i][j] = 2
             } else {
-                future[i][j] = board[i][j]
+                board[i][j] = board[i][j]
             }
         }
     }
     for(let i = 0; i< m; i++) {
         for(let j = 0; j < n; j++) {
-            board[i][j] = future[i][j]
+            if(board[i][j] == -1) {
+                board[i][j] = 0
+            } else if(board[i][j] == 2) {
+                board[i][j] = 1
+            }
         }
     }
 };
