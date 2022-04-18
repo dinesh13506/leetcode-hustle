57e356e21915f2b34851a65f13bbbe234eecdcf2
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-18 23:08:24 +0530

    Time: 68 ms (94.04%), Space: 44.1 MB (92.73%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.js
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.js
@@ -5,24 +5,22 @@
 var removeCoveredIntervals = function(intervals) {
     
     intervals.sort(function(a,b) {
-        if(a[0] - b[0] == 0) {
-            return b[1] - b[1]
+        //if start time are same, keep end time before
+        if(a[0] == b[0]) {
+            return b[1] - a[1]
         }
-        return b[0] - a[0]
+        return a[0] - b[0]
     })
-    //console.log(intervals)
-    let covered = 0
-    for(let i = 0; i < intervals.length; i++) {
-        let int1 = intervals[i]
-        for(let j = i + 1; j < intervals.length; j++) {
-            let int2 = intervals[j]
-            if( (int2[0] <= int1[0]) &&  (int1[1] <= int2[1]) ) {
-                //console.log(int1,int2)
-                covered++
-                break
-            }
+    
+    let prev_end = 0
+    let curr_end = 0
+    let ans = 0
+    for(let int of intervals) {
+        curr_end = int[1]
+        if(curr_end > prev_end) {
+            ans++
+            prev_end = curr_end
         }
     }
-    //console.log(covered)
-    return intervals.length - covered
+    return ans
 };
