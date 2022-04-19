a9cdd791f59fab4c96dead853f897a6a0eddd497
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-19 07:15:02 +0530

    Time: 132 ms (92.06%), Space: 52.4 MB (50.26%) - LeetHub

---
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var recoverTree = function(root) {
+    
+    let stack = [], prev = new TreeNode(-Infinity), swaps = []
+    while(root || stack.length) {
+        while(root) {
+            stack.push(root)
+            root = root.left
+        }
+        root = stack.pop()
+        //console.log(root.val)
+        if(root.val > prev.val) {
+        } else {
+            if(swaps.length >= 2) {
+                swaps.pop()
+                swaps.push(root)
+            } else {
+                swaps.push(prev)
+                swaps.push(root)
+            }
+        }
+        prev = root
+        root = root.right
+    }
+    //console.log(swaps.length)
+    let node1 = swaps[0], node2 = swaps[1]
+    //console.log(node1.val,node2.val)
+    let t = node1.val
+    node1.val = node2.val
+    node2.val = t
+    
+};
