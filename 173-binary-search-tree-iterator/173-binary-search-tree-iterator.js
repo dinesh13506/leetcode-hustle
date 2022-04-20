d2aaa491ad97c21a9e8e025a7197ee4115e6f09f
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-20 07:13:02 +0530

    Time: 146 ms (72.51%), Space: 55.8 MB (41.16%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.js
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.js
@@ -10,34 +10,35 @@
  * @param {TreeNode} root
  */
 var BSTIterator = function(root) {
-    this.pointer = 0
-    this.inorder = []
-    let stack = [], p = root
-    while(p || stack.length) {
-        while(p) {
-            stack.push(p)
-            p = p.left
-        }
-        p = stack.pop()
-        this.inorder.push(p.val)
-        p = p.right
-    }
+    this.stack = []
+    this.nums = []
+    this.pointer = -1
+    this.last = root
 };
 
 /**
  * @return {number}
  */
 BSTIterator.prototype.next = function() {
-    let value = this.inorder[this.pointer]
     this.pointer++
-    return value
+    if(this.pointer == this.nums.length) {
+        let p = this.last
+        while(p) {
+            this.stack.push(p)
+            p = p.left
+        }
+        p = this.stack.pop()
+        this.nums.push(p.val)
+        this.last = p.right
+    }
+    return this.nums[this.pointer]
 };
 
 /**
  * @return {boolean}
  */
 BSTIterator.prototype.hasNext = function() {
-    return this.pointer < this.inorder.length ? true : false
+    return this.last || this.stack.length > 0 || this.pointer + 1 < this.nums.length
 };
 
 /** 
