6a1e412ea85f59c2a4364e655cde1bb3351d775f
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-20 21:40:18 +0530

    Time: 266 ms (49.14%), Space: 80 MB (7.93%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.js
@@ -0,0 +1,67 @@
+
+var TrieNode = function() {
+    this.children = new Array(26)
+    this.children.fill(null)
+    this.isEndOfWord = false
+    
+}
+
+var Trie = function() {
+    this.root = new TrieNode()
+};
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function(word) {
+    let curr = this.root
+    for(let ch of word) {
+        let i = ch.charCodeAt() - 'a'.charCodeAt()
+        if(curr.children[i] === null) {
+            curr.children[i] = new TrieNode()
+        }
+        curr = curr.children[i]
+    }
+    curr.isEndOfWord = true
+};
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function(word) {
+    let curr = this.root
+    for(let ch of word) {
+        let i = ch.charCodeAt() - 'a'.charCodeAt()
+        if(curr.children[i] === null) {
+            return false
+        }
+        curr = curr.children[i]
+    }
+    return curr.isEndOfWord
+};
+
+/** 
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function(prefix) {
+    let curr = this.root
+    for(let ch of prefix) {
+        let i = ch.charCodeAt() - 'a'.charCodeAt()
+        if(curr.children[i] === null) {
+            return false
+        }
+        curr = curr.children[i]
+    }
+    return true
+};
+
+/** 
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
