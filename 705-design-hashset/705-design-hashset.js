ead0e139b1511a35fb191e47399a987a0494cf2c
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-21 07:40:29 +0530

    Time: 209 ms (68.44%), Space: 51 MB (92.21%) - LeetHub

--- 705-design-hashset/705-design-hashset.js
+++ 705-design-hashset/705-design-hashset.js
@@ -1,17 +1,104 @@
+let Node = function(val) {
+    this.val = val
+    this.left = null
+    this.right = null
+}
+
+let BST = function() {
+   this.root = null
+}
+
+BST.prototype.search = function(root, val) {
+    if(root == null) {
+        return root
+    }
+    if(val < root.val) {
+        return this.search(root.left,val)
+    } else if(val === root.val) {
+        return root
+    } else {
+        return this.search(root.right,val)
+    }
+}
+
+BST.prototype.insert = function(root,val) {
+    if(root == null) {
+        return new Node(val)
+    }
+    if(val < root.val) {
+        root.left = this.insert(root.left,val)
+    } else if(val === root.val) {
+        return root
+    } else if( val > root.val) {
+        root.right = this.insert(root.right,val)
+    }
+    return root
+}
+
+BST.prototype.predecessor = function(root) {
+    root = root.left
+    while(root.right) {
+        root = root.right
+    }
+    return root
+}
+
+BST.prototype.successor = function(root) {
+    root = root.right
+    while(root.left) {
+        root = root.left
+    }
+    return root
+}
+
+BST.prototype.delete = function(root,val) {
+    if(root == null) {
+        return null
+    }
+    if(val < root.val) {
+        root.left = this.delete(root.left,val)
+    } else if( val > root.val) {
+        root.right = this.delete(root.right,val)
+    } else {
+        if(root.left == null && root.right == null) {
+            root = null
+        } else if(root.right != null) {
+            let successor = this.successor(root)
+            root.val = successor.val
+            root.right = this.delete(root.right, successor.val)
+        }
+        else {
+            let predecessor = this.predecessor(root)
+            root.val = predecessor.val
+            root.left = this.delete(root.left, predecessor.val)
+        } 
+    }
+    return root
+}
+
+
 
 var MyHashSet = function() {
-    let maxKeySize = Math.pow(10,6) + 1
-    this.set = new Array(maxKeySize)
-    this.set.fill(false)
-    
+    this.keySize = 997
+    this.buckets = new Array(997)
+    for(let i = 0; i < this.keySize; i++) {
+        this.buckets[i] = new BST()
+    }
 };
+
+MyHashSet.prototype.hash = function(key) {
+    return key % this.keySize
+}
 
 /** 
  * @param {number} key
  * @return {void}
  */
 MyHashSet.prototype.add = function(key) {
-    this.set[key] = true
+    let index = this.hash(key)
+    let bst = this.buckets[index]
+    let root = bst.root
+    bst.root = bst.insert(root,key)
 };
 
 /** 
@@ -19,9 +106,10 @@ MyHashSet.prototype.add = function(key) {
  * @return {void}
  */
 MyHashSet.prototype.remove = function(key) {
-    if(this.contains(key)) {
-        this.set[key] = false
-    }
+    let index = this.hash(key)
+    let bst = this.buckets[index]
+    let root = bst.root
+    bst.root = bst.delete(root,key)
 };
 
 /** 
@@ -29,7 +117,10 @@ MyHashSet.prototype.remove = function(key) {
  * @return {boolean}
  */
 MyHashSet.prototype.contains = function(key) {
-    return this.set[key] ? true : false
+    let index = this.hash(key)
+    let bst = this.buckets[index]
+    let root = bst.root
+    return bst.search(root,key) ? true : false
 };
 
 /** 
