f0009282c98d2e8ccf1f3ffdc1f335d1392ee937
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-22 22:11:55 +0530

    Time: 95 ms (50.36%), Space: 44.3 MB (85.91%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.js
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.js
@@ -11,18 +11,29 @@
  * @return {void} Do not return anything, modify root in-place instead.
  */
 var flatten = function(root) {
-    let prev = null
-    let flatenTree = function(curr) {
-        if(curr) {
-            flatenTree(curr.right)
-            flatenTree(curr.left)
-            curr.right = prev
-            curr.left = null
-            prev = curr
-            
-        } else {
-            return
+    
+    let ischild = function(root) {
+        if(root && root.left == null && root.right == null) {
+            return true
         }
+        return false
+    }
+    let flatenTree = function(root) {
+        if(root == null) {
+            return root
+        }
+        if(ischild(root)) {
+            return root
+        }
+        let leftTail = flatenTree(root.left)
+        let rightTail = flatenTree(root.right)
+        
+        if(leftTail) {
+            leftTail.right = root.right
+            root.right = root.left
+            root.left = null
+        }
+        return rightTail ? rightTail : leftTail
     }
     flatenTree(root)
 };
