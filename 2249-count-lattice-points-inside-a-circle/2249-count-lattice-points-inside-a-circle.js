2eb3ff9c49996234ec20d8285e359d7d918034ef
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-24 10:13:50 +0530

    Time: 274 ms (100.00%), Space: 42.2 MB (100.00%) - LeetHub

--- 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.js
+++ 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.js
@@ -8,23 +8,24 @@ var countLatticePoints = function(circles) {
     let gethash = function(x,y) {
         return `${x}_${y}`
     }
-    let set = new Set()
-    for(let c = 0; c < m; c++) {
-        let a = circles[c][0]
-        let b = circles[c][1]
-        let r = circles[c][2]
-        for(let x = a - r; x <= a + r; x++) {
-            for(let y = b -r; y<= b + r; y++) {
+    let count = 0
+    let max_r = 100, max_a = 100, max_b = 100
+    for(let x = max_a - max_r; x <= max_a + max_r; x++) {
+        for(let y = max_b - max_r; y<= max_b + max_r; y++) {
+            for(let c = 0; c < m; c++) {
+                let a = circles[c][0]
+                let b = circles[c][1]
+                let r = circles[c][2]
                 let powx = Math.pow(x-a, 2)
                 let powy = Math.pow(y-b, 2)
                 let powr = Math.pow(r,2)
                 if(powx + powy <= powr) {
-                    let hash = gethash(x,y)
-                    if(set.has(hash) == false ) set.add(hash)
-                    
+                    count = count + 1
+                    break
                 }
-            }
+            }   
         }
     }
-    return set.size
+    
+    return count
 };
