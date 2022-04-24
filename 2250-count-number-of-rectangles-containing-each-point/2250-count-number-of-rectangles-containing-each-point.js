ef83febe5067d4854eb16b59a7967d0f9f08bf60
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-24 11:53:21 +0530

    Time: 1009 ms (100.00%), Space: 87.9 MB (100.00%) - LeetHub

--- 2250-count-number-of-rectangles-containing-each-point/2250-count-number-of-rectangles-containing-each-point.js
+++ 2250-count-number-of-rectangles-containing-each-point/2250-count-number-of-rectangles-containing-each-point.js
@@ -5,24 +5,26 @@
  */
 var countRectangles = function(rectangles, points) {
     
-    let dp = new Array(101) // h >= 1 and h <= 100
     let result = []
-    for(let h = 0;  h<=100; h++) {
+    let dp = new Array(101) // h >= 1 and h <= 100
+    for(let h = 0; h<=100; h++) {
         dp[h] = []
     }
     //pre calculate
     for(let rect of rectangles) {
         let l = rect[0]
         let h = rect[1]
-        dp[h].push(l)
+        dp[h].push(l) //stores lengths with height h
     }
     //console.log(dp)
+    //sort all lengths
     for(let h = 0;  h<=100; h++) {
         dp[h].sort(function(a,b) {
             return a - b
         })
     }
     
+    //find floor value of len <= target
     let find = function(arr, target) {
         let start = 0, end = arr.length-1, ans = 0
         while(start <= end ){
@@ -36,12 +38,14 @@ var countRectangles = function(rectangles, points) {
         }
         return ans
     }
+    
     for(let point of points) {
         let x = point[0]
         let y = point[1]
         let ans = 0
-        for(let h = y; h<= 100; h++) {
-            ans = ans + find(dp[h],x)
+        //search all heights greater than equal to y
+        for(let h = y; h <= 100; h++) {
+            ans = ans + find(dp[h],x) // if height is there and lengths are present, then return lengths >= x
         }
         result.push(ans)
     }
