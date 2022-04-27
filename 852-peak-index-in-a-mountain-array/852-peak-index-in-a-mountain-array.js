4fc287ae22672c240374126a175842ddc128ff64
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-27 21:25:28 +0530

    Time: 103 ms (14.00%), Space: 43.1 MB (9.09%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
@@ -3,28 +3,15 @@
  * @return {number}
  */
 var peakIndexInMountainArray = function(arr) {
-    
-    
-    let length = arr.length
-    let maxfromleft = new Array(length)
-    maxfromleft.fill(-Infinity)
-    let maxfromright = new Array(length)
-    maxfromright.fill(-Infinity)
-    let count = new Map()
-    
-    maxfromleft[0] = arr[0]
-    for(let i = 1; i < arr.length; i++) {
-        maxfromleft[i] = Math.max(maxfromleft[i-1],arr[i])
-    }
-    maxfromright[length-1] = arr[length-1]
-    for(let i = length-2; i>=0; i--) {
-        maxfromright[i] = Math.max(maxfromright[i+1],arr[i])
-    }
-    
-    //console.log(maxfromleft,maxfromright)
-    for(let i = 1; i < length-1; i++) {
-        if(maxfromleft[i] == maxfromright[i]) {
-            return i
+    //[true,true,true,false,false,false]
+    let left = 0 , right = arr.length - 1
+    while(left < right) {
+        let mid = parseInt((left+right)/2)
+        if(arr[mid] > arr[mid+1]) {
+            right = mid
+        } else {
+            left = mid + 1
         }
     }
+    return right
 };
