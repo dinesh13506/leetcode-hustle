36821b3e549d2a7d4057c5549707374125f5834d
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-28 12:59:53 +0530

    Time: 70 ms (64.84%), Space: 42.7 MB (10.33%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    
+    let start = 0, end = nums.length-1, ans = 0
+    while(start <= end) {
+        let mid = parseInt((start+end)/2)
+        if(nums[mid] === target) {
+            return mid
+        }else if(target < nums[mid]) {
+            end = mid - 1
+        } else {
+            start = mid + 1
+        }
+    }
+    //console.log(start,end)
+    if(start < 0 ) {
+        return 0
+    } 
+    if(start >= nums.length) {
+        return nums.length
+    }
+    return start
+};
