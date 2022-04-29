0042e984073e8155e837e95405100a69368ba0bb
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-29 10:01:44 +0530

    Time: 102 ms (23.97%), Space: 44 MB (53.50%) - LeetHub

--- 78-subsets/78-subsets.js
+++ 78-subsets/78-subsets.js
@@ -3,17 +3,20 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
-    
     let subsets = []
-    let backtrack = function(index,current) {
-        subsets.push(current.slice())
-        for(let i = index; i < nums.length; i++) {
-            current.push(nums[i])
-            backtrack(i+1,current)
-            current.pop()
+    
+    let backtrack = function(i,current) {
+        if(i>=nums.length) {
+            subsets.push(current.slice())
+            return
         }
+        current.push(nums[i])
+        backtrack(i+1,current)
+        current.pop()
+        backtrack(i+1,current)
     }
-    let current = []
+    
+    let current= []
     backtrack(0,current)
     return subsets
 };
