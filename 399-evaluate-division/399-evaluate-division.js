9d71913caadb6e88f4f451bca0299c68f6fdb4aa
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-04-30 08:32:04 +0530

    Time: 84 ms (42.41%), Space: 42.1 MB (63.84%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.js
+++ 399-evaluate-division/399-evaluate-division.js
@@ -5,66 +5,57 @@
  * @return {number[]}
  */
 var calcEquation = function(equations, values, queries) {
-    try {
-    let adjacencyMap = {}
-    let length = equations.length
-    for(let i = 0; i<length;i++) {
-        let equation = equations[i]
-        let dividend = equation[0]
-        let divisor = equation[1]
-        let value = values[i]
-        if(!adjacencyMap[dividend]) {
-            adjacencyMap[dividend] = {}
+    
+    //step1. build a graph
+    let adjacencyList = new Map()
+    for(let i = 0 ; i < equations.length; i++) {
+        let src = equations[i][0], dest = equations[i][1]
+        if(adjacencyList.has(src) === false) {
+            adjacencyList.set(src, new Map())
         }
-        if(!adjacencyMap[divisor]) {
-            adjacencyMap[divisor] = {}
+        if(adjacencyList.has(dest) === false) {
+            adjacencyList.set(dest, new Map())
         }
-        adjacencyMap[dividend][divisor] = value
-        adjacencyMap[divisor][dividend] = 1 / value
+        adjacencyList.get(src).set(dest,values[i])
+        adjacencyList.get(dest).set(src,1/values[i])
     }
     
-    //console.log(adjacencyMap)
+    //console.log(adjacencyList)
+    
     let result = new Array(queries.length)
     for(let i = 0; i < queries.length; i++) {
-        let query = queries[i]
-        let dividend = query[0]
-        let divisor = query[1]
-        
-        if(!adjacencyMap[dividend] || !adjacencyMap[divisor]) {
+        let src = queries[i][0]
+        let dest = queries[i][1]
+        if(adjacencyList.has(src) === false || adjacencyList.has(dest) === false) {
             result[i] = -1.0
-        } else if(dividend===divisor) {
+        } else if(src == dest) {
             result[i] = 1.0
         } else {
-            let visited = {}
-            let product = 1
-            result[i] = dfsutil(dividend,divisor,product,visited,adjacencyMap)
-        }
-    }
-    
-    return result
-    } catch(e) {
-        console.error(e)
-    }
-    
-};
-
-let dfsutil = function(start,end,product,visited,adjacencyMap) {
-        let result = -1.0
-        visited[start] = true
-        let neighbours = adjacencyMap[start]
-        if(neighbours[end]) {
-            result = product * neighbours[end]
-        } else {
-            let keys = Object.keys(neighbours)
-            for(let neighbour of keys) {
-                if(!visited[neighbour]) {
-                    result = dfsutil(neighbour,end,product*neighbours[neighbour],visited,adjacencyMap)
-                    if(result!= -1) {
-                        break
+            //use dfs to find path between src and dest
+            let visited = new Set(), product = 1
+            let dfsUtil = function(curr,target,product) {
+                visited.add(curr)
+                let ans = -1.0
+                let neighbours = adjacencyList.get(curr)
+                if(neighbours.has(target)) {
+                    ans = product * neighbours.get(target)
+                } else {
+                    for(let neigh of neighbours.keys()) {
+                        if(visited.has(neigh)) {
+                            continue
+                        }
+                        ans = dfsUtil(neigh, target,product * neighbours.get(neigh))
+                        if(ans != -1.0) {
+                            break
+                        }
                     }
                 }
+                visited.delete(curr)
+                return ans
             }
+            result[i] = dfsUtil(src,dest,product)
+            
         }
-        //visited[start] = false
-        return result
-}
+    }
+    return result
+};
