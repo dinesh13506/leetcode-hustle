185a69e8bcd754761e72dd97e81ccb36ad2e0179
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-01 10:41:33 +0530

    Time: 90 ms (39.91%), Space: 43.1 MB (33.90%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.js
+++ 844-backspace-string-compare/844-backspace-string-compare.js
@@ -4,26 +4,37 @@
  * @return {boolean}
  */
 var backspaceCompare = function(s, t) {
-    let stack1 = []
-    let stack2 = []
-    for(let i = 0; i < s.length; i++) {
-        let ch = s[i]
-        if(ch === '#') {
-            stack1.length > 0 ? stack1.pop() : true
-            continue
-        }
-        stack1.push(ch)
-    }
-    for(let i = 0; i < t.length; i++) {
-        let ch = t[i]
-        if(ch === '#') {
-            stack2.length > 0 ? stack2.pop() : true
-            continue
-        }
-        stack2.push(ch)  
-        
-    }
-    //console.log(stack1,stack2)
     
-    return stack1.join('') == stack2.join('')
+    let i = s.length - 1, j = t.length - 1, skipS = 0, skipT = 0
+    while(i >=0 || j>=0 ) {
+        while(i>=0) {
+            if(s[i] === '#') {
+                skipS++
+                i--
+            } else if(skipS > 0) {
+                skipS--
+                i--
+            } else {
+                break
+            }
+        }
+        
+        while(j>=0) {
+            if(t[j] === '#') {
+                skipT++
+                j--
+            } else if(skipT > 0) {
+                skipT--
+                j--
+            } else {
+                break
+            }
+        }
+        
+        if(i >=0 && j >=0 && s[i] != t[j]) return false
+        if((i>=0) != (j>=0)) return false
+        i--
+        j--
+    }
+    return true
 };
