413f28bfee81d257a799ab405f579377e73b8de0
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-02 09:07:06 +0530

    Time: 117 ms (45.08%), Space: 48.4 MB (5.56%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.js
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.js
@@ -4,18 +4,18 @@
  * @return {number}
  */
 var numSubarrayProductLessThanK = function(nums, k) {
-    
     let count = 0
-    for(let i = 0; i < nums.length; i++) {
-        let product = 1
-        for(let j = i; j < nums.length; j++) {
-            product = product * nums[j]
-            if(product < k) {
-                count++
-            } else {
-                break
-            }
-        }
+    if(k<1) {
+        return count
     }
-    return count
+    let length = nums.length, product = 1, left = 0
+    for(let right = 0 ; right < length; right++) {
+        product *= nums[right]
+        while(product >= k) {
+            product = parseInt(product / nums[left])
+            left++
+        }
+        count += (right - left) + 1
+    }
+    return count < 0 ? 0 : count
 };
