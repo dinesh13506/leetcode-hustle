26279b024d1574f319e7e426eb9532ec4b0e6b30
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-04 16:28:11 +0530

    Time: 102 ms (29.93%), Space: 43.5 MB (66.78%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -6,10 +6,12 @@ var minCostClimbingStairs = function(cost) {
     
     let n = cost.length
     let dp = new Array(n+1)
-    dp[0] = 0
-    dp[1] = 0
+    let prev2 = 0
+    let prev = 0
     for(let i = 2; i <=n ; i++) {
-        dp[i] = Math.min(dp[i-2] + cost[i-2] , dp[i-1] + cost[i-1])
+        let curri = Math.min(prev2 + cost[i-2] , prev + cost[i-1])
+        prev2 = prev
+        prev = curri
     }
-    return dp[n]
+    return prev
 };
