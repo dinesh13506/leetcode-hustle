75e097ae7c3c445be51d187d6c7438ddefb78088
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-08 00:11:52 +0530

    Time: 133 ms (14.68%), Space: 44.5 MB (49.41%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.js
+++ 64-minimum-path-sum/64-minimum-path-sum.js
@@ -5,29 +5,31 @@
 var minPathSum = function(grid) {
     
     let m = grid.length, n = grid[0].length
-    let memo = new Array(m)
+    let dp = new Array(m)
     for(let i = 0; i < m; i++) {
-        memo[i] = new Array(n)
-        memo[i].fill(-1)
+        dp[i] = new Array(n)
+        dp[i].fill(-1)
     }
     
-    let dp = function(i,j) {
-        if( i == 0 && j == 0) {
-            return grid[i][j]
+    for(let i = 0 ; i < m; i++) {
+        for(let j = 0; j < n; j++) {
+            if( i== 0 && j == 0) {
+                dp[i][j] = grid[i][j]
+            }
+            else {
+                let top = Infinity
+                if(i > 0 ) {
+                    top = dp[i-1][j]
+                }
+                let left = Infinity
+                if( j > 0 ) {
+                    left = dp[i][j-1]
+                }
+                
+                dp[i][j] = grid[i][j] + Math.min(top,left)
+            }
         }
-        if( i < 0 || j < 0) {
-            return Infinity
-        }
-        
-        if(memo[i][j] != -1) {
-            return memo[i][j]
-        }
-        
-        let top = dp(i-1,j)
-        let left  = dp(i, j-1)
-        memo[i][j] = grid[i][j] + Math.min(top,left)
-        return memo[i][j]
     }
     
-    return dp(m-1,n-1)
+    return dp[m-1][n-1]
 };
