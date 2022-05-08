45953cfe7a288304e97a2f410e9cde2c41abc757
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-08 18:44:35 +0530

    Time: 72 ms (78.47%), Space: 44.2 MB (33.07%) - LeetHub

--- 120-triangle/120-triangle.js
+++ 120-triangle/120-triangle.js
@@ -6,22 +6,21 @@ var minimumTotal = function(triangle) {
     
     let n = triangle.length
     let dp = new Array(n)
-    for(let i = 0; i < n; i++) {
-        dp[i] = new Array(n)
-        dp[i].fill(-1)
-    }
+    dp.fill(-1)
     
     for(let j = 0; j <=n-1; j++) {
-        dp[n-1][j] =  triangle[n-1][j]
+        dp[j] =  triangle[n-1][j]
     }
     
     for(let i = n-2; i >=0; i--) {
+        let curr = new Array(n)
         for(let j = i; j>=0; j--) {
-            let down = triangle[i][j] + dp[i+1][j]
-            let dgdown = triangle[i][j] + dp[i+1][j+1]
-            dp[i][j] = Math.min(down,dgdown)
+            let down = triangle[i][j] + dp[j]
+            let dgdown = triangle[i][j] + dp[j+1]
+            curr[j] = Math.min(down,dgdown)
         }
+        dp = curr
     }
     
-    return dp[0][0]
+    return dp[0]
 };
