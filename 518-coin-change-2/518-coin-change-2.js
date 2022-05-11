caba6e1beaa07ce53df005d9c6d664c073567253
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-11 08:45:03 +0530

    Time: 90 ms (71.55%), Space: 48 MB (52.72%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.js
+++ 518-coin-change-2/518-coin-change-2.js
@@ -7,29 +7,28 @@ var change = function(amount, coins) {
     
     let n = coins.length
     
-    let dp = new Array(n)
-    for(let i = 0; i < n; i++) {
-        dp[i] = new Array(amount+1)
-        dp[i].fill(0)
-    }
-    
+    let prev = new Array(amount+1)
+    prev.fill(0)
     for(let j = 0; j <= amount; j++) {
         if(j % coins[0] == 0) {
-            dp[0][j] = 1
+            prev[j] = 1
         } else {
-            dp[0][j] = 0
+            prev[j] = 0
         }
     }
     
-    for(let i = 1; i <n; i++) {
+    for(let i = 1; i < n; i++) {
+         let curr = new Array(amount+1)
+         curr.fill(0)
         for(let am = 0; am <= amount; am++) {
-            let nottake = dp[i-1][am]
+            let nottake = prev[am]
             let take = 0
             if(coins[i] <= am) {
-                take = dp[i][am-coins[i]]
+                take = curr[am-coins[i]]
             }
-            dp[i][am] = take + nottake
+            curr[am] = take + nottake
         }
+        prev = curr
     }
-    return dp[n-1][amount]
+    return prev[amount]
 };
