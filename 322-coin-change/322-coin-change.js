dabf5fe745e02ffd1907d8c941b311e06dfa5058
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-11 07:38:28 +0530

    Time: 119 ms (84.78%), Space: 48.2 MB (42.00%) - LeetHub

--- 322-coin-change/322-coin-change.js
+++ 322-coin-change/322-coin-change.js
@@ -6,31 +6,31 @@
 var coinChange = function(coins, amount) {
     
     let n = coins.length
-    let dp = new Array(n)
-    for(let i = 0; i < n; i++) {
-        dp[i] = new Array(amount+1)
-        dp[i].fill(0)
-    }
+    let prev = new Array(amount+1)
+    prev.fill(0)
     
     for(let am = 0; am <= amount; am++) {
         if(am % coins[0] === 0) {
-            dp[0][am] = am / coins[0]
+            prev[am] = am / coins[0]
         } else {
-            dp[0][am] = Infinity
+            prev[am] = Infinity
         }
     }
     
     for(let i = 1; i < n; i++) {
+        let curr = new Array(amount+1)
+        curr.fill(0)
         for(let am = 0; am <= amount; am++) {
-            let nottake = dp[i-1][am]
+            let nottake = prev[am]
             let take = Infinity
             if(coins[i] <= am) {
-                take = 1 + dp[i][am-coins[i]]
+                take = 1 + curr[am-coins[i]]
             }
-            dp[i][am] = Math.min(nottake, take)
+            curr[am] = Math.min(nottake, take)
         }
+        prev = curr
     }
     
-    let ans = dp[n-1][amount]
+    let ans = prev[amount]
     return ans === Infinity ? -1 : ans
 };
