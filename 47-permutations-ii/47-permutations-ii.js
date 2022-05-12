4bac2257bd71ae2b9d25093256c470f5bb45631e
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-12 06:35:49 +0530

    Time: 117 ms (51.98%), Space: 45.1 MB (67.67%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.js
+++ 47-permutations-ii/47-permutations-ii.js
@@ -6,32 +6,34 @@ var permuteUnique = function(nums) {
     
     let visited = new Map()
     let results = [], temp = []
-    let resultsMap = new Map()
+    let countMap = new Map()
     let n = nums.length
+    
+    for(let i = 0; i < n; i++) {
+        let count = countMap.get(nums[i]) || 0
+        count++
+        countMap.set(nums[i],count)
+    }
     
     let backtrack = function(temp) {
         
         if(temp.length === n) {
-            let str = temp.slice().join("*")
-            if(resultsMap.has(str) === false) {
-                resultsMap.set(str, true)
-                results.push(temp.slice())
-            }
+            results.push(temp.slice())
             return 
         }
-        
-        for(let i = 0; i < n; i++) {
-            if(visited.has(i) === false) {
-                visited.set(i, true)
-                temp.push(nums[i])
-                backtrack(temp)
-                visited.delete(i)
-                temp.pop()
+        for(let num of countMap.keys()) {
+            if(countMap.get(num) === 0) {
+                continue
             }
+            let count = countMap.get(num)
+            temp.push(num)
+            countMap.set(num,count-1)
+            backtrack(temp)
+            temp.pop()
+            countMap.set(num,count)
         }
     }
     
     backtrack(temp)
     return results
-    
 };
