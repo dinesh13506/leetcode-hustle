bf9104f1cda0a28f415f4f4e13926b43b02b0a09
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-12 23:49:54 +0530

    Time: 124 ms (65.66%), Space: 52.4 MB (87.30%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -7,6 +7,33 @@ var longestCommonSubsequence = function(text1, text2) {
     
     
     let m = text1.length, n = text2.length
+    let dp = new Array(m+1)
+    for(let i = 0 ; i <= m; i++) {
+        dp[i] = new Array(n+1)
+        dp[i].fill(0)
+    }
+    
+
+    
+    //console.log(dp)
+    
+    for(let i = 1; i <= m; i++) {
+        for(let j = 1; j <= n; j++) {
+            if(text1[i-1] === text2[j-1]) {
+                dp[i][j] = 1 + dp[i-1][j-1]
+            } else {
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])
+            }
+        }
+    }
+    
+    return dp[m][n]
+}
+
+var longestCommonSubsequenceMemo = function(text1, text2) {
+    
+    
+    let m = text1.length, n = text2.length
     let memo = new Array(m)
     for(let i = 0 ; i < m; i++) {
         memo[i] = new Array(n)
