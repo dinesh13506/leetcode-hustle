47e3e5a84be6fac569b640907ec2dfdb256fdb6f
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-13 08:42:18 +0530

    Time: 385 ms (31.78%), Space: 47.8 MB (95.34%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.js
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.js
@@ -7,22 +7,22 @@ var longestPalindromeSubseq = function(s) {
     let rs = s.split('').reverse().join()
     
     let n = s.length, m = rs.length
-    let dp = new Array(n + 1)
-    for(let i = 0; i <= n; i++) {
-        dp[i] = new Array(m + 1)
-        dp[i].fill(0)
-    }
+    let prev = new Array(m + 1)
+    prev.fill(0)
     
     for(let i = 1; i <= n; i++) {
+        let curr = new Array(m + 1)
+        curr.fill(0)
         for(let j = 1; j <=m; j++) {
             let ch1 = s[i-1], ch2 = rs[j-1]
             if(ch1 == ch2) {
-                dp[i][j] = 1 + dp[i-1][j-1]
+                curr[j] = 1 + prev[j-1]
             } else {
-                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])
+                curr[j] = Math.max(prev[j],curr[j-1])
             }
         }
+        prev = curr
     }
     
-    return dp[n][m]
+    return prev[m]
 };
