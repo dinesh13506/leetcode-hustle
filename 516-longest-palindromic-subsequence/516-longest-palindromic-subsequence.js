ff1d10c49ffbaab1bb3b0c6c7d5196f17b100ed2
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-13 08:40:59 +0530

    Time: 618 ms (19.92%), Space: 127.9 MB (17.80%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.js
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.js
@@ -7,28 +7,22 @@ var longestPalindromeSubseq = function(s) {
     let rs = s.split('').reverse().join()
     
     let n = s.length, m = rs.length
-    let memo = new Array(n)
-    for(let i = 0; i < n; i++) {
-        memo[i] = new Array(m)
-        memo[i].fill(-1)
+    let dp = new Array(n + 1)
+    for(let i = 0; i <= n; i++) {
+        dp[i] = new Array(m + 1)
+        dp[i].fill(0)
     }
     
-    let dp = function(index1, index2) {
-        if(index1 < 0 || index2 < 0) {
-            return 0
-        }
-        if(memo[index1][index2] != -1) {
-            return memo[index1][index2]
-        }
-        if(s[index1] === rs[index2]) {
-            memo[index1][index2] =  1 + dp(index1 - 1, index2 - 1)
-            return memo[index1][index2]
-            
-        } else {
-            memo[index1][index2] =  Math.max(dp(index1-1, index2), dp(index1, index2-1))
-            return memo[index1][index2]
+    for(let i = 1; i <= n; i++) {
+        for(let j = 1; j <=m; j++) {
+            let ch1 = s[i-1], ch2 = rs[j-1]
+            if(ch1 == ch2) {
+                dp[i][j] = 1 + dp[i-1][j-1]
+            } else {
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])
+            }
         }
     }
     
-    return dp(n-1, m-1)
+    return dp[n][m]
 };
