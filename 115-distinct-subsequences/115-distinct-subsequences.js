f61da637e369971e108ecb2fdf43118893810a8b
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-14 17:10:54 +0530

    Time: 133 ms (74.83%), Space: 69.9 MB (45.45%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.js
+++ 115-distinct-subsequences/115-distinct-subsequences.js
@@ -3,7 +3,7 @@
  * @param {string} t
  * @return {number}
  */
-var numDistinct = function(s, t) {
+var numDistinctMemo = function(s, t) {
     
     let m = s.length, n = t.length
     let memo = new Array(m)
@@ -32,4 +32,35 @@ var numDistinct = function(s, t) {
         return memo[i][j]
     }
     return dp(m-1, n-1)
+};
+
+var numDistinct = function(s, t) {
+    
+    let m = s.length, n = t.length
+    let dp = new Array(m + 1)
+    for(let i = 0; i <= m; i++) {
+        dp[i] = new Array(n + 1)
+        dp[i].fill(0)
+    }
+    
+    // j = 0
+    for(let i = 0; i <= m; i++) {
+        dp[i][0] = 1
+    }
+    //i = 0
+    for(let j = 1; j <= n; j++) {
+        dp[0][j] = 0
+    }
+    
+    for(let i = 1; i <= m; i++)  {
+        for(let j = 1; j <= n; j++) {
+            if(s[i-1] === t[j-1]) {
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+            } else {
+                dp[i][j] = dp[i-1][j]
+            }
+        }
+    }
+    return dp[m][n]
+    
 };
