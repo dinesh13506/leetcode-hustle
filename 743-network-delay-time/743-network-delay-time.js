73513781271db464b664ea9ce43c83bdba03b505
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-14 07:43:09 +0530

    Time: 93 ms (96.21%), Space: 50.6 MB (68.95%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.js
@@ -0,0 +1,57 @@
+/**
+ * @param {number[][]} times
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var networkDelayTime = function(times, n, k) {
+    
+    let adjacencyList = new Map()
+    for(let time of times) {
+        let u = time[0] - 1, v = time[1] - 1, wt = time[2]
+        if(adjacencyList.has(u) === false) {
+            adjacencyList.set(u,new Map())
+        }
+        adjacencyList.get(u).set(v,wt)
+    }
+    //console.log(adjacencyList)
+    let dist = new Array(n)
+    dist.fill(Infinity)
+    dist[k-1] = 0
+    let set = new Set()
+    
+    let selectNext = function() {
+        let index = -1, min = Infinity
+        for(let i = 0; i < n; i++) {
+            if(dist[i] <= min && set.has(i) === false) {
+                min = dist[i]
+                index = i
+            }
+        }
+        return index
+    }
+    
+    for(let count = 0; count < n - 1; count++) {
+            let u = selectNext()
+            set.add(u)
+            //console.log(u,adjacencyList.get(u))
+            if(adjacencyList.has(u)) {
+                for(let v of adjacencyList.get(u).keys()) {
+                let wt = adjacencyList.get(u).get(v)
+                if(set.has(v) === false && dist[u] != Infinity && dist[v] > dist[u] + wt) {
+                    dist[v] = dist[u] + wt
+                }
+             }
+        }
+        
+    }
+    //console.log(dist)
+    let max = -Infinity
+    for(let d of dist) {
+        if(d > max) {
+            max = d
+        }
+    }
+    return max === Infinity ? -1 : max
+    
+};
