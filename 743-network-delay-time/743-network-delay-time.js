58ab698eb237e81dd566e944445d8e8631e11a92
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-14 07:45:27 +0530

    Time: 105 ms (89.71%), Space: 50.3 MB (75.45%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.js
+++ 743-network-delay-time/743-network-delay-time.js
@@ -8,19 +8,19 @@ var networkDelayTime = function(times, n, k) {
     
     let adjacencyList = new Map()
     for(let time of times) {
-        let u = time[0] - 1, v = time[1] - 1, wt = time[2]
+        let u = time[0] - 1, v = time[1] - 1, wt = time[2] //convert verticies to 0 based
         if(adjacencyList.has(u) === false) {
             adjacencyList.set(u,new Map())
         }
         adjacencyList.get(u).set(v,wt)
     }
-    //console.log(adjacencyList)
+    
     let dist = new Array(n)
     dist.fill(Infinity)
     dist[k-1] = 0
     let set = new Set()
-    
-    let selectNext = function() {
+    //select a vertex with min distance and which has not been chosen previously
+    let selectNextVertex = function() {
         let index = -1, min = Infinity
         for(let i = 0; i < n; i++) {
             if(dist[i] <= min && set.has(i) === false) {
@@ -31,21 +31,18 @@ var networkDelayTime = function(times, n, k) {
         return index
     }
     
-    for(let count = 0; count < n - 1; count++) {
-            let u = selectNext()
-            set.add(u)
-            //console.log(u,adjacencyList.get(u))
+    for(let count = 0; count < n; count++) {
+            let u = selectNextVertex()
+            set.add(u) //mark as chosen
             if(adjacencyList.has(u)) {
                 for(let v of adjacencyList.get(u).keys()) {
-                let wt = adjacencyList.get(u).get(v)
-                if(set.has(v) === false && dist[u] != Infinity && dist[v] > dist[u] + wt) {
-                    dist[v] = dist[u] + wt
+                    let wt = adjacencyList.get(u).get(v)
+                    if(set.has(v) === false && dist[u] != Infinity && dist[v] > dist[u] + wt) {
+                        dist[v] = dist[u] + wt
+                    }
                 }
-             }
-        }
-        
+            } 
     }
-    //console.log(dist)
     let max = -Infinity
     for(let d of dist) {
         if(d > max) {
