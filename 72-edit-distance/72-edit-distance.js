633a5a58f2572919eb7363c791a37307b68e56f7
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-16 18:07:56 +0530

    Time: 142 ms (37.34%), Space: 46.4 MB (83.19%) - LeetHub

--- 72-edit-distance/72-edit-distance.js
+++ 72-edit-distance/72-edit-distance.js
@@ -8,30 +8,28 @@ var minDistance = function(word1, word2) {
     let m = word1.length
     let n = word2.length
     
-    let dp = new Array(m+1)
-    for(let i = 0; i <= m; i++) {
-        dp[i] = new Array(n+1)
-        dp[i].fill(0)
-    }
+    let prev = new Array(n+1)
+    prev.fill(0)
     //first row
     for(let j = 0; j <= n; j++) {
-        dp[0][j] = j
+        prev[j] = j
     }
     
-    //first col
-    for(let i = 0; i <=m ;i++) {
-        dp[i][0] = i
-    }
+   
     
     for(let i = 1; i <= m; i++) {
+        let curr = new Array(n+1)
+        curr.fill(0)
+        curr[0] = i
         for(let j = 1; j <=n; j++) {
             if(word1[i-1] === word2[j-1]) {
-                dp[i][j] = 0 + dp[i-1][j-1]
+                curr[j] = 0 + prev[j-1]
             } else {
-                dp[i][j] = 1 + Math.min(dp[i-1][j],Math.min(dp[i-1][j-1], dp[i][j-1]))
+               curr[j] = 1 + Math.min(prev[j],Math.min(prev[j-1], curr[j-1]))
             }
         }
+        prev = curr
     }
     
-    return dp[m][n]
+    return prev[n]
 };
