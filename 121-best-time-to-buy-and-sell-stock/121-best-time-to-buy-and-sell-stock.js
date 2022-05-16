ffb12fffea67016965ba69cc1f3675024af6704a
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-16 23:08:21 +0530

    Time: 110 ms (49.61%), Space: 52.2 MB (14.83%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -3,19 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  let n = prices.length
-  let maxFuturePrice = new Array(n)
-  maxFuturePrice[n-1] = prices[n-1]
-  for(let i = n-2; i>=0; i--) {
-      let currentPrice = prices[i]
-      maxFuturePrice[i] = Math.max(maxFuturePrice[i+1],currentPrice)
-  }
-    let profit = 0
-    for(let i = 0; i < n; i++) {
-        let currentProfit = maxFuturePrice[i] - prices[i]
-        profit = Math.max(profit,currentProfit)
+    
+    let profit = 0, min = prices[0]
+    for(let i = 0; i < prices.length; i++) {
+        let currPrice = prices[i]
+        let cost = currPrice - min
+        profit = Math.max(profit, cost)
+        min = Math.min(min, currPrice)
     }
     return profit
-        
-    
 };
