2fbfe0e82f1b351db9f40af968c9082aa60af35b
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-17 23:42:21 +0530

    Time: 100 ms (68.18%), Space: 45.3 MB (53.03%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.js
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.js
@@ -3,7 +3,7 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(k, prices) {
+var maxProfitMemo = function(k, prices) {
     
     let n = prices.length
     
@@ -42,4 +42,43 @@ var maxProfit = function(k, prices) {
     }
     
     return dp(0,1,k)
+};
+
+
+var maxProfit = function(k, prices) {
+    
+    let n = prices.length
+    
+    let dp = new Array(n+1)
+    for(let i = 0; i <= n; i++) {
+        dp[i] = new  Array(2)
+        for(let buy = 0; buy <=1; buy++) {
+            dp[i][buy] = new Array(k+1)
+            dp[i][buy].fill(0)
+        }
+    }
+    
+    for(let i = n-1; i >=0; i--) {
+        for(let buy = 0; buy <=1; buy++) {
+            for(let cap=1; cap<=k; cap++) {
+               if(buy) {
+                    let buystock = -prices[i] + dp[i+1][0][cap]
+                    let notbuystock = 0 + dp[i+1][1][cap]
+                    let profit = Math.max(buystock, notbuystock)
+                    dp[i][buy][cap] =  profit
+                    
+            
+                } else {
+                    let sellstock = prices[i] + dp[i+1][1][cap-1]
+                    let notsellstock = 0 + dp[i+1][0][cap]
+                    let profit = Math.max(sellstock,notsellstock)
+                    dp[i][buy][cap] =  profit
+                } 
+            }
+        }
+    }
+    
+    return dp[0][1][k]
+    
+    
 };
