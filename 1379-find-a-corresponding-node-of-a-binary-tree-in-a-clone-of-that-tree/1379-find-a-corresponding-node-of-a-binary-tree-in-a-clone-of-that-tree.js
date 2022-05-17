bd6191fac69e3fd8347b86274dac0e9f287e12f6
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-17 05:57:33 +0530

    Time: 418 ms (29.61%), Space: 87.8 MB (13.97%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.js
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.js
@@ -12,25 +12,65 @@
  * @return {TreeNode}
  */
 
+let Node = function(treeNode) {
+    this.data = treeNode
+    this.next = null
+}
+
+let MyQueue = function() {
+    this.front = null
+    this.rear = null
+}
+
+MyQueue.prototype.isEmpty = function() {
+    if(this.front === null && this.rear === null) {
+        return true
+    }
+    return false
+}
+
+MyQueue.prototype.enque = function(treeNode) {
+    let node = new Node(treeNode)
+    if(this.isEmpty()) {
+        this.front = node
+        this.rear = node
+    } else {
+        this.rear.next = node
+        this.rear = this.rear.next
+    }
+}
+
+MyQueue.prototype.deque = function() {
+    if(this.isEmpty()) {
+        return
+    }
+    let node = this.front
+    if(this.front === this.rear) {
+        this.front = null
+        this.rear = null
+    } else {
+        this.front = this.front.next
+    }
+    return node
+}
 var getTargetCopy = function(original, cloned, target) {
     
-    let ans = null
-    let preorder = function(orig,clone) {
-        if(orig && clone) {
-            //console.log(orig.val, clone.val,target)
-            if(orig == target) {
-                ans = clone
-                return
-            }
-            preorder(orig.left, clone.left)
-            if(ans == null) {
-                preorder(orig.right, clone.right)
-            } else {
-                return ans
-            }
-            
+    let q = new MyQueue()
+    q.enque(original)
+    q.enque(cloned)
+    while(q.isEmpty() === false) {
+        let node_o = q.deque().data
+        let node_c = q.deque().data
+        if(node_o == target) {
+            return node_c
+        }
+        if(node_o.left && node_c.left) {
+            q.enque(node_o.left)
+            q.enque(node_c.left)
+        }
+        if(node_o.right && node_c.right) {
+            q.enque(node_o.right)
+            q.enque(node_c.right)
         }
     }
-    preorder(original, cloned)
-    return ans
 };
