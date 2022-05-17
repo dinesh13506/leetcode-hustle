89119efe53869bcabf94592fd56997e885c58884
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-17 23:13:45 +0530

    Time: 642 ms (19.03%), Space: 77.8 MB (21.46%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.js
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.js
@@ -2,46 +2,57 @@
  * @param {number[]} prices
  * @return {number}
  */
+
+
 var maxProfit = function(prices) {
     
     let n = prices.length
-    let memo = new Array(n)
-    for(let i = 0; i < n; i++) {
-        memo[i] = new Array(2)
+    let dp = new Array(n+1)
+    for(let i = 0; i <= n; i++) {
+        dp[i] = new Array(2)
         for(let j = 0; j < 2; j++) {
-            memo[i][j] = new Array(3) // atmost 3 transactions
-            memo[i][j].fill(-1)
+            dp[i][j] = new Array(3) // atmost 3 transactions
+            dp[i][j].fill(0)
         }
     }
     
-    let dp = function(index, buy, cap) {
-        
-        /* all days are exhausted */
-        if(index === n) {
-            return 0
-        }
-        if(cap === 0) {
-            return 0
-        }
-        
-        if(memo[index][buy][cap] != -1) {
-            return memo[index][buy][cap]
-        }
-        
-        if(buy) {
-            let buyStock = -1 * prices[index] + dp( index + 1, 0,  cap) //transaction is not caomplete
-            let notBuyStock = 0 + dp( index + 1, 1, cap)
-            let profit = Math.max(buyStock, notBuyStock)
-            memo[index][buy][cap] =  profit
-            return memo[index][buy][cap]
-        } else {
-            let sellStock = prices[index] + dp( index+1, 1, cap-1)
-            let notSellStock = 0 + dp(index+1, 0, cap)
-            let profit = Math.max(sellStock, notSellStock)
-            memo[index][buy][cap] =  profit
-            return memo[index][buy][cap]
+    //first base case if index == n then buy and cap can be anything
+    for(let buy = 0; buy <=1; buy++) {
+        for(let cap = 0; cap <=2; cap++) {
+            dp[n][buy][cap] = 0
         }
     }
     
-    return dp(0,1,2)
+    //second base case if cap == 0 then index and buy can be anything
+    for(let i = 0 ; i <= n; i++) {
+        for(let buy = 0; buy <=1; buy++) {
+            dp[i][buy][0] = 0
+        }
+    }
+    
+    for(let i = n-1; i >=0; i--) {
+        for(let buy = 0; buy <=1; buy++) {
+            for(let cap = 0; cap<=2; cap++) {
+                    if(cap == 0) {
+                        dp[i][buy][cap] =  0
+                        continue
+                    }
+                    if(buy) {
+                        let buyStock = -1 * prices[i] + dp[i + 1][0][cap]
+                        let notBuyStock = 0 + dp[i + 1][1][cap]
+                        let profit = Math.max(buyStock, notBuyStock)
+                        dp[i][buy][cap] =  profit
+                     } 
+                    else {
+                        let sellStock = prices[i] + dp[i+1][1][cap-1]
+                        let notSellStock = 0 + dp[i+1][0][cap]
+                        let profit = Math.max(sellStock, notSellStock)
+                        dp[i][buy][cap] =  profit
+                    }
+                
+            }     
+        }
+    }
+    
+    return dp[0][1][2]
 };
