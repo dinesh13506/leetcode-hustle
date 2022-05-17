9207e1edf0592fe6c42185430f96904075175f62
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-18 00:01:40 +0530

    Time: 80 ms (66.87%), Space: 44.4 MB (44.18%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.js
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.js
@@ -35,3 +35,38 @@ var maxProfit = function(prices) {
     
     return dp(0,1)
 };
+
+var maxProfitMemo = function(prices) {
+    
+    let n = prices.length
+    
+    let dp = new Array(n+1)
+    for(let i = 0; i <=n; i++) {
+        dp[i] = new Array(2)
+        dp[i].fill(0)
+    }
+    
+    for(let buy = 0; buy <= 1; buy++) {
+        dp[n][buy] = 0
+    }
+    
+    for(let i = n-1; i>=0; i--) {
+        for(let buy = 0; buy <= 1; buy++) {
+            if(buy) {
+                let buystock = -prices[i] + dp[i+1][0]
+                let notbuy = 0 + dp[i+1][1]
+                let profit = Math.max(buystock, notbuy)
+                dp[i][buy] = profit
+            } else {
+                let sellstock = prices[i] + (dp[i+2][1] || 0)
+                let notsellstock = 0 + dp[i+1][0]
+                let profit = Math.max(sellstock, notsellstock)
+               dp[i][buy] = profit
+            }
+        }
+    }
+    
+    return dp[0][1]
+    
+    
+};
