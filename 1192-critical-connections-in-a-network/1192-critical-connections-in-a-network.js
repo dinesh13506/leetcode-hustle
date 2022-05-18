8e4699a0f042440f71ec1c11323e0b757124c213
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-18 07:26:12 +0530

    Time: 995 ms (44.56%), Space: 147.6 MB (54.35%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.js
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.js
@@ -4,65 +4,41 @@
  * @return {number[][]}
  */
 var criticalConnections = function(n, connections) {
-    let result = []
-    let cycleEdges = new Map()
-    let rankList = new Array(n)
-    rankList.fill(null)
-    let adjacencyList = new Array(n)
-    for(let i = 0; i < n; i++) {
-        adjacencyList[i] = new Array()
+  const adjacency = {}
+  for (let i = 0; i < n; i++) {
+    adjacency[i] = []
+  }
+
+  for (let [node1, node2] of connections) {
+    adjacency[node1].push(node2)
+    adjacency[node2].push(node1)
+  }
+
+  const dfsNumber = new Array(n).fill(0)
+  // this is what we called oldestReachable
+  // the lowest number reachable by dfs from a node
+  const dfsLow = new Array(n).fill(0)
+  const criticalEdges = []
+  let timestamp = 0
+
+  function tarjan(node, parent) {
+    timestamp++
+    dfsNumber[node] = timestamp
+    dfsLow[node] = timestamp
+
+    for (let neighbor of adjacency[node]) {
+      if (neighbor === parent) continue
+      if (!dfsNumber[neighbor]) tarjan(neighbor, node)
+
+      // resetting oldestReachable if neighbor can cycle back
+      dfsLow[node] = Math.min(dfsLow[node], dfsLow[neighbor])
+
+      // if neighbor cannot reach back to me or oldest, we have a critical edge
+      if (dfsLow[neighbor] > dfsNumber[node]) criticalEdges.push([node, neighbor])
     }
-    
-    for(let connection of connections) {
-        let u = connection[0]
-        let v = connection[1]
-        //console.log(u,v)
-        adjacencyList[u].push(v)
-        adjacencyList[v].push(u)
-        let minU = Math.min(u,v)
-        let maxV = Math.max(u,v)
-        let key = `${minU},${maxV}`
-        cycleEdges.set(key,false)
-    }
-    
-    //console.log(adjacencyList)
-    
-    let dfsUtil = function(s,rank) {
-        //if node is already been visited
-        if(rankList[s] != null) {
-            return rankList[s]
-        }
-        let minRank = rank + 1
-        rankList[s] = rank
-        let neighbors = adjacencyList[s]
-        for(let v of neighbors) {
-            if(rankList[v] != null && rankList[v] === rank -1){
-                continue
-            }
-            let recursiveRank = dfsUtil(v,rank+1)
-            if(recursiveRank <= rankList[s]) {
-                //console.log(` s = ${s},v = ${v}`)
-                let minU = Math.min(s,v)
-                let maxV = Math.max(s,v)
-                let key = `${minU},${maxV}`
-                cycleEdges.set(key,true)
-            }
-            minRank = Math.min(recursiveRank,minRank)
-        }
-        return minRank
-    }
-    
-    dfsUtil(0,0)
-    
-    for(let connection of connections) {
-        let u = connection[0]
-        let v = connection[1]
-        let minU = Math.min(u,v)
-        let maxV = Math.max(u,v)
-        let key = `${minU},${maxV}`
-        if(cycleEdges.get(key) === false) {
-            result.push(connection)
-        }
-    }
-    return result
-};
+  }
+
+  tarjan(0, null, adjacency, criticalEdges)
+
+  return criticalEdges
+}
