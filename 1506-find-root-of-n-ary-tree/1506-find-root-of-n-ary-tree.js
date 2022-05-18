cf89bcf4cf9e517e6a9ae680dc3ea0092c8edb66
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-18 07:56:34 +0530

    Time: 277 ms (20.59%), Space: 65.1 MB (20.59%) - LeetHub

---
+++ 1506-find-root-of-n-ary-tree/1506-find-root-of-n-ary-tree.js
@@ -0,0 +1,44 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.children = children === undefined ? [] : children;
+ * };
+ */
+
+/**
+ * @param {Node[]} tree
+ * @return {Node}
+ */
+var findRoot = function(tree) {
+    let indegree = new Map()
+    let nodeMap = new Map()
+    let set = new Set()
+    for(let node of tree) {
+        indegree.set(node.val, 0)
+    }
+    for(let node of tree) {
+        //console.log(node.val, node.children.length)
+        if(node.children && node.children.length > 0) {
+            nodeMap.set(node.val, node)
+            for(let child of node.children) {
+                indegree.set(child.val, (indegree.get(child) || 0) + 1)
+                nodeMap.set(child.val, child)
+                set.add(child)
+            }
+        } else {
+            nodeMap.set(node.val, node)
+        }
+    }
+    //console.log(indegree,nodeMap)
+    for(let nodeval of indegree.keys()) {
+        if(indegree.get(nodeval) == 0) {
+            return nodeMap.get(nodeval)
+        }
+    }
+    
+    
+    
+    
+    
+};
