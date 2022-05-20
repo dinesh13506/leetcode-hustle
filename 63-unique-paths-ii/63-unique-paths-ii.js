f252eb09e5ab12c1ce4b1d7916bfb7f4ee2b8827
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-20 06:38:13 +0530

    Time: 60 ms (97.69%), Space: 42.7 MB (54.34%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.js
+++ 63-unique-paths-ii/63-unique-paths-ii.js
@@ -2,36 +2,46 @@
  * @param {number[][]} obstacleGrid
  * @return {number}
  */
-var uniquePathsWithObstacles = function(obstacleGrid) {
+var uniquePathsWithObstacles = function(grid) {
     
-    let m = obstacleGrid.length, n = obstacleGrid[0].length
-    let prev = new Array(n)
-    prev.fill(0)
+    //DP on grid
+    let n = grid.length, m = grid[0].length
     
-    let mod = 2 * Math.pow(10,9)
-    for(let i = 0; i < m; i++) {
-        let curr = new Array(n)
-        curr.fill(0)
-        for(let j = 0; j < n; j++) {
-            if(obstacleGrid[i][j] == 1) {
-                curr[j] = 0
-            }
-            else if( i == 0 && j == 0) {
-                curr[j] = 1
-            }
-            else {
-                let up = 0 
-                if(i > 0 ) {
-                    up = prev[j]
-                }
-                let left = 0
-                if( j > 0) {
-                    left = curr[j-1]
-                }
-                curr[j] = ((up%mod) + (left%mod))%mod
-            }
-        }
-        prev = curr.slice()
+    let memo = new Array(n)
+    for(let i = 0; i < n; i++) {
+        memo[i] = new Array(m)
+        memo[i].fill(-1)
     }
-    return prev[n-1]
+    
+    let uniquePaths = function(i,j) {
+        if(grid[i][j] == 1) {
+            return 0
+        }
+        if(i == 0 && j == 0) {
+            return 1
+        }
+        
+        
+        //already computed
+        if(memo[i][j] != -1) {
+            return memo[i][j]
+        }
+        
+        //left
+        let left = 0
+        if(j > 0) {
+            left = uniquePaths(i, j-1)
+        }
+        let up = 0
+        if(i > 0) {
+            up = uniquePaths(i-1,j)
+        }
+        
+        memo[i][j] = left + up //compute and store
+        return memo[i][j]
+        
+    }
+    return uniquePaths(n-1, m-1)
+    
+    
 };
