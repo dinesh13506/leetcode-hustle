4807aaca1e399ed11593cb1775234497636df61d
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-20 07:55:34 +0530

    Time: 93 ms (44.83%), Space: 44.6 MB (24.14%) - LeetHub

---
+++ 1854-maximum-population-year/1854-maximum-population-year.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} logs
+ * @return {number}
+ */
+var maximumPopulation = function(logs) {
+    let map = new Map()
+    for(let log of logs) {
+        let birth = log[0]
+        let death = log[1]
+        for(let i = birth; i < death; i++) {
+            map.set(i, (map.get(i) || 0) + 1)
+        }
+    }
+    
+    //console.log(map)
+    let arr = []
+    for(let year of map.keys()) {
+        arr.push([year, map.get(year)])
+    }
+    arr.sort(function(a,b) {
+        if(b[1] == a[1]) {
+            return a[0] - b[0]
+        }
+        return b[1] - a[1]
+    })
+    //console.log(arr)
+    return arr[0][0]
+};
