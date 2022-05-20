33d5ba7663a022b5e736f7d521a461db7784c3de
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-20 07:14:57 +0530

    Time: 102 ms (31.25%), Space: 42.9 MB (71.63%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
@@ -5,20 +5,19 @@
 var halvesAreAlike = function(s) {
     
     let vowels = ['a','e','i','o','u','A', 'E', 'I', 'O', 'U']
-    let mapa = new Map()
-    let mapb = new Map()
+    let set = new Set(vowels)
     let i = 0, j = s.length-1
+    let count1 = 0, count2 = 0
     while(i < j) {
         let cha = s[i], chb = s[j]
-        mapa.set(cha, (mapa.get(cha) || 0 ) + 1)
-        mapb.set(chb, (mapb.get(chb) || 0 ) + 1)
+        if(set.has(cha)) {
+            count1++
+        }
+        if(set.has(chb)) {
+            count2++
+        }
         i++
         j--
-    }
-    let count1 = 0, count2 = 0
-    for(let v of vowels) {
-        count1 = count1 + (mapa.get(v) || 0)
-        count2 = count2 + (mapb.get(v) || 0)
     }
     return count1 == count2
 };
