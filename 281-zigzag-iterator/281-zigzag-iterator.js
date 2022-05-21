b9d6669e2e5217e2791740ac982fcdc88f56ab86
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-21 16:22:13 +0530

    Time: 109 ms (37.04%), Space: 44.9 MB (62.96%) - LeetHub

---
+++ 281-zigzag-iterator/281-zigzag-iterator.js
@@ -0,0 +1,63 @@
+/**
+ * @constructor
+ * @param {Integer[]} v1
+ * @param {Integer[]} v1
+ */
+var ZigzagIterator = function ZigzagIterator(v1, v2) {
+    
+    let n1 = v1.length, n2 = v2.length
+    let i = 0 , j = 0
+    let first = true
+    let result = []
+    while( i < n1 && j < n2) {
+        if(first) {
+            result.push(v1[i])
+            i++
+            first = false
+        } else {
+            result.push(v2[j])
+            j++
+            first = true
+        }
+    }
+    while(i < n1) {
+        result.push(v1[i])
+        i++
+    }
+    while(j < n2) {
+        result.push(v2[j])
+        j++
+    }
+    
+    //console.log(result)
+    this.result = result
+    this.index = 0
+    this.length = n1 + n2
+};
+
+
+/**
+ * @this ZigzagIterator
+ * @returns {boolean}
+ */
+ZigzagIterator.prototype.hasNext = function hasNext() {
+    if(this.index < this.length) {
+        return true
+    }
+    return false
+};
+
+/**
+ * @this ZigzagIterator
+ * @returns {integer}
+ */
+ZigzagIterator.prototype.next = function next() {
+    return this.result[this.index++]
+    
+};
+
+/**
+ * Your ZigzagIterator will be called like this:
+ * var i = new ZigzagIterator(v1, v2), a = [];
+ * while (i.hasNext()) a.push(i.next());
+*/
