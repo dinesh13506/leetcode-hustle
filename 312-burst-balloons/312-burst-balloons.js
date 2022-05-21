6593062011aed02ee4c0ed4cc89e8641d7bfcfea
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-22 01:42:13 +0530

    Time: 151 ms (100.00%), Space: 44.3 MB (87.27%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.js
+++ 312-burst-balloons/312-burst-balloons.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxCoins = function(nums) {
+var maxCoinsMemo = function(nums) {
    
     let n = nums.length
     nums.push(1)
@@ -32,4 +32,36 @@ var maxCoins = function(nums) {
     }
     
     return dp(1,n)
+};
+
+
+
+var maxCoins = function(nums) {
+   
+    let n = nums.length
+    nums.push(1)
+    nums.unshift(1)
+    
+    let dp = new Array(n+2)
+    for(let i = 0; i <= n+1; i++) {
+        dp[i] = new Array(n+2)
+        dp[i].fill(0)
+    }
+    
+    for(let i = n; i >= 1; i--) {
+        for(let j=1; j <=n; j++) {
+            if( i > j) continue
+            let max = -Infinity
+            for(let index = i; index <=j; index++) {
+                let coins = (nums[i-1] * nums[index] * nums[j+1]) + dp[i][index-1] + dp[index+1][j]
+                max = Math.max(max,coins)
+            }
+            dp[i][j] =  max
+        }
+    }
+    
+    return dp[1][n]
+   
+    
+    
 };
