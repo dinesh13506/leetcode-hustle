2217e978138cf3095c21b15d61c12f09715c4430
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-22 01:27:12 +0530

    Time: 571 ms (56.36%), Space: 44.3 MB (90.00%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.js
+++ 312-burst-balloons/312-burst-balloons.js
@@ -3,19 +3,18 @@
  * @return {number}
  */
 var maxCoins = function(nums) {
-    
+   
     let n = nums.length
-    let coins = new Array(n+2)
-    for(let i = 0; i < n; i++) {
-        coins[i+1] = nums[i]
-    }
-    coins[0] = 1
-    coins[n+1] = 1
-    let memo = new Array(n+2)
-    for(let i = 0; i < n+2; i++) {
-        memo[i] = new Array(n+2)
+    nums.push(1)
+    nums.unshift(1)
+    
+    let memo = new Array(n+1)
+    for(let i = 0; i <= n; i++) {
+        memo[i] = new Array(n+1)
         memo[i].fill(-1)
     }
+   
+    
     let dp = function(i,j) {
         if(i > j) {
             return 0
@@ -23,15 +22,14 @@ var maxCoins = function(nums) {
         if(memo[i][j] != -1) {
             return memo[i][j]
         }
-        let ans = 0
-        for(let k= i + 1; k < j; k++) {
-            let gain = coins[k] * coins[i] * coins[j]
-            gain = gain + dp(i, k) + dp(k,j)
-            ans = Math.max(ans, gain)
+        let max = -Infinity
+        for(let index = i; index <=j; index++) {
+            let coins = (nums[i-1] * nums[index] * nums[j+1]) + dp(i,index-1) + dp(index+1,j)
+            max = Math.max(max,coins)
         }
-        memo[i][j] = ans
-        return  memo[i][j]
+        memo[i][j] =  max
+        return memo[i][j]
     }
     
-    return dp(0, n+1)
+    return dp(1,n)
 };
