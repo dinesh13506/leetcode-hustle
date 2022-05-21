b453c73ce4f797259072cc44750267be2452d9b5
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-21 18:26:24 +0530

    Time: 125 ms (37.50%), Space: 42.6 MB (65.00%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.js
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.js
@@ -2,7 +2,7 @@
  * @param {number[]} arr
  * @return {number}
  */
-var mctFromLeafValues = function(arr) {
+var mctFromLeafValuesMemo = function(arr) {
     
     let n = arr.length
     let memo = new Array(n)
@@ -43,4 +43,42 @@ var mctFromLeafValues = function(arr) {
     
    
     return dp(0,n-1)
+};
+
+
+var mctFromLeafValues = function(arr) {
+    
+    let n = arr.length
+    let dp = new Array(n)
+    for(let i = 0; i < n; i++) {
+        dp[i] = new Array(n)
+        dp[i].fill(0)
+    }
+    
+    for(let i = n-1; i >=0; i--) {
+        for(let j = i + 1; j < n; j++) {
+            let ans = Infinity
+            for(let k = i; k < j; k++) {
+
+                //partition1
+                let left = 0
+                for(let index = i; index <=k; index++) {
+                    left = Math.max(left, arr[index])
+                }
+
+                //partition2
+                let right = 0
+                for(let index = k+1; index <=j; index++) {
+                    right = Math.max(right, arr[index])
+                }
+
+                ans = Math.min(ans, (left * right) + dp[i][k] + dp[k+1][j])
+
+            }
+            dp[i][j] =  ans
+        }
+    }
+    return dp[0][n-1]
+    
+    
 };
