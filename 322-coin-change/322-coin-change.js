4cb7056852fa99a640fb3233d8a8c330cc773df3
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-21 08:25:32 +0530

    Time: 194 ms (42.49%), Space: 47.3 MB (59.57%) - LeetHub

--- 322-coin-change/322-coin-change.js
+++ 322-coin-change/322-coin-change.js
@@ -3,7 +3,7 @@
  * @param {number} amount
  * @return {number}
  */
-var coinChange = function(coins, amount) {
+var coinChangeMemo = function(coins, amount) {
     
     let n = coins.length
     let memo = new Array(n)
@@ -38,4 +38,44 @@ var coinChange = function(coins, amount) {
     
     let count =  dp(n-1,amount)
     return count == Infinity ? -1 : count
+};
+
+
+var coinChange = function(coins, amount) {
+    
+    let n = coins.length
+    let dp = new Array(n)
+    for(let i = 0; i < n; i++) {
+        dp[i] = new Array(amount+1)
+        dp[i].fill(0)
+    }
+    
+    //if amount is 0
+    for(let i = 0; i < n; i++) {
+        dp[i][0] = 0
+    }
+    //second base case
+    for(let amt = 1; amt <= amount; amt++) {
+        if(amt % coins[0] == 0) {
+            dp[0][amt] = amt / coins[0]
+        } else {
+            dp[0][amt] =  Infinity
+        }
+    }
+    
+    //console.log(dp)
+    for(let i = 1; i < n; i++) {
+        for(let amt = 1; amt <= amount; amt++) {
+            let take = Infinity
+            if(amt >= coins[i]) {
+                take = 1 + dp[i][amt-coins[i]]
+            }
+            let nottake = 0 + dp[i-1][amt]
+            
+            dp[i][amt]  =  Math.min(take, nottake)
+        }
+    }
+    
+    return ( dp[n-1][amount] === Infinity ? -1 : dp[n-1][amount])
+    
 };
