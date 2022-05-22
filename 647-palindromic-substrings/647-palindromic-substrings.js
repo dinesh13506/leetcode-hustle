66fdfe033e43825044f0b889c9d70927dca2426e
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-22 13:20:17 +0530

    Time: 79 ms (81.56%), Space: 42.6 MB (87.20%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.js
+++ 647-palindromic-substrings/647-palindromic-substrings.js
@@ -5,32 +5,27 @@
 var countSubstrings = function(s) {
     
     let n = s.length
-    let ans = 0
-    let dp = new Array(n)
-    for(let i = 0; i < n; i++) {
-        dp[i] = new Array(n)
-        dp[i].fill(false)
-    }
+    let result = 0
     
-    for(let i = 0; i < n; i++) {
-        dp[i][i] = true
-        ans++
-    }
-    for(let i = 0; i < n-1; i++) {
-        if(s[i] == s[i+1]) {
-            dp[i][i+1] = true
-            ans++
+    let countPalindromes = function(start, end) {
+        let count = 0
+        while(start >=0 && end < n) {
+            if(s[start] == s[end]) {
+                start--
+                end++
+                count++
+            } else {
+                break
+            }
         }
+        return count
     }
-    
-    for(let len = 3; len <=n; len++) {
-        let i = 0, j = i + len -1
-        while(j < n) {
-            dp[i][j] = dp[i+1][j-1] && s[i] == s[j]
-            ans = ans + (dp[i][j] == true ? 1 : 0)
-            i++
-            j++
-        }
+    for(let centerIndex = 0; centerIndex < n; centerIndex++) {
+        //odd length palindrome
+        result += countPalindromes(centerIndex,centerIndex)
+        //odd length of palindrome
+        result += countPalindromes(centerIndex,centerIndex + 1)
+        
     }
-    return ans
+    return result
 };
