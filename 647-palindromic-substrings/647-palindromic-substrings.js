22d08d30a30c9c48b61a5ba1fb1acf28c69d8eb5
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-22 09:57:52 +0530

    Time: 1191 ms (5.91%), Space: 88 MB (5.10%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.js
+++ 647-palindromic-substrings/647-palindromic-substrings.js
@@ -5,19 +5,29 @@
 var countSubstrings = function(s) {
    let count = 0
    let n = s.length
-   for(let start = 0; start < n; start++) {
-       for(let len = 1; len <= n - start; len++) {
-           //console.log(" * " ,s.substr(start, len))
+   let memo = new Map()
+   memo.set('',true)
+    
+   for(let len = 1; len <= n; len++) {
+       for(let start = 0; start <= n - len; start++) {
            let str = s.substr(start, len)
-           let p1 = 0, p2 = str.length-1
-           count = count + 1
-           while(p1 <= p2) {
-               if(str[p1] != str[p2]) {
-                   count--
-                   break
-               }
-               p1++
-               p2--
+           //console.log("curr => " ,s.substr(start, len))
+           //console.log("small => ", str.slice(1, -1), memo.get(str.slice(1, -1)))
+           if(str.length == 1) {
+               count++
+               //console.log("plaindrome => ", str)
+               memo.set(str, true)
+               continue
+           }
+           let firstChar = str[0]
+           let lastChar = str[str.length-1]
+          
+           if(firstChar === lastChar && memo.get(str.slice(1, -1)) == true) {
+               count++
+               //console.log("plaindrome => ", str)
+               memo.set(str, true)
+           } else {
+               memo.set(str, false)
            }
        }
    }
