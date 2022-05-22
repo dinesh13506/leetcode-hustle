d03673798aeaf83e543fd8c3363e5aa8070ef67a
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-22 10:00:01 +0530

    Time: 1232 ms (5.46%), Space: 82.7 MB (5.64%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.js
+++ 647-palindromic-substrings/647-palindromic-substrings.js
@@ -11,6 +11,7 @@ var countSubstrings = function(s) {
    for(let len = 1; len <= n; len++) {
        for(let start = 0; start <= n - len; start++) {
            let str = s.substr(start, len)
+           let prev = str.slice(1, str.length-1)
            //console.log("curr => " ,s.substr(start, len))
            //console.log("small => ", str.slice(1, -1), memo.get(str.slice(1, -1)))
            if(str.length == 1) {
@@ -22,7 +23,7 @@ var countSubstrings = function(s) {
            let firstChar = str[0]
            let lastChar = str[str.length-1]
           
-           if(firstChar === lastChar && memo.get(str.slice(1, -1)) == true) {
+           if(firstChar === lastChar && memo.get(prev) == true) {
                count++
                //console.log("plaindrome => ", str)
                memo.set(str, true)
