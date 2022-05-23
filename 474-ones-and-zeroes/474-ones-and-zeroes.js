d32c1a76901b51739bdb016187dd126f72c00740
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-23 08:25:02 +0530

    Time: 548 ms (52.33%), Space: 130.4 MB (26.74%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.js
+++ 474-ones-and-zeroes/474-ones-and-zeroes.js
@@ -4,7 +4,7 @@
  * @param {number} n
  * @return {number}
  */
-var findMaxForm = function(strs, m, n) {
+var findMaxFormMemo = function(strs, m, n) {
     
     //strs has n states
     //zeroes has 0 ..  to m states => m + 1
@@ -56,6 +56,55 @@ var findMaxForm = function(strs, m, n) {
         memo[i][zeroes][ones] = Math.max(take,nottake)
         return memo[i][zeroes][ones]
     }
-    
     return dp(0, m, n)
+};
+
+var findMaxForm = function(strs, m, n) {
+    
+    //strs has n states
+    //zeroes has 0 ..  to m states => m + 1
+    // ones has 0 ... n states => n+1
+    // memo[strs.length][m+1][n+1]
+    let dp = new Array(strs.length + 1)
+    for(let i = 0; i <= strs.length; i++) {
+        dp[i] = new Array(m+1)
+        for(let j = 0; j < m+1; j++) {
+            dp[i][j] = new Array(n+1)
+            dp[i][j].fill(0)
+        }
+    }
+    
+    //calculate zeroes and ones
+    let getCharCount = function(str) {
+        let zero = 0, len = str.length
+        for(let ch of str) {
+            if(ch == '0') {
+                zero++
+            }
+        }
+        return [zero, len - zero]
+    }
+    
+    //pre calculate for all strs
+    let count = []
+    for(let str of strs) {
+        count.push(getCharCount(str))
+    }
+    
+    for(let i = strs.length-1; i>=0 ; i--) {
+        for(let zeroes = 0; zeroes <=m; zeroes++) {
+            for(let ones = 0; ones <=n; ones++) {
+                let max = 0
+                let arr = count[i]
+                let take = -1
+                if(zeroes - arr[0] >= 0 && ones - arr[1] >= 0) {
+                    take = 1 + dp[i+1][zeroes - arr[0]][ones - arr[1]]
+                }
+                let nottake = 0 + dp[i+1][zeroes - 0][ones - 0]
+                dp[i][zeroes][ones] = Math.max(take,nottake)
+            }
+        }
+    }
+    
+    return dp[0][m][n]
 };
