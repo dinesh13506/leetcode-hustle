74156fbe5a793802c6b778224c5aee07679e6472
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-23 22:27:47 +0530

    Time: 109 ms (43.24%), Space: 43.1 MB (27.03%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.js
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.js
@@ -3,45 +3,35 @@
  * @param {number} k
  * @return {number}
  */
+var maxSumAfterPartitioning = function(arr, k) {
+    return maxSumAfterPartitioningMemo(arr, k)
+};
+
+
 var maxSumAfterPartitioningMemo = function(arr, k) {
-    
     let n = arr.length
     let memo = new Array(n)
     memo.fill(-1)
+    let min = function(a,b) {
+        return Math.min(a,b)
+    }
+    let max = function(a,b) {
+        return Math.max(a,b)
+    }
     let dp = function(i) {
-        if(i == n) {
-            return 0
-        }
+        if(i == n) return 0
         if(memo[i] != -1) {
             return memo[i]
         }
-        let len = 0, maxi = -Infinity, maxAns = -Infinity 
-        for(let j = i; j < Math.min(i + k, n); j++) {
+        let len = 0, maxi = -Infinity, maxel = -Infinity
+        for(let j= i ; j < min(i+k, n); j++) {
             len++
-            maxi = Math.max(maxi, arr[j])
-            let sum = (len * maxi) + dp(j+1)
-            maxAns = Math.max(maxAns, sum)
+            maxel = max(maxel, arr[j])
+            let sum = len * maxel + dp(j+1)
+            maxi = max(maxi, sum)
         }
-        memo[i] =  maxAns
+        memo[i] =  maxi
         return memo[i]
     }
     return dp(0)
-};
-
-var maxSumAfterPartitioning = function(arr, k) {
-    
-    let n = arr.length
-    let dp = new Array(n+1)
-    dp.fill(0)
-    for(let i = n-1; i >=0; i--) {
-        let len = 0, maxi = -Infinity, maxAns = -Infinity 
-        for(let j = i; j < Math.min(i + k, n); j++) {
-            len++
-            maxi = Math.max(maxi, arr[j])
-            let sum = (len * maxi) + dp[j+1]
-            maxAns = Math.max(maxAns, sum)
-        }
-        dp[i] =  maxAns
-    }
-    return dp[0]
 };
