5939112785873bf2173e8fe771661ada6d300c3c
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-23 22:17:45 +0530

    Time: 1236 ms (19.28%), Space: 43 MB (93.98%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.js
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.js
@@ -41,3 +41,32 @@ var minCutMemo = function(s) {
     }
     return dp(0) - 1
 }
+
+var minCutTopDown = function(s) {
+    let n = s.length
+    let dp = new Array(n+1)
+    dp.fill(0)
+    
+    let ispalindrome = function(i, j) {
+        while(i <= j) {
+            if(s[i] != s[j]) {
+                return false
+            }
+            i++
+            j--
+        }
+        return true
+    }
+    
+    for(let i = n-1; i >=0; i++) {
+        let mini = Infinity
+        for(let j = i; j < n; j++) {
+            if(ispalindrome(i,j) === true) {
+                let count = 1 + dp[j + 1]
+                mini = Math.min(mini, count)
+            }
+        }
+        dp[i] = mini
+    }
+    return dp[0] - 1
+}
