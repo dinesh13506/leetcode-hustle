3097122f7762a9df5b312f5ea38bc015e36bc5fe
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-23 18:58:45 +0530

    Time: 134 ms (40.54%), Space: 42.6 MB (48.65%) - LeetHub

---
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var maxSumAfterPartitioning = function(arr, k) {
+    
+    let n = arr.length
+    let memo = new Array(n)
+    memo.fill(-1)
+    let dp = function(i) {
+        if(i == n) {
+            return 0
+        }
+        if(memo[i] != -1) {
+            return memo[i]
+        }
+        let len = 0, maxi = -Infinity, maxAns = -Infinity 
+        for(let j = i; j < Math.min(i + k, n); j++) {
+            len++
+            maxi = Math.max(maxi, arr[j])
+            let sum = (len * maxi) + dp(j+1)
+            maxAns = Math.max(maxAns, sum)
+        }
+        memo[i] =  maxAns
+        return memo[i]
+    }
+    return dp(0)
+};
