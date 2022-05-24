8d47b8224998e27fb68b2b0908d5f86e28494346
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-24 18:40:13 +0530

    Time: 155 ms (15.40%), Space: 45.1 MB (74.86%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    
+    let n = candidates.length
+    let combinations = []
+    let f = function(index, target, combination) {
+        if(index == n) {
+            if(target == 0) {
+                combinations.push(combination.slice())
+            }
+            return
+        }
+        
+        
+        //take
+        if(candidates[index] <= target) {
+            combination.push(candidates[index])
+            f(index, target - candidates[index], combination)
+            //not take
+            combination.pop()
+        }
+        f(index + 1, target, combination)
+    }
+    
+    f(0, target, [])
+    return combinations
+ };
