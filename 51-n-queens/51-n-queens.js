42b4c8ca4e2fe5f5d62c130ebc8314590d19c0f6
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-26 23:22:07 +0530

    Time: 93 ms (72.48%), Space: 45.6 MB (40.83%) - LeetHub

---
+++ 51-n-queens/51-n-queens.js
@@ -0,0 +1,67 @@
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+var solveNQueens = function(n) {
+   
+    
+    let result = []
+    let board = new Array(n)
+    for(let i = 0; i < n; i++) {
+        board[i] = new Array(n)
+        board[i].fill('.')
+    }
+    
+    let canPlace = function(row, col) {
+        let i = row, j = col
+        
+        while(j>=0) {
+            if(board[i][j] == 'Q') {
+                return false
+            }
+            j--
+        }
+        i = row, j = col
+        while(i>=0 && j >=0) {
+            if(board[i][j] == 'Q') {
+                return false
+            }
+            i--
+            j--
+        }
+        i = row, j = col
+        while( i < n && j >=0) {
+            if(board[i][j] == 'Q') {
+                return false
+            }
+            i++
+            j--
+        }
+        return true
+    }
+    
+    let f = function(col, board) {
+        
+        if(col == n) {
+            //console.log(board.slice())
+            let temp = []
+            for(let i = 0; i < n; i++) {
+                temp.push(board[i].join(''))
+            }
+            result.push(temp)
+            return
+        }
+        
+        for(let row = 0; row < n; row++) {
+            if(canPlace(row, col) == true) {
+                board[row][col] = 'Q'
+                f(col+1, board)
+                board[row][col] = '.'
+            }
+        }
+    }
+    
+    
+    f(0, board)
+    return result
+};
