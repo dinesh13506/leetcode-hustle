05dbbb1d8b231656e51d28c735cb80368541b150
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-26 09:27:26 +0530

    Time: 106 ms (33.45%), Space: 44.9 MB (33.27%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.js
+++ 90-subsets-ii/90-subsets-ii.js
@@ -4,28 +4,22 @@
  */
 var subsetsWithDup = function(nums) {
     
+    nums.sort(function(a,b) {
+        return a - b
+    })
+    
     let n = nums.length
-    let map = new Map()
     let ans = []
-    let f = function(i,subset) {
-        
-        if(i >= n) {
-            let copy = subset.slice()
-            copy.sort(function(a,b) { return a - b})
-            let key = copy.join('*')
-            if(map.has(key) == false) {
-                ans.push(copy)
-                map.set(key, true)
+    let f = function(index, arr) {
+        ans.push(arr.slice())
+        for(let i = index; i < n; i++) {
+            if( i != index && nums[i] == nums[i-1]) {
+                continue
             }
-            return
+            arr.push(nums[i])
+            f(i+1, arr)
+            arr.pop()
         }
-        //take
-        subset.push(nums[i])
-        f(i+1, subset)
-        
-        //notake
-        subset.pop()
-        f(i+1, subset)
     }
     
     f(0,[])
