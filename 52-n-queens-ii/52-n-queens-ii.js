e7e611b1d82cdf74d5552017ade430b802cb64f0
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-26 23:36:53 +0530

    Time: 97 ms (41.42%), Space: 43.1 MB (70.23%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.js
@@ -0,0 +1,63 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var totalNQueens = function(n) {
+    
+    let board = new Array(n)
+    for(let i = 0; i < n; i++) {
+        board[i] = new Array(n)
+        board[i].fill('.')
+    }
+    
+    let canAttack = function(row, col) {
+        let i = row, j = col
+        while(j >= 0 ) {
+            if(board[i][j] == 'Q') {
+                return true
+            }
+            j--
+        }
+        
+        i = row
+        j = col
+        while( i >=0 && j >= 0) {
+            if(board[i][j] == 'Q') {
+                return true
+            }
+            i--
+            j--
+        }
+        
+        i = row
+        j = col
+        while( i < n && j >= 0) {
+            if(board[i][j] == 'Q') {
+                return true
+            }
+            i++
+            j--
+        }
+        return false
+    }
+    
+    let ways = 0
+    
+    let f = function(col, board) {
+        
+        if(col == n) {
+            ways++
+            return
+        }
+        for(let row = 0; row < n; row++) {
+            if(canAttack(row,col) == false) {
+                board[row][col] = 'Q'
+                f(col+1, board)
+                board[row][col] = '.'
+            }
+        }
+    }
+    
+    f(0, board)
+    return ways
+};
