6fd5dd3d087fa3c01beb947872b11de6a2a8a7ca
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-29 10:22:14 +0530

    Time: 1850 ms (13.43%), Space: 49.9 MB (37.31%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.js
@@ -0,0 +1,49 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var maxProduct = function(words) {
+    
+    let masterMap = new Map()
+    //set freq of all words
+    for(let word of words) {
+        //skip duplicate words
+        if(masterMap.has(word)) {
+            continue
+        }
+        let map = new Map()
+        masterMap.set(word, map)
+        for(let ch of word) {
+            map.set(ch, (map.get(ch) || 0) + 1)
+        }
+    }
+    
+    let ans = 0
+    let n = words.length
+    
+    
+    let checkCommonChars = function(word1, word2) {
+        let map1 = masterMap.get(word1)
+        let map2 = masterMap.get(word2)
+        for(let ch of word1) {
+            if(map2.get(ch) >= 1) {
+                return true
+            }
+        }
+        for(let ch of word2) {
+            if(map1.get(ch) >= 1) {
+                return true
+            }
+        }
+        return false
+    }
+    for(let i = 0; i < n; i++) {
+        for(let j = i + 1; j < n; j++) {
+            if(checkCommonChars(words[i], words[j]) == false) {
+                ans = Math.max(ans, words[i].length * words[j].length)
+            }
+        }
+    }
+    return ans
+    
+};
