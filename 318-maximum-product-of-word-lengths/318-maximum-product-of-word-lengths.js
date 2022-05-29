8287911fbf106e31a271120d599e39d0bc1fa053
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-05-29 10:35:57 +0530

    Time: 195 ms (74.63%), Space: 45.1 MB (89.55%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.js
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.js
@@ -4,48 +4,33 @@
  */
 var maxProduct = function(words) {
     
-    let lengthMap = new Map()
-    let masterMap = new Map()
-    //set freq of all words
+    let product = 0
+    let n = words.length
+    let bitMap = new Map()
     for(let word of words) {
-        //skip duplicate words
-        if(masterMap.has(word)) {
+        //avoid duplicates
+        if(bitMap.has(word)) {
             continue
         }
-        lengthMap.set(word, word.length)
-        let map = new Map()
-        masterMap.set(word, map)
+        bitMap.set(word, 0)
+        let value = 0
         for(let ch of word) {
-            map.set(ch, (map.get(ch) || 0) + 1)
+            let ascii = ch.charCodeAt()
+            value = value | 1 << (ascii - 97)
         }
+        bitMap.set(word, value)
     }
     
-    let ans = 0
-    let n = words.length
-    
-    
-    let checkCommonChars = function(word1, word2) {
-        let map1 = masterMap.get(word1)
-        let map2 = masterMap.get(word2)
-        for(let ch of word1) {
-            if(map2.get(ch) >= 1) {
-                return true
-            }
-        }
-        for(let ch of word2) {
-            if(map1.get(ch) >= 1) {
-                return true
-            }
-        }
-        return false
-    }
     for(let i = 0; i < n; i++) {
         for(let j = i + 1; j < n; j++) {
-            if(checkCommonChars(words[i], words[j]) == false) {
-                ans = Math.max(ans, lengthMap.get(words[i]) * lengthMap.get(words[j]))
+            //abc => 111 , d => 1000
+            let bitset_i = bitMap.get(words[i])
+            let bitset_j = bitMap.get(words[j])
+            if( (bitset_i & bitset_j) == 0) {
+                product = Math.max(product, words[i].length * words[j].length)
             }
         }
     }
-    return ans
+    return product
     
 };
