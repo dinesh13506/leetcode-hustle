a9efb88b1d04a55a28dfcbebf4ac915e52f31866
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-06-02 08:04:24 +0530

    Time: 83 ms (95.06%), Space: 44.2 MB (81.48%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.js
+++ 1314-matrix-block-sum/1314-matrix-block-sum.js
@@ -6,6 +6,33 @@
 var matrixBlockSum = function(mat, k) {
     
     let m = mat.length, n = mat[0].length
+    let dp = new Array(m+1)
+    for(let i = 0; i <= m; i++) {
+        dp[i] = new Array(n+1)
+        dp[i].fill(0)
+    }
+    
+    //row wise
+    for(let i = 1; i <= m; i++) {
+        let sum = 0
+        for(let j = 1; j <=n; j++) {
+            sum = sum + mat[i-1][j-1]
+            dp[i][j] = sum
+        }
+        
+    }
+    //console.log(dp)
+    
+    //col wise
+    for(let j = 1; j <= n; j++) {
+        let sum = 0
+        for(let i = 1; i <= m; i++) {
+            sum = sum + dp[i][j]
+            dp[i][j] = sum
+        }
+    }
+    
+    //console.log(dp)
     let answer = new Array(m)
     for(let i = 0; i <m ;i++) {
         answer[i] = new Array(n)
@@ -17,12 +44,9 @@ var matrixBlockSum = function(mat, k) {
             let cmin = Math.max(0, j-k)
             let cmax = Math.min(n-1, j+k)
             let sum = 0
-            for(let r = rmin; r<= rmax; r++) {
-                for(let c = cmin; c <= cmax; c++) {
-                    sum += mat[r][c]
-                }
-            }
-            answer[i][j] = sum
+            
+            rmin++, rmax++, cmin++, cmax++
+            answer[i][j] = dp[rmax][cmax] - dp[rmax][cmin-1] - dp[rmin-1][cmax] + dp[rmin-1][cmin-1]
         }
     }
     return answer
