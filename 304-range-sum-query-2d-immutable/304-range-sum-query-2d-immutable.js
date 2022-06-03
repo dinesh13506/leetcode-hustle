011ff90821f6c7677c54e7ab64d5f4deee073899
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-06-03 23:12:06 +0530

    Time: 817 ms (31.97%), Space: 85.5 MB (13.94%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.js
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.js
@@ -3,6 +3,33 @@
  */
 var NumMatrix = function(matrix) {
     this.matrix = matrix
+    let m = matrix.length
+    let n = matrix[0].length
+    
+    this.prefix = new Array(m)
+    for(let i = 0; i < m; i++) {
+        this.prefix[i] = new Array(n)
+        this.prefix[i].fill(0)
+    }
+    //row wise
+    for(let i = 0; i < m; i++) {
+        let sum = 0
+        for(let j= 0; j < n; j++) {
+            sum = sum + matrix[i][j]
+            this.prefix[i][j] = sum
+        }
+    }
+    
+    //col wise
+    for(let j= 0; j < n; j++) {
+        let sum = 0
+        for(let i = 0; i < m; i++) {
+            sum = sum + this.prefix[i][j]
+            this.prefix[i][j] = sum
+        }
+    }
+            
+    //console.log(this.prefix)
 };
 
 /** 
@@ -14,13 +41,15 @@ var NumMatrix = function(matrix) {
  */
 NumMatrix.prototype.sumRegion = function(row1, col1, row2, col2) {
    
-    let sum = 0
-    for(let i = row1; i <= row2; i++) {
-        for(let j = col1; j <= col2; j++) {
-            sum += this.matrix[i][j]
-        }
+    //console.log(this.prefix[row2][col2], this.prefix[row2][col1], this.prefix[row1][col2])
+    let sum = this.prefix[row2][col2] - this.prefix[row2][col1] - this.prefix[row1][col2] + this.prefix[row1][col1]
+    for(let j = col1; j <= col2; j++) {
+        sum = sum + this.matrix[row1][j]
     }
-    return sum
+    for(let i = row1; i <= row2; i++) {
+         sum = sum + this.matrix[i][col1]
+    }
+    return sum - this.matrix[row1][col1]
 };
 
 /** 
