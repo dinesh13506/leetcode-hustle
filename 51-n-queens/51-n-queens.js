7bd37583bbbf44f2fdb565283cd838628d339619
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-06-04 09:32:41 +0530

    Time: 79 ms (91.47%), Space: 46 MB (30.09%) - LeetHub

--- 51-n-queens/51-n-queens.js
+++ 51-n-queens/51-n-queens.js
@@ -4,58 +4,11 @@
  */
 var solveNQueens = function(n) {
    //return solveNQueensBySimple(n)
-    return solveNQueensByHashing(n)
+    return solveNQueensBySimple(n)
 };
 
 
-let solveNQueensByHashing = function (n) {
-    let result = []
-    let board = new Array(n)
-    for(let i = 0; i < n; i++) {
-        board[i] = new Array(n)
-        board[i].fill('.')
-    }
-    
-    let leftRow = new Array(n)
-    leftRow.fill(0)
-    let upperDiagonal = new Array(2*n-1)
-    upperDiagonal.fill(0)
-    let lowerDiagonal = new Array(2*n - 1)
-    lowerDiagonal.fill(0)
-    
-    
-    
-    let f = function(col, board) {
-        
-        if(col == n) {
-            //console.log(board.slice())
-            let temp = []
-            for(let i = 0; i < n; i++) {
-                temp.push(board[i].join(''))
-            }
-            result.push(temp)
-            return
-        }
-        
-        for(let row = 0; row < n; row++) {
-            if(leftRow[row] == 0 && upperDiagonal[row+col] == 0 && lowerDiagonal[col-row + n - 1] == 0) {
-                leftRow[row] = 1
-                upperDiagonal[row+col] = 1
-                lowerDiagonal[col-row + n - 1] = 1
-                board[row][col] = 'Q'
-                f(col+1, board)
-                board[row][col] = '.'
-                leftRow[row] = 0
-                upperDiagonal[row+col] = 0
-                lowerDiagonal[col-row + n - 1] = 0
-            }
-        }
-    }
-    
-    
-    f(0, board)
-    return result
-}
+
 
 let solveNQueensBySimple = function (n) {
         let result = []
