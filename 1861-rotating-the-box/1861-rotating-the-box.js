8cef0cd4539b1dfc42cfca9ca8f1e272598c7795
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-06-10 00:27:04 +0530

    Time: 620 ms (18.71%), Space: 85.3 MB (71.94%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.js
@@ -0,0 +1,47 @@
+/**
+ * @param {character[][]} box
+ * @return {character[][]}
+ */
+var rotateTheBox = function(box) {
+    
+    let m = box.length
+    let n = box[0].length
+    
+    let output = new Array(n)
+    for(let i = 0; i < n; i++) {
+        output[i] = new Array(m)
+        output[i].fill('.')
+    }
+    
+    for(let i = 0; i < m; i++) {
+        let count = 0
+        for(let j = 0; j < n; j++) {
+            if(box[i][j] == '#') {
+                count++
+            } else if(box[i][j] == '*') {
+                
+                let row = j
+                let col = m - i - 1
+                //console.log("obst => " ,row,col)
+                output[row][col] = '*'
+                row--
+                while(row >= 0 && count > 0) {
+                    //console.log(row,col)
+                    output[row][col] = '#'
+                    row--
+                    count--
+                }
+            }
+        }
+        if(count > 0) {
+            let col = m - i - 1
+            let row = n-1
+            while(row >=0 && count > 0) {
+                output[row][col] = '#'
+                row--
+                count--
+            }
+        }
+    }
+    return output
+};
