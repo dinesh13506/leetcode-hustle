15ad9a7ee9b704d3117e062b5eaa42a554387551
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-06-13 08:57:23 +0530

    Time: 80 ms (64.89%), Space: 44.4 MB (20.49%) - LeetHub

--- 120-triangle/120-triangle.js
+++ 120-triangle/120-triangle.js
@@ -2,7 +2,7 @@
  * @param {number[][]} triangle
  * @return {number}
  */
-var minimumTotal = function(triangle) {
+var minimumTotalMemo = function(triangle) {
     
     let m = triangle.length
     let n = triangle[m-1].length
@@ -31,6 +31,30 @@ var minimumTotal = function(triangle) {
    
     for(let j = 0; j < triangle[m-1].length; j++) {
         minpathsum = Math.min(minpathsum, dp(m-1, j))
+    }
+    return minpathsum
+};
+
+
+var minimumTotal = function(triangle) {
+    
+    let m = triangle.length
+    let n = triangle[m-1].length
+    let dp = new Array(m)
+    for(let i = 0; i < m; i++) {
+        dp[i] = new Array(i)
+        dp[i].fill(0)
+    }
+    dp[0] = [triangle[0][0]]
+    for(let i = 1; i < m; i++) {
+        for(let j = 0; j <= i; j++) {
+            dp[i][j] = triangle[i][j] + Math.min( (dp[i-1][j] === undefined ? Infinity : dp[i-1][j]), (dp[i-1][j-1] === undefined ? Infinity : dp[i-1][j-1]))
+        }
+    }
+    let minpathsum = Infinity
+   
+    for(let j = 0; j < triangle[m-1].length; j++) {
+        minpathsum = Math.min(minpathsum, dp[m-1][j])
     }
     return minpathsum
 };
