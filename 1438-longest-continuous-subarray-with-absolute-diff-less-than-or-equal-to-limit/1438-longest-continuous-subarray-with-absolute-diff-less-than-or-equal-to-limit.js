561ed8f8ac5b6d693c8ee6d92bc3bcc6e1a8a236
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-06-13 17:16:26 +0530

    Time: 142 ms (63.18%), Space: 55.3 MB (41.29%) - LeetHub

---
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @param {number} limit
+ * @return {number}
+ */
+var longestSubarray = function(nums, limit) {
+    
+    let n = nums.length
+    let left = 0, min = Infinity, max = -Infinity
+    let longest = 0
+    let maxdeq = []
+    let mindeq = []
+    
+    for(let right = 0; right < n; right++) {
+       
+        while(maxdeq.length && maxdeq[maxdeq.length - 1] < nums[right]) {
+            maxdeq.pop()
+        }
+        maxdeq.push(nums[right])
+        while(mindeq.length && mindeq[mindeq.length-1] > nums[right]) {
+            mindeq.pop()
+        }
+        mindeq.push(nums[right])
+        
+        if(left <= right && (maxdeq[0] - mindeq[0]) > limit) {
+            if(nums[left] == maxdeq[0]) {
+                maxdeq.shift()
+            }
+            if(nums[left] == mindeq[0]) {
+                mindeq.shift()
+            }
+            left++
+        } else {
+            longest = Math.max(longest, right - left + 1)
+        }
+    }
+    return longest
+};
