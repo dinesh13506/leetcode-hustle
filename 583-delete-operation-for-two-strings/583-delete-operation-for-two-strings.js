a7f728b3e119e5c709a2420dcdc70db7ebd92a04
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-06-14 08:13:48 +0530

    Time: 121 ms (65.81%), Space: 48.7 MB (66.67%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.js
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.js
@@ -5,6 +5,14 @@
  */
 var minDistance = function(word1, word2) {
     
+    //return minDistanceMemo(word1, word2)
+    return minDistanceBottomUp(word1, word2)
+};
+
+
+
+var minDistanceMemo = function(word1, word2) {
+    
     let m = word1.length, n = word2.length
     let memo = new Array(m+1)
     for(let i = 0; i <=m; i++) {
@@ -29,5 +37,26 @@ var minDistance = function(word1, word2) {
     let lcs = dp(m,n)
     let ops = (m-lcs) + (n-lcs)
     return ops
+};
+
+var minDistanceBottomUp = function(word1, word2) {
     
+    let m = word1.length, n = word2.length
+    let dp = new Array(m+1)
+    for(let i = 0; i <=m; i++) {
+        dp[i] = new Array(n+1)
+        dp[i].fill(0)
+    }
+    for(let i = 1; i <= m; i++) {
+        for(let j =1; j <=n; j++) {
+            if(word1[i-1] == word2[j-1]) {
+                dp[i][j] = 1 + dp[i-1][j-1]
+            } else {
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])
+            }
+        }
+    }
+    let lcs = dp[m][n]
+    let ops = (m-lcs) + (n-lcs)
+    return ops
 };
