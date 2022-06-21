bb346312528cc0e48d63bad3ed5c0b4dd3d57c0e
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-06-21 09:18:25 +0530

    Time: 205 ms (43.08%), Space: 52.1 MB (98.46%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.js
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.js
@@ -1,102 +1,82 @@
-const minHeap = () => {
-    const data = [];
-    
-    const size = () => {
-      return data.length;  
-    };
-    
-    const bottomUp = (index) => {
-        
-        if(size() <= 1) return;
-        
-        const parenti = Math.floor((index - 1) / 2);
-        if(data[parenti] > data[index]){
-            [data[parenti], data[index]] = [data[index], data[parenti]];
-            bottomUp(parenti);   
-        }
-    };
-    
-    const topDown = (index) => {
-       
-        const n = size();
-        
-        let smaller = index;
-        let lefti = 2*index + 1;
-        let righti = lefti + 1;
-        
-        if(lefti < n && data[lefti] < data[smaller]){
-            smaller = lefti;
-        }
-        
-        if(righti < n && data[righti] < data[smaller]){
-            smaller = righti;
-        }
-        
-        if(index !== smaller){
-            [data[index], data[smaller]] = [data[smaller], data[index]];
-            topDown(smaller);
-        }
-    };
-    
-    const add = (val) => {
-        data.push(val);
-        bottomUp(data.length - 1);
-    };
-    
-    const poll = () => {
-        if(size() <= 0) return null;
-        
-        const min = data[0];
-        
-        data[0] = data[size() - 1];
-        data.pop();
-        topDown(0);
-        
-        return min;
-    };
-    
-    const peek = () => {
-      return size() > 0 ? data[0] : null;  
-    };
-    
-    return {
-        add,
-        poll,
-        peek,
-        size,
+class MinHeap {
+    constructor() {
+        this.arr = []
+        this.length = 0
     }
-};
+    swap(i,j) {
+        this.arr[i] = this.arr[i] ^ this.arr[j]
+        this.arr[j] = this.arr[j] ^ this.arr[i]
+        this.arr[i] = this.arr[i] ^ this.arr[j]
+    }
+    
+    bottomUp(i) {
+        if(this.length <=1 ) {
+            return
+        }
+        let parent = Math.floor((i-1)/2)
+        if(this.arr[i] < this.arr[parent]) {
+            this.swap(i,parent)
+            this.bottomUp(parent)
+        }
+    }
+    heapify(i) {
+        let smallest = i
+        let left = 2 * i  + 1
+        let right = left + 1
+        if(left < this.length && this.arr[left] < this.arr[smallest]) {
+            smallest = left
+        }
+        if(right < this.length  && this.arr[right] < this.arr[smallest]) {
+            smallest = right
+        }
+        if(smallest != i) {
+            this.swap(i,smallest)
+            this.heapify(smallest)
+        }
+    }
+    
+    insert(val) {
+        this.arr.push(val)
+        this.length++
+        this.bottomUp(this.length-1)
+    }
+    
+    extractMin() {
+        let min = this.arr[0]
+        //this.swap(0, this.length-1)
+        this.arr[0] = this.arr[this.length-1]
+        this.arr.pop()
+        this.length--
+        this.heapify(0)
+        return min
+    }
+}
+
 
 /**
- * Time: O(NlogN)
- * Space: O(N)
+ * @param {number[]} heights
+ * @param {number} bricks
+ * @param {number} ladders
+ * @return {number}
  */
 var furthestBuilding = function(heights, bricks, ladders) {
-    const n = heights.length;
     
-    const heap = minHeap();
-    
-    for(let i=0; i<n-1; i++){
-        
-        const currentHeight = heights[i];
-        const nextHeight = heights[i + 1];
-        
-        // it's a jump down
-        if(currentHeight >= nextHeight) continue;
-           
-        const diff = nextHeight - currentHeight; 
-        heap.add(diff);
-        
-        if(heap.size() <= ladders) continue;
-        
-        // use the minimal number of bricks
-        const minDifference = heap.peek();
-        bricks -= minDifference;
-        heap.poll();
-        
-        if(bricks < 0) return i;
+    let n = heights.length
+    let minheap = new MinHeap()
+    for(let i = 0; i < n-1; i++) {
+        let curr = heights[i]
+        let next = heights[i+1]
+        let jump = next - curr
+        if(jump > 0 ) {
+            minheap.insert(jump)
+        }
+        if(minheap.arr.length > ladders) {
+            let min = minheap.extractMin()
+            bricks = bricks - min
+        }
+        if(bricks < 0) {
+            return i
+        }
     }
-    
-    // we can reach all heights
-    return n - 1;
+    return n-1
 };
