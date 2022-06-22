dc5c004963fbeb330b3b89f332fbcc70f824a7f9
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-06-22 08:44:54 +0530

    Time: 95 ms (32.86%), Space: 41.5 MB (93.07%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.js
+++ 392-is-subsequence/392-is-subsequence.js
@@ -4,12 +4,9 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
-    
-    if(s.length > t.length) {
-        return false
-    }
+   
     let i = 0, j = 0
-    while(i < s.length && j < t.length) {
+    while( i < s.length && j < t.length) {
         if(s[i] == t[j]) {
             i++
             j++
@@ -20,7 +17,6 @@ var isSubsequence = function(s, t) {
     
     if(i >= s.length) {
         return true
-    } else {
-        return false
     }
+    return false
 };
