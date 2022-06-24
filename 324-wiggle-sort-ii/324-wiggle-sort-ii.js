d1235ade08bc404a7b22c4cf4d9a6294d1fc326a
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-06-25 00:34:56 +0530

    Time: 185 ms (11.36%), Space: 47.5 MB (61.36%) - LeetHub

---
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var wiggleSort = function(nums) {
+    
+    let copy = nums.slice()
+    copy.sort((a,b) => {
+        return a - b
+    })
+    
+    let j = 0, k = parseInt((nums.length-1)/2) + 1
+    for(let i = nums.length - 1; i >=0; i--) {
+        nums[i] = copy[i&1 ? k++ : j++]
+    }
+};
