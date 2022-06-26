b37a89c654a155f73418f0d67f582bfa36f365bd
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-06-26 11:28:59 +0530

    Time: 110 ms (40.67%), Space: 47.6 MB (83.49%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.js
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.js
@@ -5,25 +5,19 @@
  */
 var maxScore = function(cardPoints, k) {
     
-    let windowsize = cardPoints.length - k // min sum window - TBD
-    
+    let windowsize = cardPoints.length - k // find subarray of length windowsize with min sum
+    let totalsum = 0
     let sum = 0
     for(let i = 0; i < windowsize; i++) {
         sum = sum + cardPoints[i]
+        totalsum += cardPoints[i]
     }
-    
     let left = 0, minsum = sum
     for(let right = windowsize; right < cardPoints.length; right++) {
         sum = sum + cardPoints[right] - cardPoints[left]
         left++
         minsum = Math.min(sum, minsum)
-        
+        totalsum += cardPoints[right]
     }
-    
-    let totalsum = 0
-    for(let cp of cardPoints) {
-        totalsum += cp
-    }
-    
     return totalsum - minsum
 };
