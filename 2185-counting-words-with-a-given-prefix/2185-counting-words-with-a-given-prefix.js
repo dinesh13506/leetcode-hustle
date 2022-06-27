9bbbc1f7014b369d40b55a992d144db9930a8e06
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-06-27 23:15:42 +0530

    Time: 166 ms (5.38%), Space: 57.6 MB (6.36%) - LeetHub

---
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.js
@@ -0,0 +1,54 @@
+/**
+ * @param {string[]} words
+ * @param {string} pref
+ * @return {number}
+ */
+
+class TrieNode {
+    constructor() {
+        this.children = new Array(26)
+        this.count = 0
+        this.children.fill(null)
+        this.isend = false
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode()
+    }
+    
+    insert(word) {
+        let curr = this.root
+        for(let ch of word) {
+            let index = ch.charCodeAt() - 'a'.charCodeAt()
+            if(curr[index] == null) {
+                curr[index] = new TrieNode()
+            }
+            curr[index].count++
+            curr = curr[index]
+        }
+        curr.isend = true
+    }
+    
+    search(prefix) {
+        let curr = this.root
+        for(let ch of prefix) {
+            let index = ch.charCodeAt() - 'a'.charCodeAt()
+            if(curr[index] == null) {
+                return 0
+            }
+            curr = curr[index]
+        }
+        return curr.count
+    }
+ }
+var prefixCount = function(words, pref) {
+    
+    let trie = new Trie()
+    for(let word of words) {
+        trie.insert(word)
+    }
+    
+    return trie.search(pref)
+};
