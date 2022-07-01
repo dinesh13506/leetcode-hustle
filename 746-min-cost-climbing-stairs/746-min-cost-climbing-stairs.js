f1410077a283d49dae1fcce7358effd775cc6c50
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-01 10:01:11 +0530

    Time: 68 ms (92.56%), Space: 43.1 MB (66.73%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -2,7 +2,7 @@
  * @param {number[]} cost
  * @return {number}
  */
-var minCostClimbingStairs = function(cost) {
+var minCostClimbingStairsMemo = function(cost) {
     
     let memo = new Array(cost.length + 1)
     memo.fill(-1)
@@ -19,4 +19,17 @@ var minCostClimbingStairs = function(cost) {
     
     let top = cost.length
     return dp(top)
+};
+
+
+var minCostClimbingStairs = function(cost) {
+    
+    let top = cost.length
+    let dp = new Array(top+1)
+    dp[0] = 0
+    dp[1] = 0
+    for(let i = 2; i <= top; i++) {
+        dp[i] = Math.min(cost[i-1] + dp[i-1], cost[i-2] + dp[i-2])
+    }
+    return dp[top]
 };
