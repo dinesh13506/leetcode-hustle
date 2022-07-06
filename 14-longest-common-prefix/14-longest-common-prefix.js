3e8b650812f726da9f1544c916b6dcacfa35961f
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-06 17:51:48 +0530

    Time: 83 ms (66.93%), Space: 42.1 MB (88.63%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -4,25 +4,11 @@
  */
 var longestCommonPrefix = function(strs) {
     
-    let map = new Map()
-    let n = strs.length
-    for(let str of strs) {
-        let prefix = ""
-        for(let ch of str) {
-            prefix += ch
-            if(map.has(prefix) == false) {
-                map.set(prefix, 0)
-            } 
-            map.set(prefix, map.get(prefix) + 1)
+    let prefix = strs[0]
+    for(let i = 1; i < strs.length; i++) {
+        while(strs[i].indexOf(prefix) != 0) {
+            prefix = prefix.substring(0, prefix.length - 1)
         }
     }
-    let ans = ""
-    for(let str of map.keys()) {
-        if(map.get(str) >= n) {
-            if(ans.length < str.length) {
-                ans = str
-            }
-        }
-    }
-    return ans
+    return prefix
 };
