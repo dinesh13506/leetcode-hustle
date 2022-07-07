8e3640702742bb55ba7a8d0bc064a123890392bb
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-07 11:28:22 +0530

    Time: 218 ms (49.34%), Space: 69.5 MB (79.50%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.js
+++ 234-palindrome-linked-list/234-palindrome-linked-list.js
@@ -11,18 +11,39 @@
  */
 var isPalindrome = function(head) {
     
-    let p = head, stack = []
-    while(p) {
-        stack.push(p)
-        p = p.next
+    let findMid = (node) => {
+        let slow = node
+        let fast = node
+        while(fast.next && fast.next.next) {
+            slow = slow.next
+            fast = fast.next.next
+        }
+        return slow
     }
-    p = head
-    while(p) {
-        let top = stack.pop()
-        if(p.val != top.val) {
+    
+    let reverse = (node) => {
+        let p = node
+        let prev = null
+        while(p) {
+            let temp = p.next
+            p.next = prev
+            prev = p
+            p = temp
+        }
+        return prev
+    }
+    
+    let mid = findMid(head)
+    let p2 = reverse(mid.next)
+    
+    let p1 = head
+    while(p2) {
+        if(p1.val != p2.val) {
             return false
         }
-        p = p.next
+        p1 = p1.next
+        p2 = p2.next
     }
+    mid.next = reverse(p2)
     return true
 };
