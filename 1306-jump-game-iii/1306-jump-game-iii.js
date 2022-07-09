d74612597080390fb96478ac5bb994f6fbae80c1
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-09 14:23:14 +0530

    Time: 242 ms (7.98%), Space: 63.3 MB (7.14%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.js
+++ 1306-jump-game-iii/1306-jump-game-iii.js
@@ -3,73 +3,30 @@
  * @param {number} start
  * @return {boolean}
  */
-
-class Node {
-    constructor(index) {
-        this.index = index
-        this.next = null
-    }
-}
-
-class MyQueue {
-    constructor() {
-        this.front = null
-        this.rear = null
-    }
-    
-    isEmpty() {
-        if(this.front == null && this.rear == null) {
-            return true
-        }
-        return false
-    }
-    
-    enque(index) {
-        let node = new Node(index)
-        if(this.isEmpty()) {
-            this.front = node
-            this.rear = node
-        } else {
-            this.rear.next = node
-            this.rear = this.rear.next
-        }
-    }
-    
-    deque() {
-        if(this.isEmpty()) {
-            return
-        }
-        let node = this.front
-        this.front = this.front.next
-        if(this.front == null) {
-            this.rear = null
-        }
-        return node
-    }
-}
 var canReach = function(arr, start) {
-    let q = new MyQueue()
+    
     let n = arr.length
     let visited = new Array(n)
     visited.fill(false)
     
-    q.enque(start)
-    visited[start] = true
-    while(q.isEmpty() == false) {
-        let index = q.deque().index
-        if(arr[index] == 0) {
-            return true
-        }
-        let neighbors  = [index + arr[index], index - arr[index]]
-        for(let neighbor of neighbors) {
-            if(neighbor < 0 || neighbor >= n) {
+    let dfs = function(index) {
+        if(index < 0 || index >=n ) return false
+        if(arr[index] == 0) return true
+        visited[index] = true
+        
+        let neigbors = [index + arr[index], index - arr[index]]
+        let ans = false
+        for(let neigbor of neigbors) {
+            if(neigbor < 0 || neigbor >=n) {
                 continue
             }
-            if(visited[neighbor] == false) {
-                visited[neighbor] = true
-                q.enque(neighbor)
+            if(visited[neigbor]) {
+                continue
             }
+            ans = ans | dfs(neigbor)
         }
+        return ans
+        
     }
-    return false
+    return dfs(start)
 };
