b1ae2beaf1875511ae111f6e445092e5a6a65e01
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-12 10:06:02 +0530

    Time: 77 ms (71.16%), Space: 42.2 MB (42.59%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -5,64 +5,20 @@
  */
 var search = function(nums, target) {
     
-    let n = nums.length
-    let start = 0, end = n - 1
-    let minElemIdx = 0
-    if(nums[start] <= nums[end]) {
-        minElemIdx = 0
-    } else {
-        while(start <= end) {
-            let mid = start + parseInt((end - start)/2)
-
-            let left = (mid - 1 + n) % n
-            let right = (mid + 1) % n
-
-            if(nums[mid] > nums[right]) {
-                minElemIdx = right
-                break
-            } else if(nums[mid] < nums[left]) {
-                minElemIdx = mid
-                break
-            }
-            
-            if(nums[start] < nums[mid]) {
-                start = mid + 1
-            } else if(nums[mid] < nums[end]) {
-                end = mid -1
-            }
+    let start = 0, end = nums.length - 1
+    while(start <= end) {
+        let mid = start + parseInt((end - start)/2)
+        if(nums[mid] == target) {
+            return mid
         }
-    }
-    
-    //console.log("minElemIdx => ",minElemIdx)
-    
-    let bs = function(start, end) {
-        if(start >=0 && end <= n-1) {
-            while(start <= end) {
-                let mid = start + parseInt((end - start)/2)
-                if(nums[mid] == target) {
-                    return mid
-                }
-                else if(target < nums[mid]) {
-                    end = mid -1
-                } else {
-                    start = mid + 1
-                }
-            }
-            return -1
-        } else {
-            return -1
+        else if(nums[start] <= nums[mid]) {
+            if(nums[start] <= target && target < nums[mid]) end = mid - 1
+            else start = mid + 1
         }
-    }
-    if(target == nums[minElemIdx]) {
-        return minElemIdx
-    }
-    let leftsearch = bs(0, minElemIdx - 1)
-    if(leftsearch != -1) {
-        return leftsearch
-    }
-    let rightsearch = bs(minElemIdx + 1, n-1)
-    if(rightsearch != -1) {
-        return rightsearch
+        else {
+            if(nums[mid] < target && target <= nums[end]) start = mid + 1
+            else end = mid - 1
+        }
     }
     return -1
 };
