1c262c3176a93a3b5f9467a123d18d890a41fcb7
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-15 10:07:27 +0530

    Time: 117 ms (59.65%), Space: 48.8 MB (23.12%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.js
@@ -0,0 +1,112 @@
+class Vertex {
+    constructor(value) {
+        this.data = value
+        this.next = null
+    }
+}
+
+
+class MyQueue {
+    constructor() {
+        this.length = 0
+        this.front = null
+        this.rear = null
+    }
+
+    getfront() {
+        return this.front
+    }
+
+    enq(value) {
+        let vertex = new Vertex(value)
+        if(this.length == 0) {
+            this.front = vertex
+            this.rear = vertex
+        } else {
+            this.rear.next = vertex
+            this.rear = this.rear.next
+        }
+        this.length++
+    }
+
+    deq() {
+        if(this.length <=1) {
+            this.front = null
+            this.rear = null
+        } else {
+            this.front = this.front.next
+        }
+        this.length--
+    }
+
+    size() {
+        return this.length
+    }
+}
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function(grid) {
+    let m = grid.length
+    let n = grid[0].length
+    
+    let visited = []
+    for(let i = 0; i < m; i++) {
+        visited[i] = new Array()
+        for( let j = 0; j < n; j++) {
+            visited[i][j] = false
+        }
+    }
+    
+    let count = 0
+    for(let i = 0; i < m; i++) {
+        for( let j = 0; j < n; j++) {
+            if(visited[i][j] == false && grid[i][j] == 1) {
+                count = Math.max(count , bfs([i,j],grid,visited,m,n))
+            }
+        }
+    }
+    return count
+};
+
+let bfs = function(location,grid,visited,m,n) {
+    let ans = 0
+    let q = new MyQueue()
+    q.enq(location)
+    visited[location[0]][location[1]] = true
+    ans++
+    
+    while(q.size() >0) {
+        
+        let loc = q.getfront().data
+        q.deq()
+        let i = loc[0]
+        let j = loc[1]
+        if( (i-1)>=0 && grid[i-1][j] == 1 && visited[i-1][j] == false) {
+            q.enq([i-1,j])
+            visited[i-1][j] = true
+            ans++
+        }
+        if((i+1) < m && grid[i+1][j] == 1 && visited[i+1][j] == false) {
+            q.enq([i+1,j])
+            visited[i+1][j] = true
+            ans++
+        }
+        
+        if((j-1) >= 0 && grid[i][j-1] == 1 && visited[i][j-1] == false) {
+            q.enq([i,j-1])
+            visited[i][j-1] = true
+            ans++
+        }
+        
+        if((j+1) < n && grid[i][j+1] == 1 && visited[i][j+1] == false) {
+            q.enq([i,j+1])
+            visited[i][j+1] = true
+            ans++
+        }
+    }
+    
+    return ans
+}
