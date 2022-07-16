16546b5a4f37264b03f134ff62883b873c00fa9f
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-16 17:40:59 +0530

    Time: 299 ms (21.65%), Space: 57.7 MB (6.09%) - LeetHub

--- 322-coin-change/322-coin-change.js
+++ 322-coin-change/322-coin-change.js
@@ -3,112 +3,42 @@
  * @param {number} amount
  * @return {number}
  */
-var coinChangeMemo = function(coins, amount) {
-    
-    let n = coins.length
-    let memo = new Array(n)
-    for(let i = 0; i < n; i++) {
-        memo[i] = new Array(amount+1)
-        memo[i].fill(-1)
-    }
-    let dp = function(i, amount) {
-        if(amount == 0) {
-            return 0
-        }
-        if(i === 0) {
-            if(amount % coins[0] == 0) {
-                return amount / coins[0]
-            } else {
-                return Infinity
-            }
-        }
-        
-        if(memo[i][amount] != -1) {
-            return memo[i][amount] 
-        }
-        
-        let take = Infinity
-        if(amount >= coins[i]) {
-            take = 1 + dp(i, amount-coins[i])
-        }
-        let nottake = 0 + dp(i-1, amount)
-        memo[i][amount]  =  Math.min(take, nottake)
-        return memo[i][amount]
-    }
-    
-    let count =  dp(n-1,amount)
-    return count == Infinity ? -1 : count
-};
-
-
-var coinChangeTab = function(coins, amount) {
-    
-    let n = coins.length
-    let dp = new Array(n)
-    for(let i = 0; i < n; i++) {
-        dp[i] = new Array(amount+1)
-        dp[i].fill(0)
-    }
-    
-    //if amount is 0
-    for(let i = 0; i < n; i++) {
-        dp[i][0] = 0
-    }
-    //second base case
-    for(let amt = 1; amt <= amount; amt++) {
-        if(amt % coins[0] == 0) {
-            dp[0][amt] = amt / coins[0]
-        } else {
-            dp[0][amt] =  Infinity
-        }
-    }
-    for(let i = 1; i < n; i++) {
-        for(let amt = 1; amt <= amount; amt++) {
-            let take = Infinity
-            if(amt >= coins[i]) {
-                take = 1 + dp[i][amt-coins[i]]
-            }
-            let nottake = 0 + dp[i-1][amt]
-            
-            dp[i][amt]  =  Math.min(take, nottake)
-        }
-    }
-    return ( dp[n-1][amount] === Infinity ? -1 : dp[n-1][amount])
-};
-
-
-
 var coinChange = function(coins, amount) {
     
     let n = coins.length
-    let prev = new Array(amount+1)
-    prev.fill(0)
     
-    //if amount is 0
+    let memo = new Array(n)
     for(let i = 0; i < n; i++) {
-        prev[0] = 0
+        memo[i] = new Array(amount + 1)
+        memo[i].fill(-1)
     }
-    //second base case
-    for(let amt = 1; amt <= amount; amt++) {
-        if(amt % coins[0] == 0) {
-            prev[amt] = amt / coins[0]
-        } else {
-            prev[amt] =  Infinity
+    let dp = (index , amount) => {
+        if(index < 0) {
+            return Infinity
         }
-    }
-    for(let i = 1; i < n; i++) {
-        let curr = new Array(amount+1)
-        curr.fill(0)
-        for(let amt = 1; amt <= amount; amt++) {
-            let take = Infinity
-            if(amt >= coins[i]) {
-                take = 1 + curr[amt-coins[i]]
-            }
-            let nottake = 0 + prev[amt]
-            
-            curr[amt]  =  Math.min(take, nottake)
+        if(amount < 0) {
+            return Infinity
         }
-        prev = curr
+        if(amount == 0) {
+            return 0
+        }
+        
+        if(memo[index][amount] != -1) {
+            return memo[index][amount]
+        }
+        
+        let take = Infinity
+        if(coins[index] <= amount) {
+            take = 1 + dp(index, amount - coins[index])
+        }
+        
+        let nottake = dp(index - 1, amount)
+        
+        memo[index][amount] =  Math.min(take, nottake)
+        return memo[index][amount]
     }
-    return ( prev[amount] === Infinity ? -1 : prev[amount])
+    
+    let ans = dp(n-1, amount)
+    if(ans == Infinity) return -1
+    return ans
 };
