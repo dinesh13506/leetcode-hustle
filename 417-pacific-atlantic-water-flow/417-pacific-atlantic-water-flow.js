49c79c7532d94be3df0199fdbce2ccda8ebfc1b8
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-16 14:25:45 +0530

    Time: 217 ms (48.82%), Space: 51 MB (71.37%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.js
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.js
@@ -20,43 +20,36 @@ var pacificAtlantic = function(heights) {
     }
     
     const dirs = [[1,0],[-1,0],[0,1],[0,-1]]
-    let pacificQueue = new Queue()
-    let atlanticQueue = new Queue()
-    for(let r = 0; r < m; r++) {
-        pacificQueue.enqueue([r,0])
-        visitedp[r][0] = true
-        atlanticQueue.enqueue([r, n-1])
-        visiteda[r][n-1] = true
-    }
     
-    for(let c = 0; c < n; c++) {
-        pacificQueue.enqueue([0,c])
-        visitedp[0][c] = true
-        atlanticQueue.enqueue([m-1, c])
-        visiteda[m-1][c] = true
-    }
-    
-    let bfs = (visited, queue) => {
-        while(queue.isEmpty() == false) {
-            let cell = queue.dequeue()
-            for(let dir of dirs) {
-                let newr = cell[0] + dir[0]
-                let newc = cell[1] + dir[1]
-                if(newr < 0 || newr >= m || newc < 0 || newc >= n) {
-                    continue
-                }
-                if(visited[newr][newc] == false && heights[newr][newc] >= heights[cell[0]][cell[1]]) {
-                    visited[newr][newc] = true
-                    queue.enqueue([newr, newc])
-                }
+    let dfs = (visited, i, j) => {
+        if(visited[i][j] == true) {
+            return
+        }
+        visited[i][j] = true
+        let cell = [i,j]
+        for(let dir of dirs) {
+            let newr = cell[0] + dir[0]
+            let newc = cell[1] + dir[1]
+            if(newr < 0 || newr >= m || newc < 0 || newc >= n) {
+                continue
+            }
+            if(visited[newr][newc] == false && heights[newr][newc] >= heights[cell[0]][cell[1]]) {
+                dfs(visited,newr, newc)
             }
         }
     }
     
     // console.log(visiteda)
     // console.log(visitedp)
-    bfs(visitedp, pacificQueue)
-    bfs(visiteda, atlanticQueue)
+    for(let r = 0; r < m; r++) {
+        dfs(visitedp, r, 0)
+        dfs(visiteda, r, n-1 )
+    }
+    
+    for(let c = 0; c < n; c++) {
+        dfs(visitedp, 0, c)
+        dfs(visiteda, m-1, c )
+    }
     // console.log(visiteda)
     // console.log(visitedp)
     
