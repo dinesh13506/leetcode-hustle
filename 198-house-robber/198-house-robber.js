5b551790d65de3b5d61f08aee46a9c2aad6b48d9
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-16 17:18:42 +0530

    Time: 63 ms (92.39%), Space: 41.9 MB (56.78%) - LeetHub

--- 198-house-robber/198-house-robber.js
+++ 198-house-robber/198-house-robber.js
@@ -2,58 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-var robTopDown = function(nums) {
-    
-    let dp = new Array(nums.length)
-    dp.fill(-1)
-    //top down, start with bigger problem - > base cases -> return solution of bigger problem
-    let rob = function(index) {
-        if(index == 0) {
-            return nums[index]
-        }
+var rob = function(nums) {
+    let n = nums.length
+    let memo = new Array(n)
+    memo.fill(-1)
+    let dp = (index) => {
         if(index < 0) {
             return 0
         }
-        if(dp[index] == -1) {
-            let take = nums[index] + rob(index-2)
-            let nottake = rob(index-1)
-            dp[index] = Math.max(take,nottake)
-        }
-        return dp[index]
+        if(memo[index] != -1) return memo[index]
+        let rob = nums[index] + dp(index - 2)
+        let not_rob = dp(index - 1)
+        memo[index] =  Math.max(rob, not_rob)
+        return memo[index]
     }
-    return rob(nums.length-1)
+    return dp(n-1)
 };
-
-var robBottomUp = function(nums) {
-    let dp = new Array(nums.length)
-    //start with base cases
-    dp[0] = nums[0]
-    for(let i = 1; i < nums.length; i++) {
-        let take = nums[i] + dp[i-2] ? dp[i-2] : 0
-        let nottake = 0 + dp[i-1]
-        dp[i] = Math.max(take,nottake)
-    }
-    return dp[nums.length-1]
-}
-
-var robBottomUpSpaceOptimised = function(nums) {
-    
-    //start with base cases
-    let prev2 = nums[0]
-    if(nums.length == 1) {
-        return prev2
-    }
-    let prev = Math.max(nums[0], nums[1])
-    for(let i = 2; i < nums.length; i++) {
-        let take = nums[i] + prev2
-        let nottake = 0 + prev
-        let curri = Math.max(take,nottake)
-        prev2 = prev
-        prev = curri
-    }
-    return prev
-}
-
-var rob = function(nums) {
-    return robBottomUpSpaceOptimised(nums)
-}
