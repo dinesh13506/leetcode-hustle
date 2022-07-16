0a456ac6e868764f27be1a01b18cf27fbfa90d87
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-17 00:18:23 +0530

    Time: 99 ms (62.16%), Space: 51.1 MB (5.41%) - LeetHub

--- 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.js
+++ 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.js
@@ -18,17 +18,12 @@ var canThreePartsEqualSum = function(arr) {
     let sum = total/ 3
     //console.log(prefixsum)
     
+    let parts = 0
     for(let i = 0; i < arr.length; i++) {
-        for(let j = i + 1; j < arr.length; j++) {
-            let part1 = prefixsum[i]
-            let part2 = prefixsum[j] - prefixsum[i]
-            let part3 = j != arr.length - 1 ? total - prefixsum[j] : null
-            //console.log(part1, part2, part3, i, j)
-            if(part1 == part2 && part2 == part3 && part3 == part1 && part1 == sum) {
-                return true
-            }
+        if(prefixsum[i] == (parts + 1) * sum) {
+            parts++
         }
     }
-    return false
+    return parts>=3
     
 };
