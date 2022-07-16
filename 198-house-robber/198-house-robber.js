638f18593722b5a8efd217cb56884ecce296b046
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-16 17:22:50 +0530

    Time: 86 ms (50.78%), Space: 42.3 MB (24.64%) - LeetHub

--- 198-house-robber/198-house-robber.js
+++ 198-house-robber/198-house-robber.js
@@ -4,6 +4,18 @@
  */
 var rob = function(nums) {
     let n = nums.length
+    let dp = new Array(n)
+    dp[0] = nums[0]
+    dp[1] = Math.max(nums[0], nums[1])
+    for(let i = 2; i < n; i++) {
+        dp[i] = Math.max(nums[i] + dp[i-2] , dp[i-1])
+    }
+    return dp[n-1]
+};
+
+
+var robMemo = function(nums) {
+    let n = nums.length
     let memo = new Array(n)
     memo.fill(-1)
     let dp = (index) => {
