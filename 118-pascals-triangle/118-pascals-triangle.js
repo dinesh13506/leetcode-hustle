38491f5bb985933fa8b5d6ba8c9f7b3c5f981d68
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-19 22:42:12 +0530

    Time: 72 ms (74.92%), Space: 41.9 MB (71.84%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.js
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -3,16 +3,15 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-    let arr = [[1]]
-    while(arr.length < numRows) {
-        let lastArr = arr[arr.length-1]
+    let ans = [[1]]
+    while(ans.length < numRows) {
+        let lastArr = ans[ans.length-1]
         let curr = [1]
-        for(let i = 1; i < arr.length; i++) {
+        for(let i = 1; i < lastArr.length; i++) {
             curr.push(lastArr[i-1] + lastArr[i])
         }
         curr.push(1)
-        arr.push(curr)
+        ans.push(curr)
     }
-    //console.log(arr)
-    return arr
+    return ans
 };
