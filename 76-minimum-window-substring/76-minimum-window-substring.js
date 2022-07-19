9b280ec54b1815c7295ae3e9e359e61b2a592af0
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-19 23:37:52 +0530

    Time: 167 ms (34.75%), Space: 45.9 MB (53.60%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.js
@@ -0,0 +1,53 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function(s, t) {
+    
+    if(s.length == 0 && t.length == 0) {
+        return ""
+    }
+    let tmap = new Map()
+    for(let ch of t) {
+        tmap.set(ch, (tmap.get(ch) || 0 ) + 1)
+    }
+    
+    let ans = [-1,0,0]
+    let required = tmap.size
+    let formed = 0
+    let l = 0, r = 0
+    let windowMap = new Map()
+    while(r < s.length) {
+        let ch = s[r]
+        windowMap.set(ch, (windowMap.get(ch) || 0) + 1)
+        if(tmap.has(ch) && tmap.get(ch) == windowMap.get(ch)) {
+            formed++
+        }
+        
+        while(l <= r && formed == required) {
+            let ch2 = s[l]
+            if(ans[0] == -1 || (r-l+1) < ans[0]) {
+                ans[0] = r - l + 1
+                ans[1] = l
+                ans[2] = r
+            }
+            windowMap.set(ch2, windowMap.get(ch2) - 1)
+            if(tmap.has(ch2) && tmap.get(ch2) > windowMap.get(ch2)) {
+                formed--
+            }
+            l++
+        }
+        r++
+    }
+    
+    let substr = () => {
+        let op = ""
+        while(ans[1] <= ans[2]) {
+            op += s[ans[1]]
+            ans[1]++
+        }
+        return op
+    }
+    return ans[0] == -1 ? "" : substr()
+};
