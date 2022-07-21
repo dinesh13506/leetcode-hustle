f3c5f6fb14e93bfda6b2d268b57086bcff814ac9
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-21 23:27:18 +0530

    Time: 71 ms (77.19%), Space: 42.5 MB (21.72%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.js
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.js
@@ -11,81 +11,26 @@
  * @param {number} right
  * @return {ListNode}
  */
-
-class Node {
-    constructor(data) {
-        this.data = data
-        this.next = null
-        this.prev = null
-    }
-}
-
-class Deque {
-    constructor() {
-        this.front = null
-        this.rear = null
-        this.size = 0
-    }
-    
-    insertAtRear(value) {
-        let node = new Node(value)
-        if(this.front == null && this.rear == null) {
-            this.front = node
-            this.rear = node
-        } else {
-            this.rear.next = node
-            node.prev = this.rear
-            this.rear = this.rear.next
-        }
-        this.size++
-    }
-    
-    popAtRear() {
-        if(this.front == null && this.rear == null) {
-            return
-        }
-        
-        let node = this.rear
-        this.rear = this.rear.prev
-        if(this.rear) this.rear.next = null
-        
-        this.size--
-        return node
-    }
-    
-     popAtFront() {
-        if(this.front == null && this.rear == null) {
-            return
-        }
-        let node = this.front
-        this.front = this.front.next
-        if(this.front) this.front.prev = null
-        this.size--
-        return node
-    }
-}
 var reverseBetween = function(head, left, right) {
     
-    let deq = new Deque()
-    let p = head, pos = 0
-    while(p!= null) {
-        pos++
-        if(pos>= left && pos <= right) {
-            deq.insertAtRear(p)
+    let nodes = []
+    let index = 1
+    let p = head
+    while(p) {
+        if(index >= left && index <= right) {
+            nodes.push(p)
         }
+        index++
         p = p.next
     }
-    
-    //console.log(deq.size)
-    while(deq.size > 1) {
-        let front = deq.front.data
-        let rear = deq.rear.data
-        let temp = front.val
-        front.val = rear.val
-        rear.val = temp
-        deq.popAtFront()
-        deq.popAtRear()
+    //console.log(nodes)
+    let i = 0, j = nodes.length - 1
+    while(i<=j) {
+        let temp = nodes[i].val
+        nodes[i].val = nodes[j].val
+        nodes[j].val = temp
+        i++
+        j--
     }
     return head
-    
 };
