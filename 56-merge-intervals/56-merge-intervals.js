a4be23a252144fe2496a28417612adfdc7105c46
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-22 18:17:42 +0530

    Time: 100 ms (96.27%), Space: 48.6 MB (84.16%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -3,43 +3,29 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    
-    intervals.sort( function(a , b) {
-        if(a[0] == b[0]) {
-            if(a[1] <= b[1]) {
-                return -1
-            } else if(a[1] == b[1]) {
-                return 0
-            } else {
-                return 1
-            }
+    intervals.sort((a,b) => {
+        if(a[0] < b[0]) {
+            return -1
+        }
+        else if(a[0] == b[0] && a[1] < b[1]) {
+            return -1
         } else {
-            if(a[0] < b[0]) {
-                return -1
-            } else {
-                return 1
-            }
+            return 1
         }
     })
-    
     //console.log(intervals)
-    let answer = [intervals[0]]
-    for(let i = 1; i < intervals.length; i++) {
-        let prevInterval = answer[answer.length-1]
-        let currentInterval = intervals[i]
-        //console.log(prevInterval,currentInterval)
-        //addition
-        if(currentInterval[0] > prevInterval[1]) {
-            answer.push(currentInterval)
-        } else if(currentInterval[0] >= prevInterval[0] &&  currentInterval[1] <= prevInterval[1]) {
-            continue
+    let mergedIntervals = []
+    if(intervals.length <= 0) return mergedIntervals
+    let tempinterval = intervals[0]
+    for(let interval of intervals) {
+        if(interval[0] <= tempinterval[1]) {
+            tempinterval[0] = Math.min(interval[0], tempinterval[0])
+            tempinterval[1] = Math.max(interval[1], tempinterval[1])
+        } else {
+            mergedIntervals.push(tempinterval)
+            tempinterval = interval
         }
-        else if(currentInterval[0] <= prevInterval[1]) {
-            answer.pop()
-            let mergedInterval = [prevInterval[0],currentInterval[1]]
-            answer.push(mergedInterval)
-        }
-        //console.log(answer)
     }
-    return answer
+    mergedIntervals.push(tempinterval)
+    return mergedIntervals
 };
