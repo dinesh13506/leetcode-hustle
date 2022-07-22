5cfcd80a911036ec5c4744dde2cc4a016ad5c59f
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-23 00:10:44 +0530

    Time: 111 ms (46.23%), Space: 44 MB (62.53%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.js
+++ 735-asteroid-collision/735-asteroid-collision.js
@@ -5,28 +5,30 @@
 var asteroidCollision = function(asteroids) {
     
     let stack = []
-    stack.push(asteroids[0])
-    stack.push(asteroids[1])
-    for(let i = 2; i < asteroids.length + 1; i++) {
-        while(stack.length >= 2) {
-            let top1 = stack[stack.length-1]
-            let top2 = stack[stack.length-2]
-            //console.log(top1,top2)
-            if((top1 < 0 && top2 > 0)) {
-                if(Math.abs(top1) == Math.abs(top2)) {
+    let n = asteroids.length
+    for(let i = 0; i < n; i++) {
+        if(stack.length <=0 || asteroids[i] > 0) {
+            stack.push(asteroids[i])
+        } else {
+            while(true) {
+                let peek = stack[stack.length-1]
+                if(peek < 0) {
+                    stack.push(asteroids[i])
+                    break
+                } else if(peek == -asteroids[i]) {
                     stack.pop()
-                    stack.pop()
+                    break
+                } else if(peek > -asteroids[i]) {
+                    break
                 } else {
                     stack.pop()
-                    stack.pop()
-                    stack.push(Math.abs(top1) < Math.abs(top2) ? top2 : top1)
+                    if(stack.length <= 0) {
+                        stack.push(asteroids[i])
+                        break
+                    }
                 }
-            } else {
-                break
-            } 
+            }
         }
-        if( i < asteroids.length )
-            stack.push(asteroids[i])
     }
     return stack
 };
