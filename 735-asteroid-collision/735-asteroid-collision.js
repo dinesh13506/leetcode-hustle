cba36c0da05c5d1470eb440ac0a8ebb35e94aa39
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-23 00:13:52 +0530

    Time: 115 ms (40.33%), Space: 43.3 MB (99.80%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.js
+++ 735-asteroid-collision/735-asteroid-collision.js
@@ -7,20 +7,23 @@ var asteroidCollision = function(asteroids) {
     let stack = []
     let n = asteroids.length
     for(let i = 0; i < n; i++) {
+        /* positive values will never do harm */
         if(stack.length <=0 || asteroids[i] > 0) {
             stack.push(asteroids[i])
         } else {
             while(true) {
                 let peek = stack[stack.length-1]
+                //both are in moving left direction
                 if(peek < 0) {
                     stack.push(asteroids[i])
                     break
-                } else if(peek == -asteroids[i]) {
+                } else if(peek == -asteroids[i]) { //peek is right and new asteroid is in left and both are same size
                     stack.pop()
                     break
-                } else if(peek > -asteroids[i]) {
+                } else if(peek > -asteroids[i]) { //peek is of higher size, current asteroid will blast
                     break
                 } else {
+                    //peek is smaller and current is greater then peek will blast and current will blast others if they are there
                     stack.pop()
                     if(stack.length <= 0) {
                         stack.push(asteroids[i])
