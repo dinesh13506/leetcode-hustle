d052a38f69adc997fc357a90634afba5242e80b9
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-07-29 08:36:56 +0530

    Time: 70 ms (93.33%), Space: 44.2 MB (68.33%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.js
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.js
@@ -4,61 +4,23 @@
  * @return {string[]}
  */
 var findAndReplacePattern = function(words, pattern) {
-    let answer = []
-    let getid = (word) => {
-        let stack = []
-        for(let ch of word) {
-            if(stack.length <= 0) {
-                stack.push([ch, 1])
-            } else {
-                let top = stack[stack.length-1]
-                if(top[0] == ch) {
-                    top[1]++
-                } else {
-                    stack.push([ch, 1])
-                }
-            }
-        }
-        return stack
-    }
     
-    let compareStacks = (stack1, stack2) => {
-        if(stack1.length != stack2.length) {
-            return false
+    let answer = []
+    
+    let match = (word) => {
+        let map1 = new Map()
+        let map2 = new Map()
+        for(let i = 0; i < word.length; i++) {
+            let w = word[i], p = pattern[i]
+            if(map1.has(w) == false) map1.set(w,p)
+            if(map2.has(p) == false) map2.set(p,w)
+            if(map1.get(w) != p || map2.get(p) != w) return false
         }
-        let taken = new Set()
-        let map = new Map()
-        for(let i = 0; i < stack1.length; i++) {
-            
-            let top1 = stack1[i]
-            let ch1 = top1[0], count1 = top1[1]
-            let top2 = stack2[i]
-            let ch2 = top2[0], count2 = top2[1]
-            
-            if(map.has(ch1) && map.get(ch1) == ch2 && count1 == count2) {
-                continue
-            }
-            
-            if(map.has(ch1) && (map.get(ch1) != ch2 || count1 != count2)) {
-                return false
-            }
-            if(count1!= count2) {
-                return false
-            }
-            if(taken.has(ch2)) return false
-            map.set(ch1,ch2)
-            taken.add(ch2)
-        }
-        //console.log(map)
         return true
     }
-    
-    let patid = getid(pattern)
-    //console.log(patid)
-    for(let w of words) {
-        let wid = getid(w)
-        if(compareStacks(wid, patid)) {
-            answer.push(w)
+    for(let word of words) {
+        if(match(word, pattern)) {
+            answer.push(word)
         }
     }
     return answer
