439e67c35a73d2172bd1660a2db671f2e7cebc31
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-08-01 08:33:19 +0530

    Time: 93 ms (47.71%), Space: 42.4 MB (50.00%) - LeetHub

--- 62-unique-paths/62-unique-paths.js
+++ 62-unique-paths/62-unique-paths.js
@@ -5,22 +5,15 @@
  */
 var uniquePaths = function(m, n) {
     
-    let dp = new Array(m)
-    for(let i = 0; i < m; i++) {
-        dp[i] = new Array(n)
-        dp[i].fill(0)
-    }
-    for(let j = 0; j < n; j++) {
-        dp[0][j] = 1
-    }
-    for(let i = 0; i < m; i++) {
-        dp[i][0] = 1
-    }
+    let prev = new Array(n)
+    prev.fill(1)
     for(let i = 1; i < m; i++) {
+        let curr = new Array(n)
+        curr[0] = 1
         for(let j = 1; j < n; j++) {
-            dp[i][j] = dp[i-1][j] + dp[i][j-1]
+            curr[j] = prev[j] + curr[j-1]
         }
+        prev = curr
     }
-    return dp[m-1][n-1]
-    
+    return prev[n-1]
 };
