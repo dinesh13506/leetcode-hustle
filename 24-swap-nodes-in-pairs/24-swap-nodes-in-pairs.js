f9073f438f34f92da3f11dce1c299ecc4f541b94
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-08-03 12:45:05 +0530

    Time: 87 ms (53.32%), Space: 42.4 MB (22.18%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.js
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.js
@@ -10,19 +10,58 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-    if(head == null) {
+    let k = 2
+    let getLength = (node) => {
+        let p = node
+        let length = 0
+        while(p) {
+            length++
+            p = p.next
+        }
+        return length
+    }
+    let length = getLength(head)
+    if(length == 0 || length==1) {
         return head
     }
-    let p = head, count = 1, prev = null
-    while(p) {
-        prev = p
-        p = p.next
-        count++
-        if(count % 2 == 0 && p != null) {
-            let temp = p.val
-            p.val = prev.val
-            prev.val = temp
+    
+    let groups = parseInt( length / k )
+    let groupInfo = new Array(groups)
+    let curr = head, remainingPart = null
+    for(let i = 0; i < groups; i++) {
+        groupInfo[i] = new Array(2)
+        groupInfo[i][0] = curr
+        let count = k
+        while(count > 0) {
+            count--
+            groupInfo[i][1] = curr
+            curr = curr.next
         }
+        //console.log(groupInfo[i][0].val + " -> "  + groupInfo[i][1].val)
+        remainingPart = groupInfo[i][1].next
+        groupInfo[i][1].next = null
     }
-    return head
+    
+    let reverse = (group) => {
+        let curr = group[0], prev = null, end = curr
+        while(curr) {
+            let next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        }
+        
+        group[0] = prev
+        group[1] = end
+        //console.log(prev, curr)
+    }
+    reverse(groupInfo[0])
+    let prev = groupInfo[0]
+    for(let i = 1; i < groups; i++) {
+        reverse(groupInfo[i])
+        prev[1].next = groupInfo[i][0]
+        prev = groupInfo[i]
+    }
+    prev[1].next = remainingPart
+    return groupInfo[0][0]
 };
