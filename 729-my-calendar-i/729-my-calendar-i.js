8e21ae2fdff278df157d45e90520c2c8b75155b0
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-08-03 08:37:15 +0530

    Time: 217 ms (74.00%), Space: 50.5 MB (82.00%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.js
+++ 729-my-calendar-i/729-my-calendar-i.js
@@ -1,6 +1,41 @@
+class TreeNode {
+    constructor(start, end) {
+        this.start = start
+        this.end = end
+        this.left = null
+        this.right = null
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null
+    }
+    insert(root, node) {
+        if(node.end <= root.start) {
+            if(root.left == null) {
+                root.left = node
+                return true
+            } else {
+                return this.insert(root.left, node)
+            }
+        }
+        else if(node.start >= root.end) {
+            if(root.right == null) {
+                root.right = node
+                return true
+            } else {
+                return this.insert(root.right, node)
+            }
+        } else {
+            return false
+        }
+    }
+}
+
 
 var MyCalendar = function() {
-   this.events = [] 
+    this.bst = new BST()
 };
 
 /** 
@@ -9,13 +44,12 @@ var MyCalendar = function() {
  * @return {boolean}
  */
 MyCalendar.prototype.book = function(start, end) {
-    for(let ev of this.events) {
-        if(ev[0] < end && ev[1] > start) {
-            return false
-        }
+    const node = new TreeNode(start, end)
+    if(this.bst.root == null) {
+        this.bst.root = node
+        return true
     }
-    this.events.push([start,end])
-    return true
+    return this.bst.insert(this.bst.root, node)
 };
 
 /** 
