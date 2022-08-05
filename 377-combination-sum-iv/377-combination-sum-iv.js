2176224f025ef9b0c3e8636321e23a0eda0aeb01
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-08-05 09:06:15 +0530

    Time: 76 ms (79.76%), Space: 42.6 MB (63.61%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var combinationSum4 = function(nums, target) {
+    
+    let memo = new Map()
+    let dp = (sum) => {
+        if(sum == 0) {
+            return 1
+        }
+        if(memo.has(sum)) {
+            return memo.get(sum)
+        }
+        let result = 0
+        for(let num of nums) {
+            if(sum >= num) {
+                result += dp(sum - num)
+            }
+        }
+        memo.set(sum, result)
+        return result
+    }
+    
+    return dp(target)
+};
