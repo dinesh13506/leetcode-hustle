11b2e624b474a00d0f59f9b047cd72b6add39b9e
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-08-18 09:04:27 +0530

    Time: 222 ms (56.60%), Space: 69 MB (33.02%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var minSetSize = function(arr) {
+    
+    let map = new Map()
+    for(let num of arr) {
+        map.set(num, (map.get(num) || 0) + 1)
+    }
+    //console.log(map)
+    let nums = [... map.keys()]
+    nums.sort((a,b) => {
+        if(map.get(a) > map.get(b)) {
+            return -1
+        }
+        else if(map.get(a) < map.get(b)) {
+            return 1
+        }
+        return 0
+    })
+    //console.log(nums)
+    
+    let len = arr.length
+    let ans = 0, i = 0
+    while(i < nums.length) {
+        let count = map.get(nums[i])
+        //console.log(len, count)
+        len = len - count
+        ans++
+        i++
+        if(len <= arr.length/2) break
+    }
+    return ans
+};
