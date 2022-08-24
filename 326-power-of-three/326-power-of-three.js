c302fdb31979633940f23650e5fe6ecd725beb65
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-08-24 10:05:01 +0530

    Time: 203 ms (96.73%), Space: 51 MB (69.23%) - LeetHub

--- 326-power-of-three/326-power-of-three.js
+++ 326-power-of-three/326-power-of-three.js
@@ -2,12 +2,11 @@
  * @param {number} n
  * @return {boolean}
  */
+const max = Math.pow(2,31) - 1
+const pow = parseInt(Math.log(max) / Math.log(3))
+const value = Math.pow(3, pow)
 var isPowerOfThree = function(n) {
     
     if(n <= 0) return false
-    
-    while(n > 1) {
-        n = n / 3
-    }
-    return n == 1
+    return value % n == 0
 };
