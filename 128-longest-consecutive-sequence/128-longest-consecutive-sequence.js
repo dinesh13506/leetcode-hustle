1b60a7d99556bfdcc5f3e1f4a0640fe1e66b9a7a
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-08-24 23:33:31 +0530

    Time: 201 ms (58.69%), Space: 58.5 MB (40.55%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -3,40 +3,29 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    
-    let set = new Set()
-    nums.sort(function(a,b) {
-        return b - a
-    })
-    for(let num of nums) {
-        set.add(num)
-    }
-    
-    let map = new Map()
-    for(let num of nums) {
-        if(set.has(num-1)) {
-            map.set(num, num-1)
-        } else {
-             map.set(num, null)
-        }
-    }
-    
-    //console.log(map)
-    let visited = new Map()
     let ans = 0
-    for(let num of nums) {
-        if(visited.has(num)) {
-            continue
-        }
-        let count = 1
-        let x = num
-        visited.set(x, true)
-        while( map.get(x) != null && !visited.has(map.get(x)) ) {
-            count++
-            x = map.get(x)
-            visited.set(x, true)
-        }
-        ans = Math.max(ans, count)
+    const n = nums.length
+    if(n == 0) {
+        return ans
     }
-    return ans
+    nums.sort((a,b) => {
+        return a - b
+    })
+    
+    let set = new Set(nums)
+    nums = [...set]
+    
+    let currlen = 1, prevnum = nums[0], max = 1
+    for(let i = 1; i < nums.length; i++) {
+        if(nums[i] == prevnum + 1) {
+            currlen++
+            max = Math.max(currlen, max)
+            prevnum = nums[i]
+        } else {
+            max = Math.max(currlen, max)
+            currlen = 1
+            prevnum = nums[i]
+        }
+    }
+    return max
 };
