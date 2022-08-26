15ed6f7b3df107c8b1559130a8184b5fc001edef
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-08-26 17:24:04 +0530

    Time: 1302 ms, Memory: 0B - LeetHub

--- sales-person/sales-person.sql
+++ sales-person/sales-person.sql
@@ -2,4 +2,6 @@
 
 #select sales_id from  Orders where com_id in (select com_id from Company where name not in ("RED"))
 
-select sp.name from SalesPerson as sp where sp.sales_id not in( select orders.sales_id from Orders as orders inner join Company as company on orders.com_id = company.com_id and company.name = "RED");
+#select sp.name from SalesPerson as sp where sp.sales_id not in( select orders.sales_id from Orders as orders inner join Company as company on orders.com_id = company.com_id and company.name = "RED");
+
+select name from SalesPerson sp where sp.sales_id not in (select od.sales_id from orders as od where od.com_id in ( select cmp.com_id from Company as cmp where cmp.name = "RED"));
