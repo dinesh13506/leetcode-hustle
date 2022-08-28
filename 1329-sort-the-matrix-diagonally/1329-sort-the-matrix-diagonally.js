33852f8d51960bcf30d85aa7483b8429d568737f
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-08-28 12:12:47 +0530

    Time: 209 ms (5.11%), Space: 48.1 MB (11.36%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.js
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.js
@@ -5,50 +5,24 @@
 var diagonalSort = function(mat) {
     
     let n = mat.length, m = mat[0].length
-    
-    
-    for(let j = 0; j < m; j++) {
-        let r = 0, c = j, diag = []
-        while(r < n && c < m) {
-            diag.push(mat[r][c])
-            r++
-            c++
+    let map = new Map()
+    for(let r = 0; r < n; r++) {
+        for(let c = 0;  c < m; c++) {
+            let hash = r - c
+            if(map.has(hash)) {
+                map.get(hash).enqueue(mat[r][c])
+            } else {
+                map.set(hash, new MinPriorityQueue())
+                map.get(hash).enqueue(mat[r][c])
+            }
         }
-        if(r >= n || c >= m) {
-            diag.sort((a,b) => {return a - b})
-            //console.log(diag)
-            r = 0, c = j
-            let i = 0
-            while(r < n && c < m && i < diag.length) {
-                mat[r][c] = diag[i]
-                r++
-                c++
-                i++
-            } 
-        }
-        
     }
     
-    for(let i = 0; i < n; i++) {
-        let c = 0, r = i, diag = []
-        while(c < m && r < n) {
-           diag.push(mat[r][c])
-            r++
-            c++ 
+    for(let r = 0; r < n; r++) {
+        for(let c = 0;  c < m; c++) {
+            let bucket = map.get(r - c)
+            mat[r][c] = bucket.dequeue().element
         }
-        if(r >= n || c >= m) {
-            diag.sort((a,b) => {return a - b})
-            //console.log(diag)
-            c = 0, r = i
-            let p = 0
-            while(r < n && c < m && p < diag.length) {
-                mat[r][c] = diag[p]
-                r++
-                c++
-                p++
-            } 
-        }
-        
     }
     return mat
     
