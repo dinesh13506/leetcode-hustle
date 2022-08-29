93b605f15e4c3c8a0225f5765a45acb0ee7b1054
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-08-29 23:19:24 +0530

    Time: 204 ms (40.28%), Space: 62.1 MB (8.31%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
@@ -1,79 +1,21 @@
-class MinHeap {
-    constructor() {
-        this.arr = []
-        this.size = 0
-    }
-    
-    swap(i,j) {
-        this.arr[i] = this.arr[i] ^ this.arr[j]
-        this.arr[j] = this.arr[i] ^ this.arr[j]
-        this.arr[i] = this.arr[i] ^ this.arr[j]
-    }
-    
-    heapify(i) {
-        let smallest = i
-        let left = 2 * i  + 1
-        let right = left + 1
-        if(left < this.size && this.arr[left] < this.arr[smallest]) {
-            smallest = left
-        }
-        if(right < this.size && this.arr[right] < this.arr[smallest]) {
-            smallest = right
-        }
-        if(smallest != i) {
-            this.swap(i, smallest)
-            this.heapify(smallest)
-        }
-    }
-    
-    bottomUp(i) {
-        let parent = parseInt((i-1)/2)
-        if(this.arr[parent] > this.arr[i]) {
-            this.swap(i, parent)
-            this.bottomUp(parent)
-        }
-    }
-    
-    insert(num) {
-        this.arr.push(num)
-        this.size++
-        this.bottomUp(this.size -1)
-    }
-    
-    extractMin() {
-        let min = this.arr[0]
-        this.swap(0, this.size-1)
-        this.arr.pop()
-        this.size--
-        this.heapify(0)
-        return min
-    }
-    
-    getMin() {
-        return this.arr[0]
-    }
-}
-
-
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    
-    let minHeap = new MinHeap()
-    for(let i=0; i < nums.length; i++) {
-        //console.log(minHeap.arr)
-        if(minHeap.size < k) {
-            minHeap.insert(nums[i])
-        } else {
-            if(nums[i] > minHeap.getMin()) {
-                minHeap.extractMin()
-                minHeap.insert(nums[i])
-            }
-        }
+   
+    let minpq = new MinPriorityQueue()
+    for(let i = 0; i < k; i++) {
+        minpq.enqueue(nums[i])
     }
     
-    return minHeap.getMin()
+    for(let i = k; i < nums.length; i++) {
+        let top = minpq.front().element
+        if(top < nums[i]) {
+            minpq.dequeue()
+            minpq.enqueue(nums[i])
+        }
+    }
+    return minpq.front().element
 };
