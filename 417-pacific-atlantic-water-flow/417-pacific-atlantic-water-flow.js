8cb59f3491fb06b23cde93d5b4c6fa29f18c98f3
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-08-31 16:57:36 +0530

    Time: 224 ms (42.07%), Space: 51.7 MB (39.58%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.js
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.js
@@ -3,62 +3,69 @@
  * @return {number[][]}
  */
 var pacificAtlantic = function(heights) {
-    
     let m = heights.length, n = heights[0].length
-    let cells = []
-    if(m == 0 && n ==0) {
-        return cells
-    }
-    
-    let visitedp = new Array(m)
-    let visiteda = new Array(m)
+    let visited = new Array(m)
     for(let i = 0; i < m; i++) {
-        visitedp[i] = new Array(n)
-        visitedp[i].fill(false)
-        visiteda[i] = new Array(n)
-        visiteda[i].fill(false)
+        visited[i] = new Array(n)
+        visited[i].fill(0)
     }
     
-    const dirs = [[1,0],[-1,0],[0,1],[0,-1]]
-    
-    let dfs = (visited, i, j) => {
-        if(visited[i][j] == true) {
-            return
-        }
-        visited[i][j] = true
-        let cell = [i,j]
-        for(let dir of dirs) {
-            let newr = cell[0] + dir[0]
-            let newc = cell[1] + dir[1]
-            if(newr < 0 || newr >= m || newc < 0 || newc >= n) {
-                continue
-            }
-            if(visited[newr][newc] == false && heights[newr][newc] >= heights[cell[0]][cell[1]]) {
-                dfs(visited,newr, newc)
-            }
-        }
-    }
-    
-    // console.log(visiteda)
-    // console.log(visitedp)
-    for(let r = 0; r < m; r++) {
-        dfs(visitedp, r, 0)
-        dfs(visiteda, r, n-1 )
-    }
-    
-    for(let c = 0; c < n; c++) {
-        dfs(visitedp, 0, c)
-        dfs(visiteda, m-1, c )
-    }
-    // console.log(visiteda)
-    // console.log(visitedp)
-    
+    let queue = new Queue()
     for(let i = 0; i < m; i++) {
-        for(let j = 0; j < n; j++) {
-            if(visiteda[i][j] == true && visitedp[i][j] == true) {
-                cells.push([i,j])
+        queue.enqueue([i,0])
+    }
+    for(let j = 1; j < n; j++) {
+        queue.enqueue([0,j])
+    }
+    while(queue.isEmpty() == false) {
+        let cell = queue.dequeue()
+        visited[cell[0]][cell[1]] = 1
+        const dir = [[-1,0],[1,0],[0,-1],[0,1]]
+        for(let d of dir) {
+            let x = cell[0] + d[0]
+            let y = cell[1] + d[1]
+            if(x >= 0 && y >= 0 && x < m && y < n && visited[x][y] == 0) {
+                if(heights[cell[0]][cell[1]] <= heights[x][y]) {
+                    queue.enqueue([x,y])
+                }
             }
         }
     }
-    return cells
+    //console.log(visited)
+    for(let i = 0; i < m; i++) {
+        queue.enqueue([i,n-1])
+    }
+    for(let j = 0; j < n; j++) {
+        queue.enqueue([m-1,j])
+    }
+    let visited2 = new Array(m)
+    for(let i = 0; i < m; i++) {
+        visited2[i] = new Array(n)
+        visited2[i].fill(0)
+    }
+    while(queue.isEmpty() == false) {
+        let cell = queue.dequeue()
+        visited2[cell[0]][cell[1]] = 1
+        const dir = [[-1,0],[1,0],[0,-1],[0,1]]
+        for(let d of dir) {
+            let x = cell[0] + d[0]
+            let y = cell[1] + d[1]
+            if(x >= 0 && y >= 0 && x < m && y < n && visited2[x][y] == 0) {
+                if(heights[cell[0]][cell[1]] <= heights[x][y]) {
+                    queue.enqueue([x,y])
+                }
+            }
+        }
+    }
+    
+    //console.log(visited2)
+    let ans = []
+    for(let i = 0; i < m; i++) {
+       for(let j = 0; j < n; j++) {
+           if(visited[i][j] == 1 && visited2[i][j] == 1) {
+               ans.push([i,j])
+           }
+       }
+    }
+    return ans
 };
