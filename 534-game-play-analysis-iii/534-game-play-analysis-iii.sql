c5546bc07a8ad3af718741ce05304ac80512bcb0
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-01 00:47:24 +0530

    Time: 957 ms (44.38%), Space: 0B (100.00%) - LeetHub

--- 534-game-play-analysis-iii/534-game-play-analysis-iii.sql
+++ 534-game-play-analysis-iii/534-game-play-analysis-iii.sql
@@ -1,7 +1,8 @@
 # Write your MySQL query statement below
 
 
-select a1.player_id,a1.event_date, (
-    select sum(a2.games_played) from Activity as a2 where a2.player_id = a1.player_id and a2.event_date <= a1.event_date
-) as games_played_so_far
-from Activity as a1
+SELECT
+player_id, event_date, sum(games_played) over(PARTITION BY player_id ORDER BY event_date)
+AS 'games_played_so_far'
+FROM activity
+ORDER BY player_id, games_played_so_far;
