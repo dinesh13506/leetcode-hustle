1932aab6cb80c720f2d08435871e0ef6f046b8ba
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-01 19:00:21 +0530

    Time: 493 ms (24.18%), Space: 0B (100.00%) - LeetHub

--- 570-managers-with-at-least-5-direct-reports/570-managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/570-managers-with-at-least-5-direct-reports.sql
@@ -1,10 +1,11 @@
 # Write your MySQL query statement below
 
 
-select name from Employee where id in (select
+select name from Employee inner join (select
 managerId
 from 
 Employee
 where managerId is not null
 group by managerId
-having count(*) >= 5)
+having count(*) >= 5) as B
+on id = B.managerId
