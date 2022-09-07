d98529142fce217df8c051d8a7cafb236f14b016
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-07 12:16:07 +0530

    Time: 176 ms (22.73%), Space: 49.8 MB (68.18%) - LeetHub

---
+++ 536-construct-binary-tree-from-string/536-construct-binary-tree-from-string.js
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {string} s
+ * @return {TreeNode}
+ */
+var str2tree = function(s) {
+    
+    let index = 0
+    let getNumber = () => {
+        let isNegative = false
+        if(s[index] == '-') {
+            isNegative = true
+            index++
+        }
+        let num = 0
+        while(index < s.length && s[index] >= '0' && s[index] <= '9') {
+            num = (num*10) + parseInt(s[index])
+            index++
+        }
+        return (isNegative ? -1 * num : num)
+    }
+    
+    let str2treeinternal = () => {
+        if(index >= s.length) {
+            return null
+        }
+        if(s[index] == ')') {
+            index++
+            return null
+        }
+        
+        let num = getNumber(index)
+        let node = new TreeNode(num)
+        if(index < s.length && s[index] == '(') {
+            index++
+            node.left = str2treeinternal()
+        }
+        
+        if(index < s.length && s[index] == '(') {
+            index++
+            node.right = str2treeinternal()
+        }
+        index++
+        return node
+        
+    }
+    
+    return str2treeinternal()
+};
