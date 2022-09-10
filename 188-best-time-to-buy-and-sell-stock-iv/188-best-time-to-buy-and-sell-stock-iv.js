d7fa99f46da152f69bb515a85258abd53aa1cc98
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-10 12:07:23 +0530

    Time: 149 ms (40.64%), Space: 45.7 MB (37.43%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.js
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.js
@@ -3,82 +3,40 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfitMemo = function(k, prices) {
-    
-    let n = prices.length
-    
-    let memo = new Array(n)
-    for(let i = 0; i < n; i++) {
-        memo[i] = new  Array(2)
-        for(let buy = 0; buy <=1; buy++) {
-            memo[i][buy] = new Array(k+1)
-            memo[i][buy].fill(-1)
-        }
-    }
-    
-    let dp = function(i, buy, cap) {
-        if(i === n || cap ==0) {
-            return 0
-        }
-        
-        if(memo[i][buy][cap] != -1) {
-            return memo[i][buy][cap]
-        }
-        
-        if(buy) {
-            let buystock = -prices[i] + dp(i+1, 0, cap)
-            let notbuystock = 0 + dp(i+1, 1, cap)
-            let profit = Math.max(buystock, notbuystock)
-            memo[i][buy][cap] =  profit
-            return memo[i][buy][cap]
-            
-        } else {
-            let sellstock = prices[i] + dp(i+1,1,cap-1)
-            let notsellstock = 0 + dp(i+1,0,cap)
-            let profit = Math.max(sellstock,notsellstock)
-            memo[i][buy][cap] =  profit
-            return memo[i][buy][cap]
-        }
-    }
-    
-    return dp(0,1,k)
-};
-
-
 var maxProfit = function(k, prices) {
     
-    let n = prices.length
-    
-    let dp = new Array(n+1)
-    for(let i = 0; i <= n; i++) {
-        dp[i] = new  Array(2)
-        for(let buy = 0; buy <=1; buy++) {
-            dp[i][buy] = new Array(k+1)
-            dp[i][buy].fill(0)
+    const n = prices.length
+    let memo = new Array(n)
+    for(let i = 0; i < n; i++) {
+        memo[i] = new Array(2)
+        for(let j = 0; j < 2; j++) {
+            memo[i][j] = new Array(k+1)
+            memo[i][j].fill(-1)
         }
     }
-    
-    for(let i = n-1; i >=0; i--) {
-        for(let buy = 0; buy <=1; buy++) {
-            for(let cap=1; cap<=k; cap++) {
-               if(buy) {
-                    let buystock = -prices[i] + dp[i+1][0][cap]
-                    let notbuystock = 0 + dp[i+1][1][cap]
-                    let profit = Math.max(buystock, notbuystock)
-                    dp[i][buy][cap] =  profit
-                    
+    let dp = (index, haveBought, transaction_count) => {
+        if(index >= n || transaction_count <= 0) {
+            return 0
+        }
+        if(memo[index][haveBought][transaction_count] != -1) {
+            return memo[index][haveBought][transaction_count]
+        }
+        /** customer has already bought a stock before*/
+        if(haveBought == 1) {
+            // sell or not sell
+            let sell = prices[index] + dp(index+1, 0, transaction_count-1)
+            let notsell = 0 + dp(index + 1, 1, transaction_count)
+            memo[index][haveBought][transaction_count] =  Math.max(sell, notsell)
             
-                } else {
-                    let sellstock = prices[i] + dp[i+1][1][cap-1]
-                    let notsellstock = 0 + dp[i+1][0][cap]
-                    let profit = Math.max(sellstock,notsellstock)
-                    dp[i][buy][cap] =  profit
-                } 
-            }
+        } else {
+            //buy or not buy
+            let buy = -prices[index] + dp(index+1, 1, transaction_count)
+            let notbuy = 0 + dp(index+1, 0, transaction_count)
+            memo[index][haveBought][transaction_count] =  Math.max(buy, notbuy)
         }
+        return memo[index][haveBought][transaction_count]
     }
     
-    return dp[0][1][k]
-    
+    return dp(0, 0,k)
     
 };
