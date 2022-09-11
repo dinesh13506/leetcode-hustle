5524ad1a22215d75a023779a50a6b1aa63ec44d6
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-12 00:00:36 +0530

    Time: 458 ms (7.69%), Space: 76.8 MB (7.69%) - LeetHub

---
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} n
+ * @param {number[]} speed
+ * @param {number[]} efficiency
+ * @param {number} k
+ * @return {number}
+ */
+var maxPerformance = function(n, speed, efficiency, k) {
+    
+    const mod = 1000000000n + 7n
+    let input = []
+    let answer = 0n
+    let sum = 0n
+    let minpq = new MinPriorityQueue()
+    
+    for(let i = 0; i < n; i++) {
+        input.push([efficiency[i], speed[i]])
+    }
+    
+    input.sort((a,b) => {
+        return  b[0] - a[0] 
+    })
+    
+    for(let i = 0; i < n; i++) {
+        let minEfficiency = BigInt(input[i][0])
+        if(minpq.size() > k - 1) {
+           sum = sum - BigInt(minpq.dequeue().element)
+        }
+        minpq.enqueue(input[i][1])
+        sum = sum + BigInt(input[i][1])
+        if(answer <  (sum * minEfficiency)) {
+            answer =  (sum * minEfficiency)
+        }
+    }
+    return (answer % mod)
+};
