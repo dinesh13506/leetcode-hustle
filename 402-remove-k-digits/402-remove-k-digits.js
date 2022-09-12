1e59c6904849a66983db913a990f1c932ca04698
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-12 09:34:32 +0530

    Time: 116 ms (50.85%), Space: 49.4 MB (17.63%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.js
+++ 402-remove-k-digits/402-remove-k-digits.js
@@ -4,27 +4,28 @@
  * @return {string}
  */
 var removeKdigits = function(num, k) {
-    
     let count = k
     let stack = []
     for(let ch of num) {
-        while(stack.length && count && parseInt(stack[stack.length-1]) > parseInt(ch)) {
+        while(count && stack.length && stack[stack.length-1] > parseInt(ch)) {
             stack.pop()
             count--
         }
-        stack.push(ch)
+        stack.push(parseInt(ch))
     }
     while(count) {
         stack.pop()
         count--
     }
-    let leading = true
     let ans = []
-    for(let ch of stack) {
-        if(leading && ch == '0') continue
-        leading = false
-        ans.push(ch)
+    let startwithzero = true
+    for(let n of stack) {
+        if(n == 0 && startwithzero) {
+            continue
+        }
+        startwithzero = false
+        ans.push(n)
     }
     if(ans.length == 0) return "0"
-    return ans.join('')
+    return ans.join("")
 };
