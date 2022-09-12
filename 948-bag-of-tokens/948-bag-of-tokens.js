1c197ad144e6a17e4f2ae23fd33125b2492046bb
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-12 08:40:23 +0530

    Time: 65 ms (100.00%), Space: 43.4 MB (90.91%) - LeetHub

---
+++ 948-bag-of-tokens/948-bag-of-tokens.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} tokens
+ * @param {number} power
+ * @return {number}
+ */
+var bagOfTokensScore = function(tokens, power) {
+    let score = 0, max = 0
+    const n = tokens.length
+    let i = 0, j = n-1
+    tokens.sort((a,b) => {
+        return a - b
+    })
+    
+    while(i <= j) {
+        //console.log(score, power, i, j)
+        if(power >= tokens[i]) {
+            score++
+            power = power - tokens[i]
+            i++
+        }
+        else if(score >= 1) {
+            score--
+            power = power + tokens[j]
+            j--
+        } else {
+            //console.log("bye")
+            break
+        }
+        if(score > max) max = score
+    }
+    return max
+    
+};
