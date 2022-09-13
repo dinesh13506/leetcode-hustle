e2706ce7ad03df208cc5ef2dcdc1b8455d077fc4
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-13 16:04:10 +0530

    Time: 174 ms (82.35%), Space: 50.5 MB (90.59%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.js
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.js
@@ -6,68 +6,30 @@
 var maxNumberOfFamilies = function(n, reservedSeats) {
     let map = new Map()
     for(let rs of reservedSeats) {
-        let row = rs[0]
-        let col = rs[1]
+        let [row, col] = rs
         if(map.has(row)) {
-            map.get(row).add(col)
+            let vector = map.get(row) | (1 << col)
+            map.set(row, vector)
         } else {
-            map.set(row, new Set())
-            map.get(row).add(col)
+            let vector = (1 << col)
+            map.set(row, vector)
         }
     }
-    //console.log(map, map.size)
-    let emptyrows = n - map.size
-    let ans = emptyrows * 2
-    
-    
-    for(let rs of map.keys()) {
-        let col = map.get(rs)
-        let count = 2
-        for(let i=2; i <=9; i++) {
-            if(col.has(i)) {
-                count = 0
-                break
-            }
+    //console.log(map)
+    let ans = (n - map.size) * 2
+    for(let row of map.keys()) {
+        let cnt = 0
+        let vector = map.get(row)
+        if( (vector & 60) == 0) {
+            cnt = cnt + 1
         }
-        if(count == 2) {
-            //console.log("hey")
-            ans = ans + 2
-        } else {
-            count = 1
-            for(let i=4; i <=7; i++) {
-                if(col.has(i)) {
-                    count = 0
-                    break
-                }
-            }
-            if(count) {
-                ans = ans + count
-            }
-            else {
-               count = 1
-                for(let i=2; i <=5; i++) {
-                    if(col.has(i)) {
-                        count = 0
-                        break
-                    }
-                }
-                if(count) {
-                    ans = ans + count
-                } else {
-                    count = 1
-                    for(let i=6; i <=9; i++) {
-                        if(col.has(i)) {
-                            count = 0
-                            break
-                        }
-                    }
-                    if(count) {
-                        ans = ans + count
-                    }
-                }
-            }
+        if( (vector & 960) == 0) {
+            cnt = cnt + 1
         }
-        //console.log(ans)
+        if((vector & 240) == 0  && cnt == 0) {
+            cnt = cnt + 1
+        }
+        ans += cnt
     }
     return ans
 };
