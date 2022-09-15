1f51af6f0acd81f4ed9573a0faff0d0c3804290c
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-15 09:01:16 +0530

    Time: 794 ms (14.06%), Space: 74.6 MB (54.69%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.js
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.js
@@ -3,11 +3,11 @@
  * @return {number[]}
  */
 var findOriginalArray = function(changed) {
-    
+    const noResult = []
     const orig = []
     const n = changed.length
     if(n%2 != 0) {
-        return orig
+        return noResult
     }
     let map = new Map()
     for(let num of changed) {
@@ -16,20 +16,20 @@ var findOriginalArray = function(changed) {
         map.set(num, count)
     }
     changed.sort((a,b) => {return a - b})
-    let temp = []
+   
     for(let num of changed) {
         if(map.get(num) == 0) continue
         let double = 2 * num
         if(map.get(num) > 0 &&  map.get(double) > 0) {
-            temp.push(num)
+            orig.push(num)
             map.set(num, map.get(num) - 1)
             map.set(double, map.get(double) - 1)
         } else {
-            return orig
+            return noResult
         }
     }
     for(let key of map.keys()) {
-        if(map.get(key) > 0) return orig 
+        if(map.get(key) > 0) return noResult 
     }
-    return temp
+    return orig
 };
