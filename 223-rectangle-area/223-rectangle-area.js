ce751c3f861d0358734789c032b65537b349a4a1
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-15 17:31:58 +0530

    Time: 209 ms (37.76%), Space: 47.4 MB (32.65%) - LeetHub

--- 223-rectangle-area/223-rectangle-area.js
+++ 223-rectangle-area/223-rectangle-area.js
@@ -11,11 +11,7 @@
  */
 var computeArea = function(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2) {
     
-    let xarr = [ax1, ax2,bx1, bx2]
-    let yarr = [ay1, ay2, by1, by2]
-    xarr.sort((a,b) =>  { return a - b } )
-    yarr.sort((a,b) =>  { return a - b } )
-    
+   
     let sum = Math.abs(ax2-ax1) * Math.abs(ay2 - ay1)
     sum += Math.abs(bx2-bx1) * Math.abs(by2 - by1)
     
