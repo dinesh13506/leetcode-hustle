d68356115db3458961ba8fef83f45b1ba53eb26a
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-15 08:58:48 +0530

    Time: 569 ms (67.19%), Space: 75 MB (45.31%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.js
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.js
@@ -4,54 +4,37 @@
  */
 var findOriginalArray = function(changed) {
     
+    const orig = []
+    const n = changed.length
+    if(n%2 != 0) {
+        return orig
+    }
     let map = new Map()
     for(let num of changed) {
         let count = map.get(num) || 0
         count++
         map.set(num, count)
     }
-    changed.sort((a,b) => {
-        return a - b
-    })
+    changed.sort((a,b) => {return a - b})
+    //console.log(changed, map)
+    let temp = []
+    for(let num of changed) {
+        if(map.get(num) == 0) continue
+        let double = 2 * num
+        //console.log(map.get(num), map.get(2 * num))
+        if(map.get(num) > 0 &&  map.get(double) > 0) {
+            temp.push(num)
+            map.set(num, map.get(num) - 1)
+            map.set(double, map.get(double) - 1)
+        } else {
+            return orig
+        }
+    }
+    //console.log(map)
+    for(let key of map.keys()) {
+        if(map.get(key) > 0) return orig 
+    }
+    return temp
     
-    if(map.has(0) && map.get(0) % 2 != 0) {
-        return []
-    }
-    const n = changed.length
-    let ansmap = new Map()
-    for(let i = 0; i < n; i++) {
-        let x = changed[i], y = 2 * x
-        if(x == 0 || y == 0) {
-            continue
-        }
-        if(map.has(x) == false || map.get(x) <= 0) {
-            continue
-        }
-        if(map.has(y) == false || map.get(y) <= 0) {
-            return []
-        }
-        let min = Math.min(map.get(x),map.get(y))
-        ansmap.set(x, (ansmap.get(x) || 0) + min)
-        map.set(x, map.get(x) - min)
-        map.set(y, map.get(y) - min)
-    }
-    //console.log(ansmap)
-    let ans = []
-    if(map.has(0)) {
-        let count = map.get(0) / 2
-        while(count> 0) {
-            ans.push(0)
-            count--
-        }
-        map.delete(0)
-    }
     
-    for(let key of ansmap.keys()) {
-        let count = ansmap.get(key)
-        while(count> 0) {
-            ans.push(key)
-            count--
-        }
-    }
-    return ans
 };
