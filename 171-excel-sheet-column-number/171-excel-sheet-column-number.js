151a57d68f09e482bc6a8ddf9eabfaaff3b71d90
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-16 23:00:48 +0530

    Time: 118 ms (39.71%), Space: 42.3 MB (97.80%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.js
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.js
@@ -3,18 +3,11 @@
  * @return {number}
  */
 var titleToNumber = function(columnTitle) {
-    
-    let map = new Map()
-    for(let i = 0; i < 26; i++) {
-        let asc = i + 65
-        let ch = String.fromCharCode(asc)
-        map.set(ch,i+1)
-    }
-    //console.log(map)
-    let n = columnTitle.length, ans = 0
-    for(let i = 0; i < n; i++) {
-        let asc = map.get(columnTitle[n-1-i]) * Math.pow(26,i)
-        ans = ans + asc
+    let pow = 0
+    let ans = 0
+    for(let i = columnTitle.length - 1; i>= 0; i--) {
+        ans += (columnTitle[i].charCodeAt() - 'A'.charCodeAt() + 1 ) * Math.pow(26, pow)
+        pow++
     }
     return ans
 };
