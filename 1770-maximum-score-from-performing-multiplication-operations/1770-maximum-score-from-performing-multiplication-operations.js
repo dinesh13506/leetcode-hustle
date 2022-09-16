ef89aef57b1f36487ac0fc84b7bc40dc26e05ebf
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-16 22:48:03 +0530

    Time: 426 ms (61.60%), Space: 84.1 MB (78.40%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.js
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.js
@@ -14,16 +14,20 @@ var maximumScore = function(nums, multipliers) {
         dp[i].fill(0)
     }
     
+    let nextrow = new Array(m+1)
+    nextrow.fill(0)
     for(let mindex = m-1; mindex >= 0; mindex--) {
+        let currrow = new Array(m+1)
+        currrow.fill(0)
         for(let left = m-1; left>=0 ; left--) {
             let right = (n-1) - (mindex - left)
-            let l = nums[left] * multipliers[mindex] + dp[mindex + 1][left + 1]
-            let r = nums[right] * multipliers[mindex] + dp[mindex + 1][left]
-            dp[mindex][left] =  Math.max(l,r)
+            let l = nums[left] * multipliers[mindex] + nextrow[left + 1]
+            let r = nums[right] * multipliers[mindex] + nextrow[left]
+            currrow[left] =  Math.max(l,r)
         }
+        nextrow = currrow
     }
-    
-    return dp[0][0]
+    return nextrow[0]
 };
 
 
