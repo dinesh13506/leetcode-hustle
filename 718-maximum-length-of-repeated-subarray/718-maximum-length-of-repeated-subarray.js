2932e2b11c3f672512647ce04b06b7f639a6cc24
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-20 17:26:14 +0530

    Time: 360 ms (76.36%), Space: 88.4 MB (48.73%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.js
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.js
@@ -15,29 +15,10 @@ var findLength = function(nums1, nums2) {
     }
     
     let max = 0
-    // case 1 if length of both is zero
-    dp[0][0] = 0 //nothing common
-    
-    //case 2 nums2 length is 1
-    for(let i=1; i <= m; i++) {
-        if(nums1[i-1] == nums2[0]) {
-            dp[i][1] = 1
-            max = 1
-        }
-    }
-    
-    //case 3 nums1 length is 1
-    for(let j=1; j <= n; j++) {
-        if(nums1[0] == nums2[j]) {
-            dp[1][j] = 1
-            max = 1
-        }
-    }
-    
-    for(let i=1; i <= m; i++) {
-        for(let j=1; j <= n; j++) {
-            if(nums1[i-1] == nums2[j-1]) {
-                dp[i][j] = 1 + dp[i-1][j-1]
+    for(let i=m-1; i >=0; i--) {
+        for(let j = n-1; j >=0; j--) {
+            if(nums1[i] == nums2[j]) {
+                dp[i][j] = 1 + dp[i+1][j+1]
                 max = Math.max(max, dp[i][j])
             }
         }
