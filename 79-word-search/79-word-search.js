4d1472a3bac632228fa705275eb37cd90ff014c6
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-22 00:19:39 +0530

    Time: 3208 ms (11.71%), Space: 49.1 MB (16.58%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -3,36 +3,75 @@
  * @param {string} word
  * @return {boolean}
  */
+class TrieNode {
+    constructor() {
+        this.children = new Array(26)
+        this.children.fill(null)
+        this.isend = false
+    }
+    
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode()
+    }
+    insert(w) {
+        let parent = this.root
+        for(let ch of w) {
+            let idx = ch.charCodeAt() - 'a'.charCodeAt()
+            if(parent.children[idx] == null) {
+                parent.children[idx] = new TrieNode()
+            }
+            parent = parent.children[idx]
+        }
+        parent.isend = true
+        
+    }
+}
 var exist = function(board, word) {
     
-    let m = board.length, n = board[0].length
-    let backtrack = function(row,col,index) {
-        if(index>= word.length) {
-            return true
+    let trie = new Trie()
+    trie.insert(word)
+    
+    const m = board.length, n = board[0].length
+    let ans = []
+    let dfs = (i,j,curr) => {
+        let letter = board[i][j]
+        let idx = letter.charCodeAt() - 'a'.charCodeAt()
+        curr = curr.children[idx]
+        if(curr.isend) {
+            ans.push(true)
+            return
         }
-        if(row < 0 || row >= m || col < 0 || col >=n || board[row][col] != word[index]) {
-            return false
-        }
-        
-        let ret = false
-        let rowOffsets = [0,1,0,-1]
-        let colOffsets = [1,0,-1,0]
-        board[row][col] = '#'
-        for(let d = 0 ; d < 4; d++) {
-            ret = backtrack(row+rowOffsets[d],col+colOffsets[d],index+1)
-            if(ret) {
-                break
+        board[i][j] = '#'
+        let dir = [
+            [1,0],
+            [-1,0],
+            [0,1],
+            [0,-1]
+        ]
+        for(let d of dir) {
+            let x = i + d[0]
+            let y = j + d[1]
+            if(x < 0 || y < 0 || x >= m || y >= n) continue
+            let letter = board[x][y]
+            let idx = letter.charCodeAt() - 'a'.charCodeAt()
+            if(curr.children[idx]) {
+                dfs(x,y,curr)
             }
         }
-        board[row][col] = word[index]
-        return ret
+        board[i][j] = letter
     }
     for(let i = 0; i < m; i++) {
-        for(let j = 0; j <n; j++) {
-            if(backtrack(i,j,0)) {
-                return true
+        for(let j = 0; j < n; j++) {
+            let letter = board[i][j]
+            let idx = letter.charCodeAt() - 'a'.charCodeAt()
+            if(trie.root.children[idx]) {
+                dfs(i,j,trie.root)
             }
         }
     }
-    return false
+    
+    return ans.length >=1 ? true : false
 };
