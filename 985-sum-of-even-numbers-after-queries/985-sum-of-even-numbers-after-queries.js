467c0da7adfbfd8069ed3d9ba953fae3961cf1a5
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-22 00:52:26 +0530

    Time: 235 ms (45.45%), Space: 51.4 MB (13.64%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.js
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.js
@@ -21,17 +21,15 @@ var sumEvenAfterQueries = function(nums, queries) {
         let [value, idx] = query
         let valMod = mod(value, 2)
         let numMod = mod(nums[idx], 2)
-        if( (numMod % 2 == 0) && (valMod == 0) ) {
-            total += value
-        }
-        else if( (numMod % 2 == 1) && (valMod == 1) ) {
-            total = total + value + nums[idx]
-        }
-        else if( (numMod % 2 == 0) && (valMod == 1) ) {
+        if(numMod == 0) {
             total -= nums[idx]
         }
-        ans.push(total)
         nums[idx] += value
+        numMod = mod(nums[idx], 2)
+        if(numMod == 0) {
+            total += nums[idx]
+        }
+        ans.push(total)
     }
     return ans
 };
