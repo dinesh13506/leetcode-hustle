7212a90123b09b728fc171300df25f5ee08f7016
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-23 16:09:42 +0530

    Time: 89 ms (53.22%), Space: 42.3 MB (25.22%) - LeetHub

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -6,16 +6,14 @@
 var strStr = function(haystack, needle) {
     
     for(let i = 0; i <= haystack.length - needle.length; i++) {
-        let found = true
         let j = 0
         while(j < needle.length) {
             if(haystack[i+j] != needle[j]) {
-                found = false
                 break
             }
             j++
         }
-        if(found) return i
+        if( j == needle.length ) return i
     }
     return -1
 };
