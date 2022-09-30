482d19cc6f726dfcd835517d1535d2cb12fc8441
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-09-30 18:01:55 +0530

    Time: 133 ms (37.83%), Space: 45.2 MB (46.02%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.js
+++ 994-rotting-oranges/994-rotting-oranges.js
@@ -4,49 +4,53 @@
  */
 var orangesRotting = function(grid) {
     
-    let time = 0
     let m = grid.length, n = grid[0].length
+    
     let queue = new Queue()
-    let fresh = 0
-    for(let i = 0; i < m; i++) {
-        for(let j = 0; j < n; j++) {
-            if(grid[i][j] == 2) {
-                queue.enqueue([i,j])
-            } else if(grid[i][j] == 1) {
-                fresh++
+    let time = -1
+    for(let r = 0; r < m; r++) {
+        for(let c = 0; c < n; c++) {
+            if(grid[r][c] == 2) {
+                queue.enqueue([r,c])
             }
         }
     }
+    time++
     
-    if(fresh == 0 && queue.isEmpty()) {
-        return 0
-    }
-    
-    let dirs = [
+    const dir = [
         [-1,0],
         [1,0],
         [0,1],
         [0,-1]
     ]
+    
     while(queue.isEmpty() == false) {
-        time++
         let temp = new Queue()
         while(queue.isEmpty() == false) {
-            let node = queue.dequeue()
-            for(let dir of dirs) {
-                let x = node[0] + dir[0], y = node[1] + dir[1]
-                if(x < 0 || x >=m || y <0 || y>= n) {
+            let [r,c] = queue.dequeue()
+            for(let d of dir) {
+                let x = d[0] + r
+                let y = d[1] + c
+                if(x < 0 || x >= m || y < 0 || y >= n) {
                     continue
                 }
                 if(grid[x][y] == 1) {
                     grid[x][y] = 2
-                    fresh--
                     temp.enqueue([x,y])
                 }
+                
             }
         }
-        queue = temp 
+        queue = temp
+        time++
     }
     
-    return fresh == 0 ? time - 1 : -1
+    for(let r = 0; r < m; r++) {
+        for(let c = 0; c < n; c++) {
+            if(grid[r][c] == 1) {
+                return -1
+            }
+        }
+    }
+    return time > 0 ? time - 1 : 0
 };
