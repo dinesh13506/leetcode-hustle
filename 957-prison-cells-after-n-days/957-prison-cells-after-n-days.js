aaf7c28e4173ffa5d75f3f0520d641d4d436d880
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-01 23:17:48 +0530

    Time: 140 ms (9.48%), Space: 44.4 MB (20.85%) - LeetHub

---
+++ 957-prison-cells-after-n-days/957-prison-cells-after-n-days.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} cells
+ * @param {number} n
+ * @return {number[]}
+ */
+var prisonAfterNDays = function(cells, n) {
+    let seen = new Map()
+    while(n > 0) {
+        let state = cells.join('')
+        seen.set(state, n)
+        let copy = cells.slice()
+        copy[0] = 0
+        copy[7] = 0
+        for(let i = 1; i < 7; i++) {
+            copy[i] = cells[i-1] == cells[i+1] ? 1 : 0
+        }
+        cells = copy
+        n--
+        state = cells.join('')
+        if(seen.has(state)) {
+            n %= (seen.get(state) - n)
+        }
+    }
+    return cells
+    
+};
