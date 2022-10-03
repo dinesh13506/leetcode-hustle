178b0f901022f4829c9d9150193641921e280b40
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-03 14:42:03 +0530

    Time: 114 ms (37.10%), Space: 44.7 MB (51.61%) - LeetHub

--- 734-sentence-similarity/734-sentence-similarity.js
+++ 734-sentence-similarity/734-sentence-similarity.js
@@ -8,41 +8,34 @@ var areSentencesSimilar = function(sentence1, sentence2, similarPairs) {
     
     let map = new Map()
     for(let pair of similarPairs) {
-        if(map.has(pair[0])) {
-            map.get(pair[0]).add(pair[1])
-        } else {
-            map.set(pair[0], new Set())
-            map.get(pair[0]).add(pair[1])
+        let [w1, w2] = pair
+        if(map.has(w1) == false) {
+            map.set(w1, [])
         }
-        if(map.has(pair[1])) {
-            map.get(pair[1]).add(pair[0])
-        } else {
-            map.set(pair[1], new Set())
-            map.get(pair[1]).add(pair[0])
+        if(map.has(w2) == false) {
+            map.set(w2, [])
         }
-        
+        map.get(w1).push(w2)
+        map.get(w2).push(w1)
     }
-    // console.log(sentence1)
-    // console.log(sentence2)
-    // console.log(map)
+    
+    
+    
+    
     
     if(sentence1.length != sentence2.length) return false
+    const n = sentence1.length
     
-    for(let i = 0; i < sentence1.length; i++) {
-        if(sentence1[i] == sentence2[i]) {
-            continue
-        }
-        else if(map.get(sentence1[i]) && map.get(sentence1[i]).has(sentence2[i])) {
-            continue
-        }
-        else if(map.get(sentence2[i]) && map.get(sentence2[i]).has(sentence1[i])) {
-            continue
-        }
-        // else if(map.get(sentence1[i]) == map.get(sentence2[i])) {
-        //     continue
-        // }
-        return false
+    for(let i = 0; i < n; i++) {
+        let w1 = sentence1[i], w2 = sentence2[i]
+        if(w1 == w2) continue
         
+        if(map.has(w1) && map.get(w1).includes(w2) == false) {
+            return false
+        }
+        if(map.has(w2) && map.get(w2).includes(w1) == false) {
+            return false
+        }
     }
     return true
 };
