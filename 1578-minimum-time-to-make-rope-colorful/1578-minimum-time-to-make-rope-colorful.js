456e96168431dc226c33a96422075a39f7db02d7
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-03 10:33:48 +0530

    Time: 332 ms (6.89%), Space: 101.2 MB (5.52%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} colors
+ * @param {number[]} neededTime
+ * @return {number}
+ */
+var minCost = function(colors, neededTime) {
+    
+    let stack = []
+    const n = colors.length
+    for(let i = 0; i < n; i++) {
+        if(stack.length == 0) {
+            stack.push([ colors[i], [ neededTime[i] ], neededTime[i], neededTime[i] ])
+        } else {
+            let top = stack[stack.length - 1]
+            let topColor = top[0]
+            if(colors[i] == topColor) {
+                
+                top[1].push(neededTime[i])
+                top[2] += neededTime[i]
+                top[3] = Math.max(top[3], neededTime[i])
+            } else {
+                stack.push([ colors[i], [ neededTime[i] ],neededTime[i], neededTime[i] ])
+            }
+        }
+    }
+    
+    //console.log(stack)
+    let ans = 0
+    while(stack.length) {
+        let top = stack.pop()
+        if(top[1].length > 0) {
+            ans += (top[2] - top[3])
+        }
+    }
+    return ans
+};
