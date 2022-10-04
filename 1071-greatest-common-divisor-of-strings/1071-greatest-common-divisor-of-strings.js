d5a9254d0bee00ade0e0217bc1fb283693c194cf
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-04 22:54:30 +0530

    Time: 56 ms (98.68%), Space: 42 MB (89.43%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -5,32 +5,13 @@
  */
 var gcdOfStrings = function(str1, str2) {
     
-    let ans = ""
-    let len1 = str1.length, len2 = str2.length
-    let len = len1 > len2 ? len2 : len1
-    let prefix1 = "", prefix2 = ""
-    for(let i = 0; i < len; i++) {
-        prefix1 = prefix1 + str1[i]
-        prefix2 = prefix2 + str2[i]
-        let l = i + 1
-        if(prefix1 == prefix2 && (len1 % l == 0) && (len2 % l == 0)) {
-            let c1 = len1 / l
-            let c2 = len2 / l
-            let s1 = ""
-            while(c1 > 0) {
-                s1 = s1 + prefix1
-                c1--
-            }
-            let s2 = ""
-            while(c2 > 0) {
-                s2 = s2 + prefix2
-                c2--
-            }
-            if(s1 == str1 && s2 == str2) {
-                ans = ans.length < prefix1.length ? prefix1 : ans
-            }
-        }
-        
+    let gcd = (a,b) => {
+        if(b == 0) return a
+        return gcd(b, a%b)
     }
-    return ans
+    if(str1 + str2 == str2 + str1) {
+        return str1.substring(0, gcd(str1.length, str2.length))
+    } else {
+        return ""
+    }
 };
