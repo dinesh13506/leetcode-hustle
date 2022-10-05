029129842506ab923a079cc430f5e31b682875ed
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-05 13:19:09 +0530

    Time: 105 ms (52.85%), Space: 43.9 MB (43.44%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.js
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.js
@@ -5,53 +5,31 @@
  */
 var targetIndices = function(nums, target) {
     
-    nums.sort(function(num1,num2) { return num1-num2})
-    let result = []
-    
-    let getFirstOccurance = function() {
-        let start = 0, end = nums.length - 1
-        let res = -1
+    nums.sort((a,b) =>  { return a - b } )
+    let n = nums.length
+    let bs = () => {
+        let start = 0, end = n-1
+        let index = Infinity
         while(start <= end) {
-            let mid = start + parseInt((end - start)/2)
+            let mid = start + parseInt((end - start) / 2)
             if(nums[mid] == target) {
-                res = mid
+                index = Math.min(mid, index)
                 end = mid - 1
-            }
-            else if(target < nums[mid]) {
+            } else if(target < nums[mid]) {
                 end = mid - 1
-            }
-            else if(target > nums[mid]) {
+            } else if(target > nums[mid]) {
                 start = mid + 1
             }
         }
-        return res
+        return index
     }
     
-    let getLastOccurance = function() {
-        let start = 0, end = nums.length - 1
-        let res = -1
-        while(start <= end) {
-            let mid = start + parseInt((end - start)/2)
-            if(nums[mid] == target) {
-                res = mid
-                start = mid + 1
-            }
-            else if(target < nums[mid]) {
-                end = mid - 1
-            }
-            else if(target > nums[mid]) {
-                start = mid + 1
-            }
-        }
-        return res
+    let low = bs()
+    //console.log(low)
+    let ans = []
+    while(low < n && nums[low] == target) {
+        ans.push(low)
+        low++
     }
-    
-    let first = getFirstOccurance()
-    let last = getLastOccurance()
-    if(first != -1 && last != -1) {
-        for(let i = first; i <= last; i++) {
-            result.push(i)
-        }
-    }
-    return result
+    return ans
 };
