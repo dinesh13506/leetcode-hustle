a7ebd7f833b94ac6ae5c577dd09b379a14f50246
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-05 17:10:59 +0530

    Time: 124 ms (86.27%), Space: 49.4 MB (40.29%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.js
@@ -0,0 +1,61 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @param {number} target
+ * @return {number}
+ */
+// var numRollsToTarget = function(n, k, target) {
+//     const mod = BigInt(10) ** BigInt(9) + BigInt(7)
+//     let memo = new Array(n)
+//     for(let i = 0; i < n ; i++) {
+//         memo[i] = new Array(target+1)
+//     }
+    
+//     let dp = (index, sum) => {
+//         if(sum > target || index > n) {
+//             return 0n
+//         }
+//         if(index == n) {
+//             return sum == target ? 1n : 0n
+//         }
+//         if(memo[index][sum]) return memo[index][sum]
+        
+//         let ways = 0n
+//         for(let i = 1; i <= Math.min(k, target); i++) {
+//             ways = ways + dp(index + 1, sum + i)
+//         }
+//         memo[index][sum] =  ways
+//         return ways
+//     }
+//     let ans = dp(0,0)
+//     //console.log(ans)
+//     return ans % mod
+// };
+
+var numRollsToTarget = function(d, f, target) {
+    
+    const MOD = BigInt(10) ** BigInt(9) + BigInt(7); // 10^9 + 7.
+    const max = Math.min(f, target);
+    const dp = [];
+    
+    const helper = (rest, length) => {
+       
+        if(length === d && rest === 0) return BigInt(1);
+        if(length > d || rest < 0) return BigInt(0);
+        
+        if(dp[length]?.[rest] !== undefined) return dp[length][rest];
+        
+        let sum = BigInt(0);
+        
+        for(let k = 1; k<= max; k++){
+            sum += helper(rest - k, length + 1);
+        }
+        
+        dp[length] = dp[length] || [];
+        dp[length][rest] = sum;
+        
+        return sum % MOD;
+    };
+    
+    return helper(target, 0) % MOD;
+};
