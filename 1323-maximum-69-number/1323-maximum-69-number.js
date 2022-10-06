b193782adc207f3e4a80fddd38ac6ec1fa8590be
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-06 18:58:16 +0530

    Time: 102 ms (36.49%), Space: 42.3 MB (20.79%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.js
+++ 1323-maximum-69-number/1323-maximum-69-number.js
@@ -3,14 +3,13 @@
  * @return {number}
  */
 var maximum69Number  = function(num) {
-    //find first 6
-    let s = "" + num
-    let arr = s.split('')
-    for(let i = 0; i< arr.length; i++) {
-        if(arr[i] == '6') {
-            arr[i] = '9'
+    
+    let s = num.toString().split('')
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] == '6') {
+            s[i] = '9'
             break
-        }
+        } 
     }
-    return parseInt(arr.join(''))
+    return parseInt(s.join(''))
 };
