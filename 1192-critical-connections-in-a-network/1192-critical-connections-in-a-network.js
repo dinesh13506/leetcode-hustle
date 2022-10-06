c269941e0ef97a2644cb733a778cc4d88721ab25
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-06 16:22:33 +0530

    Time: 765 ms (86.09%), Space: 112 MB (86.96%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.js
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.js
@@ -4,41 +4,56 @@
  * @return {number[][]}
  */
 var criticalConnections = function(n, connections) {
-  const adjacency = {}
-  for (let i = 0; i < n; i++) {
-    adjacency[i] = []
-  }
-
-  for (let [node1, node2] of connections) {
-    adjacency[node1].push(node2)
-    adjacency[node2].push(node1)
-  }
-
-  const dfsNumber = new Array(n).fill(0)
-  // this is what we called oldestReachable
-  // the lowest number reachable by dfs from a node
-  const dfsLow = new Array(n).fill(0)
-  const criticalEdges = []
-  let timestamp = 0
-
-  function tarjan(node, parent) {
-    timestamp++
-    dfsNumber[node] = timestamp
-    dfsLow[node] = timestamp
-
-    for (let neighbor of adjacency[node]) {
-      if (neighbor === parent) continue
-      if (!dfsNumber[neighbor]) tarjan(neighbor, node)
-
-      // resetting oldestReachable if neighbor can cycle back
-      dfsLow[node] = Math.min(dfsLow[node], dfsLow[neighbor])
-
-      // if neighbor cannot reach back to me or oldest, we have a critical edge
-      if (dfsLow[neighbor] > dfsNumber[node]) criticalEdges.push([node, neighbor])
+    
+    let visited = new Array(n)
+    visited.fill(false)
+    let parent = new Array(n)
+    parent.fill(null)
+    let discoveryTime = new Array(n)
+    let mindiscoveryTime = new Array(n)
+    mindiscoveryTime.fill(Infinity)
+    
+    let adjList = new Map()
+    for(let [u,v] of connections) {
+        if(adjList.has(u) == false) {
+            adjList.set(u,[])
+        }
+        if(adjList.has(v) == false) {
+            adjList.set(v,[])
+        }
+        adjList.get(u).push(v)
+        adjList.get(v).push(u)
     }
-  }
-
-  tarjan(0, null, adjacency, criticalEdges)
-
-  return criticalEdges
-}
+    
+    
+    let ans = []
+    let time = 0
+    
+    let dfs = (u) => {
+        if(visited[u]) {
+            return
+        }
+        visited[u] = true
+        time++
+        discoveryTime[u] = time
+        mindiscoveryTime[u] = time
+        
+        for(let v of adjList.get(u)) {
+            if(visited[v] == false) {
+                parent[v] = u
+                dfs(v)
+                mindiscoveryTime[u] = Math.min(mindiscoveryTime[u], mindiscoveryTime[v])
+                if(discoveryTime[u] < mindiscoveryTime[v]) {
+                    ans.push([u,v])
+                }
+            } else {
+                if(parent[u] != v) {
+                    mindiscoveryTime[u] = Math.min(mindiscoveryTime[u], mindiscoveryTime[v])
+                }
+            }
+        }
+    }
+    
+    dfs(0)
+    return ans
+};
