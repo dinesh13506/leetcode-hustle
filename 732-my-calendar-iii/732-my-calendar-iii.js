40874994c2f9d14741c7d59126577648c6ed44b2
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-08 00:09:29 +0530

    Time: 692 ms (64.29%), Space: 54.8 MB (64.29%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.js
+++ 732-my-calendar-iii/732-my-calendar-iii.js
@@ -1,6 +1,82 @@
+class ListNode {
+    constructor(val) {
+        this.val = val
+        this.prev = null
+        this.next = null
+    }
+}
+
+class SortedLinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+    }
+    
+    insert(val) {
+        let node =  new ListNode(val)
+        //console.log(val)
+        if(this.head == null) {
+            this.head = node
+            this.tail = node
+        } else {
+            let p = this.head
+            let prev = null
+            while(p) {
+                if(p.val >= val) {
+                    break
+                }
+                prev = p
+                p = p.next
+            }
+            if(prev == null) {
+                this.head.prev = node
+                node.next = this.head
+                this.head = node
+            } else {
+                node.next = prev.next
+                node.prev = prev
+                if(node.next) {
+                    node.next.prev = node
+                }
+                prev.next = node
+            }
+            
+        }
+        return node
+    }
+    
+    delete(node) {
+        if(node == this.head) {
+            this.head = this.head.next
+        } else {
+            if(node.next) {
+                node.next.prev = node.prev
+            }
+            if(node.prev) {
+                node.prev.next = node.next
+            }
+        }
+    }
+    
+    getkeys() {
+        let p = this.head
+        let set = new Set()
+        while(p) {
+            if(set.has(p.val) == false) {
+                set.add(p.val)
+            }
+            
+            p = p.next
+        }
+        //console.log("list => ", set)
+        return Array.from(set)
+    }
+}
+
 
 var MyCalendarThree = function() {
-    this.diff = new Map()
+    this.map = new Map()
+    this.sortedLL = new SortedLinkedList()
 };
 
 /** 
@@ -10,13 +86,16 @@ var MyCalendarThree = function() {
  */
 MyCalendarThree.prototype.book = function(start, end) {
     
-    this.diff.set(start, (this.diff.get(start) || 0 ) + 1)
-    this.diff.set(end, (this.diff.get(end) || 0 ) - 1)
+    let startNode = this.sortedLL.insert(start)
+    let endNode = this.sortedLL.insert(end)
+    this.map.set(start, (this.map.get(start) || 0) + 1)
+    this.map.set(end, (this.map.get(end) || 0) - 1)
+    //console.log(this.map)
     let ans = 0, cur = 0
-    let keys = [...this.diff.keys()]
-    keys.sort((a,b) => { return a - b})
+    let keys = this.sortedLL.getkeys()
     for(let time of keys) {
-        cur += this.diff.get(time)
+        //console.log(this.map.get(time))
+        cur += this.map.get(time)
         ans = Math.max(ans, cur)
     }
     return ans
