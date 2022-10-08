e81615156ec7ab6bf7f61c7d1bdcfdefa05599ed
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-08 11:41:46 +0530

    Time: 8396 ms (5.02%), Space: 128.1 MB (5.02%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.js
+++ 16-3sum-closest/16-3sum-closest.js
@@ -4,64 +4,30 @@
  * @return {number}
  */
 var threeSumClosest = function(nums, target) {
-    
-    let n = nums.length
-    nums.sort((a,b) => { return a - b })
-    
-    
-    let lowerbound = (lo,hi, target) => {
-        let lb = nums[lo], ub = nums[hi]
-        while(lo <= hi) {
-            let mid = lo + parseInt((hi - lo) / 2)
-            if(nums[mid] == target) {
-                return [target, target]
-            }
-            else if(target < nums[mid]) {
-                ub = nums[mid]
-                hi = mid - 1
-            } else if(target > nums[mid]) {
-                lb = nums[mid]
-                lo = mid + 1
-            }
-        }
-        return [lb,ub]
-    }
-    
-    let upperbound = (lo,hi, target) => {
-        let ans = nums[hi]
-        while(lo <= hi) {
-            let mid = lo + parseInt((hi - lo) / 2)
-            if(nums[mid] == target) {
-                ans = target
-                return ans
-            }
-            else if(target < nums[mid]) {
-                ans = nums[mid]
-                hi = mid - 1
-            } else if(target > nums[mid]) {
-                lo = mid + 1
+    const n = nums.length
+    nums.sort((a,b) => {
+        return a - b
+    })
+    let sums = []
+    for(let i= 0; i < n; i++) {
+        let first = nums[i]
+        let sum = target - first
+        let p1 = i + 1, p2 = n-1
+        let closest = Infinity
+        while(p1 < p2) {
+            let temp = nums[p1] + nums[p2]
+            sums.push(temp + first)
+            if(temp <= sum) {
+                p1++
+            } else if(temp > sum) {
+                p2--
             }
         }
-        return ans
     }
     
-    let diff = Infinity
-    for(let i = 0; i < n; i++) {
-        for(let j = i + 1; j < n - 1; j++) {
-            let third = target - nums[i] - nums[j]
-            //find third in j + 1 to n - 1
-            let [lb,ub] = lowerbound(j + 1, n - 1, third)
-            //let ub = upperbound(j + 1, n - 1, third)
-            //console.log(lb, ub)
-            if(Math.abs(third - lb) < Math.abs(diff)) {
-                diff =  third - lb
-            }
-            if(Math.abs(third - ub) < Math.abs(diff)) {
-                diff =  third - ub
-            }
-            if(diff == 0) return target
-        }
-    }
-    //console.log(diff)
-    return target - diff
+    sums.sort((a,b) => {
+        return Math.abs(a - target) - Math.abs(b - target)
+    })
+    //console.log(sums)
+    return sums[0]
 };
