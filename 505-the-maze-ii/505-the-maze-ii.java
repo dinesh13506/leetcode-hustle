6d35ca962def4a38be93d66c0aae227a7b0ed5d3
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-10 00:08:32 +0530

    Time: 10 ms (84.78%), Space: 54.6 MB (33.51%) - LeetHub

---
+++ 505-the-maze-ii/505-the-maze-ii.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int shortestDistance(int[][] maze, int[] start, int[] dest) {
+        final int m = maze.length;
+        final int n = maze[0].length;
+        ArrayList<int[]> directions = new ArrayList();
+        directions.add(new int[]{-1,0});
+        directions.add(new int[]{1,0});
+        directions.add(new int[]{0,-1});
+        directions.add(new int[]{0, 1});
+        
+        Queue<int[]> queue= new LinkedList<int[]>();
+        int[][] distance = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                distance[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        
+        queue.add(new int[] {start[0], start[1]});
+        distance[start[0]][start[1]] = 0;
+        while(queue.isEmpty() == false) {
+            int[] node = queue.poll();
+            int x = node[0];
+            int y = node[1];
+            for(int[] dir : directions) {
+                int a = x;
+                int b = y;
+                int count = 0;
+                while(true) {
+                    int newa = a + dir[0];
+                    int newb = b + dir[1];
+                    if(newa < 0 || newb <0 || newa >= m || newb >= n) {
+                        break;
+                    }
+                    if(maze[newa][newb] == 1) {
+                        break;
+                    }
+                    a = newa;
+                    b = newb;
+                    count++;
+                }
+                if(distance[x][y] + count < distance[a][b]) {
+                    distance[a][b] = distance[x][y] + count;
+                    queue.add(new int[] {a,b});
+                }
+            }
+        }
+        return distance[dest[0]][dest[1]] == Integer.MAX_VALUE ? -1 : distance[dest[0]][dest[1]];
+         
+    }
+}
