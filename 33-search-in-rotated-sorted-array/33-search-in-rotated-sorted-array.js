35eac1a1d32cd23bff3100be63d7ac60a19a6945
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-10 17:20:48 +0530

    Time: 69 ms (88.19%), Space: 41.8 MB (85.08%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -5,20 +5,56 @@
  */
 var search = function(nums, target) {
     
-    let start = 0, end = nums.length - 1
-    while(start <= end) {
-        let mid = start + parseInt((end - start)/2)
-        if(nums[mid] == target) {
-            return mid
+    const n = nums.length;
+    
+    let findPivot = () => {
+        let start = 0, end = n - 1;
+        while(start <= end) {
+            let mid = start;
+            mid += parseInt((end - start) / 2)
+            if(nums[mid] > nums[mid+1]) {
+                return mid + 1
+            }
+            else if(nums[start] <= nums[mid]) {
+                start = mid + 1
+            }
+            else if(nums[end] >= nums[mid]) {
+                end = mid - 1
+            }
         }
-        else if(nums[start] <= nums[mid]) {
-            if(nums[start] <= target && target < nums[mid]) end = mid - 1
-            else start = mid + 1
-        }
-        else {
-            if(nums[mid] < target && target <= nums[end]) start = mid + 1
-            else end = mid - 1
-        }
+        return 0
     }
-    return -1
+    
+    
+    let bs = (start, end) => {
+        while(start <= end) {
+            let mid = start;
+            mid += parseInt((end - start) / 2)
+            if(nums[mid] == target) {
+                return mid
+            } else if(target < nums[mid]) {
+                end = mid - 1
+            } else {
+                start = mid + 1
+            }
+        }
+        return -1
+    }
+    
+    let idx = findPivot()
+    //console.log(idx)
+    if(idx == 0) {
+        return bs(0, n-1)
+    } else {
+        let res = bs(0, idx-1)
+        if(res >= 0) {
+            return res
+        }
+        let res2 = bs(idx, n-1)
+        if(res2 >= 0) {
+            return res2
+        }
+        return -1
+    }
+    
 };
