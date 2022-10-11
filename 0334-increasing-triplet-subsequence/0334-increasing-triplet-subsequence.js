3b9c7ea774402da9c6162aaf071cd504d46f99ed
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-11 10:39:11 +0530

    Time: 226 ms (5.11%), Space: 71.5 MB (5.11%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
@@ -4,16 +4,22 @@
  */
 var increasingTriplet = function(nums) {
     
-    let value1 = Infinity, value2 = Infinity
+    let left = []
+    let right = []
     
-    for(let num of nums) {
-        if(num < value1) {
-            value1 = num
-        }
-        else if(num > value1 && num < value2) {
-            value2 = num
-        }
-        else if(num > value2) {
+    let smallest_on_left = Infinity
+    for(let i = 0; i < nums.length; i++) {
+        left[i] = smallest_on_left
+        smallest_on_left = Math.min(smallest_on_left, nums[i])
+    }
+    let largest_on_right = -Infinity
+    for(let i = nums.length - 1; i >=0; i--) {
+        right[i] = largest_on_right
+        largest_on_right = Math.max(largest_on_right, nums[i])
+    }
+    //console.log(left, right)
+    for(let i = 0; i < nums.length; i++) {
+        if(left[i] < nums[i] && nums[i] < right[i]) {
             return true
         }
     }
