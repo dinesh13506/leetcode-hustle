5cddfa6cfdbccda7832ddee4e38e120e0952eb8e
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-11 09:25:38 +0530

    Time: 133 ms (24.02%), Space: 55.6 MB (17.42%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
@@ -3,38 +3,19 @@
  * @return {boolean}
  */
 var increasingTriplet = function(nums) {
-    const n = nums.length;
-    const tail = [];
     
-    let ceilIdx = (target) => {
-        let start = 0, end = tail.length-1;
-        let idx = end;
-        while(start <= end) {
-            let mid = start;
-            mid += parseInt((end - start) / 2);
-            if(tail[mid] == target) {
-                idx = mid;
-                return idx
-            }
-            else if(target < tail[mid]) {
-                idx = mid;
-                end = mid - 1;
-            }
-            else if(target > tail[mid]) {
-                start = mid + 1
-            }
+    let value1 = Infinity, value2 = Infinity
+    
+    for(let num of nums) {
+        if(num < value1) {
+            value1 = num
         }
-        return idx;
-    }
-    
-    tail.push(nums[0]);
-    for(let i = 1; i < n; i++) {
-        if(nums[i] > tail[tail.length - 1]) {
-            tail.push(nums[i]);
-        } else {
-            let idx = ceilIdx(nums[i]);
-            tail[idx] = nums[i];
+        else if(num > value1 && num < value2) {
+            value2 = num
+        }
+        else if(num > value2) {
+            return true
         }
     }
-    return tail.length >= 3
+    return false
 };
