469e38183503777d015b27b4691026f607eff15b
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-13 19:56:24 +0530

    Time: 104 ms (50.63%), Space: 44.8 MB (18.60%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    
+    let arr = [];
+    let set = new Set();
+    for(let num of nums) {
+        if(set.has(num)) {
+            continue;
+        }
+        arr.push(num);
+    }
+    
+    let first = -Infinity;
+    let second = -Infinity;
+    let third = -Infinity;
+    
+    for(let num of arr) {
+        if(num > first) {
+            third = second;
+            second = first;
+            first = num;
+        } else if(num < first && num > second) {
+            third = second;
+            second = num;
+        } else if(num < second && num > third) {
+            third = num;
+        }
+    }
+    
+    return third == -Infinity ? first : third;
+    
+};
