3de8cbf400939a5e5af9c735fc7c1d862a567c7c
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-13 23:05:50 +0530

    Time: 6 ms (92.08%), Space: 54.5 MB (93.50%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -3,21 +3,23 @@ class Solution {
         final int m = matrix.length;
         final int n = matrix.length > 0 ? matrix[0].length : 0;
         
-        int[][] dp = new int[m][n];
+        int[] prev = new int[n];
         int max = 0;
+        
         for(int i = 0; i < m; i++) {
+            int[] curr = new int[n];
             for(int j = 0; j < n; j++) {
                 if(matrix[i][j] == '0') {
-                    dp[i][j] = 0;
+                    curr[j] = 0;
                 } else {
-                    dp[i][j] = 1;
+                    curr[j] = 1;
                     if(i > 0 && j > 0)
-                        dp[i][j] = Math.min( Math.min(dp[i-1][j-1], dp[i-1][j]), dp[i][j-1]) + 1;
-                    max = Math.max(max,dp[i][j]);  
+                        curr[j] = Math.min( Math.min(prev[j-1], prev[j]), curr[j-1]) + 1;
+                    max = Math.max(max,curr[j]);  
                 }
             }
+            prev = curr;
         }
         return max * max;
-            
     }
 }
