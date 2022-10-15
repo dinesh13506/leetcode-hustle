696a53b6d30515f310caea4d3276b581276c1d13
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-15 22:42:43 +0530

    Time: 3 ms (7.13%), Space: 42.7 MB (61.59%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,8 +1,21 @@
-import java.math.*;
 class Solution {
+    public HashMap<Integer, Double> map; // = new HashMap<int, double>();
+    public double pow(double x, int n) {
+        if(n == 0) {
+            return 1;
+        }
+        if(map.containsKey(n)) {
+            return map.get(n);
+        }
+        if(n%2 == 0) {
+            map.put(n , this.pow(x, n / 2) * this.pow(x, n / 2));
+        } else {
+            map.put(n , x * this.pow(x, n / 2) * this.pow(x, n / 2));
+        }
+        return map.get(n);
+    }
     public double myPow(double x, int n) {
-        
-        
-        return Math.pow(x,n);
+       this.map = new HashMap<Integer, Double>();
+       return n >= 0 ? pow(x,n) : (double) 1 / pow(x,n);
     }
 }
