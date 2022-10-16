368749a84f5c185807001d7eecd3beb823e9abe3
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-16 21:01:05 +0530

    Time: 60 ms (14.33%), Space: 42 MB (49.21%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int minDifficulty(int[] jobDifficulty, int d) {
+
+        int n = jobDifficulty.length;
+        // Edge case: make sure there is at least one job per day
+        if (n < d) {
+            return -1;
+        }
+
+        int[][] mem = new int[n][d + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= d; j++) {
+                mem[i][j] = -1;
+            }
+        }
+
+        return minDiff(0, d, jobDifficulty, mem);
+    }
+
+    private int minDiff(int i, int daysRemaining, int[] jobDifficulty, int[][] mem) {
+        // Use memoization to avoid repeated computation of states
+        if (mem[i][daysRemaining] != -1) {
+            return mem[i][daysRemaining];
+        }
+
+        // Base case: finish all remaining jobs in the last day
+        if (daysRemaining == 1) {
+            int res = 0;
+            for (int j = i; j < jobDifficulty.length; j++) {
+                res = Math.max(res, jobDifficulty[j]);
+            }
+            return res;
+        }
+
+        int res = Integer.MAX_VALUE;
+        int dailyMaxJobDiff = 0;
+        // Iterate through possible starting index for the next day
+        // and ensure we have at least one job for each remaining day.
+        for (int j = i; j < jobDifficulty.length - daysRemaining + 1; j++) {
+            dailyMaxJobDiff = Math.max(dailyMaxJobDiff, jobDifficulty[j]);
+            res = Math.min(res, dailyMaxJobDiff + minDiff(j + 1, daysRemaining - 1, jobDifficulty, mem));
+        }
+        mem[i][daysRemaining] = res;
+        return res;
+    }
+}
