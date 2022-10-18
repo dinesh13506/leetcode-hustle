dd0595cd548187cf534dbcbf0986db4a92b99518
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-18 23:17:15 +0530

    Time: 111 ms (20.56%), Space: 43.1 MB (12.75%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,59 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function getLowestIndex(nums, target){
+    const n = nums.length;
+    let low = 0;
+    let high = n - 1;
+    let lowestIndex = Infinity;
+    
+    while(low <= high) {
+        let mid = parseInt((low + high) / 2);
+        if(nums[mid] == target) {
+            if(mid < lowestIndex) {
+                lowestIndex = mid;
+            }
+            high = mid - 1;
+        } else if(target < nums[mid]) {
+            high = mid - 1;
+        } else if(target > nums[mid]) {
+            low = mid + 1;
+        }
+    }
+    
+    return lowestIndex == Infinity ? -1 : lowestIndex;
+}
+
+
+function getHighestIndex(nums, target) {
+    const n = nums.length;
+    let low = 0;
+    let high = n - 1;
+    let highestIndex = -Infinity;
+    
+    while(low <= high) {
+        let mid = parseInt((low + high) / 2);
+        if(nums[mid] == target) {
+            if(mid > highestIndex) {
+                highestIndex = mid;
+            }
+            low = mid + 1;
+        } else if(target < nums[mid]) {
+            high = mid - 1;
+        } else if(target > nums[mid]) {
+            low = mid + 1;
+        }
+    }
+    
+    return highestIndex == -Infinity ? -1 : highestIndex;
+}
+
+
+var searchRange = function(nums, target) {
+    
+    return [getLowestIndex(nums, target), getHighestIndex(nums, target)]
+    
+    
+};
