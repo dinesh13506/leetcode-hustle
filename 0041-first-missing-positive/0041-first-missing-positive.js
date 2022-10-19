b884a0d68be911d3cfbc562b432d46c5856d027c
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-19 15:58:51 +0530

    Time: 74 ms (97.67%), Space: 55.8 MB (27.42%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.js
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -5,15 +5,18 @@
 var firstMissingPositive = function(nums) {
     
     let set = new Set();
+    let max = 0;
     for(let num of nums) {
+        max = Math.max(max, num);
         if(num > 0 && set.has(num) == false) {
             set.add(num);
         }
     }
-    let max = (2 ** 31) - 1;
+    //let max = (2 ** 31) - 1;
     for(let i = 1; i <= max; i++) {
         if(set.has(i) == false) {
             return i;
         }
     }
+    return max + 1;
 };
