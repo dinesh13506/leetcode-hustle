6019591e0e733e01883ad8e74561c23bfaa095df
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-20 00:01:22 +0530

    Time: 145 ms (5.22%), Space: 44.3 MB (44.98%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
@@ -3,44 +3,58 @@
  * @param {number} k
  * @return {number[]}
  */
-var kWeakestRows = function(mat, k) {
+function strength(mat, k){
+    const m = mat.length, n = mat[0].length;
+    let strengthMap = new Map();
+    function soldiers(r) {
+            let row = mat[r];
+            const target = 1;
+            let low = 0, high = n - 1;
+            while(low <= high){
+                let mid = low + parseInt( (high - low) / 2 );
+                if(row[mid] === target) { 
+                    if(row[mid+1] != row[mid])
+                        return mid + 1;
+                    else { 
+                        low = mid + 1;
+                    }  
+                }
+                else if(row[mid] < target){
+                    if(row[mid-1] != row[mid])
+                        return mid;
+                    else
+                        high = mid - 1;
+                }
+            }
+            return 0;
+    }
+    for(let r = 0; r < m; r++) {
+        strengthMap.set(r, soldiers(r));
+    }
     
-    let highestIndexOfOne = (row) => {
-        let start = 0;
-        let end = row.length - 1;
-        let index = -1;
-        
-        while(start <= end) {
-            let mid = start + parseInt((end - start)/2);
-            if(row[mid] == 0) {
-                end = mid - 1
+    const weakRows = [];
+    let rowIdxs = [...strengthMap.keys()];
+    rowIdxs.sort((r1, r2) => {
+        if(strengthMap.get(r1) < strengthMap.get(r2)) {
+            return -1; // r1, r2 , weakest -> strongest
+        }
+        else if(strengthMap.get(r1) > strengthMap.get(r2)) {
+            return 1; // r2, r1 weakest -> strongest
+        }
+        else if(strengthMap.get(r1)  == strengthMap.get(r2)) {
+            if(r1 < r2) {
+                return -1;
             } else {
-                index = Math.max(index, mid);
-                start = mid + 1;
+                return 1;
             }
         }
-        return index;
-    }
+    });
     
-    const m = mat.length;
-    let count = new Array(m);
-    for(let r = 0;  r < m; r++) {
-        let index = highestIndexOfOne(mat[r]);
-        //console.log(index);
-        count[r] = [index + 1, r];
-        
+    for(let r = 0; r < k; r++) {
+        weakRows.push(rowIdxs[r]);
     }
-    
-    count.sort((a,b) => {
-        if(a[0] == b[0]) {
-            return a[1] - b[1];
-        }
-        return a[0] - b[0];
-    })
-    //console.log(count)
-    let rows = [];
-    for(let i = 0; i < k; i++) {
-        rows.push(count[i][1]);
-    }
-    return rows;
+    return weakRows;
+}
+var kWeakestRows = function(mat, k) {
+    return strength(mat,k)
 };
