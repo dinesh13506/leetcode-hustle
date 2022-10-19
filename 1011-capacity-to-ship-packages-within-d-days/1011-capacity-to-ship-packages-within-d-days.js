7b3f43167379f6c3d26e304b27c4f263573ea914
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-20 00:43:14 +0530

    Time: 89 ms (91.70%), Space: 45.6 MB (53.28%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
@@ -5,46 +5,42 @@
  */
 var shipWithinDays = function(weights, days) {
     
-    let min_capacity = 0
-    let max_capacity = 0 
-    let sum = 0
-    for(let wt of weights) {
-        sum += wt
+    let min_capacity = -Infinity;
+    let max_capacity = 0;
+    for(let w of weights) {
+        max_capacity += w;
+        if( min_capacity < w ) {
+            min_capacity = w;
+        }
     }
-    max_capacity = sum
     
-    let canShip = function(cap) {
-        let target = cap
-        let days_taken = 1
-        let i = 0, sum = 0
-        while( i < weights.length) {
-            if(cap >= weights[i]) {
-                cap = cap - weights[i]
-            } else if(target < weights[i]) {
-                return false
+    //console.log(min_capacity, max_capacity);
+    
+    let ispossible = (capacity) => {
+        let d = 1, sum = 0;
+        for(let i = 0; i < weights.length ; i++) {
+            let w = weights[i];
+            if(sum + w > capacity) {
+                d++;
+                sum = 0;
             }
-            else {
-                days_taken++
-                cap = target
-                cap = cap - weights[i]
-            }
-            i++
+            sum = sum + w;
+            //console.log("days = " , d, sum)
         }
-        if(days_taken > days) {
-            return false
-        }
-        return true
+        //console.log(capacity, d,d <= days);
+        return d <= days ? true : false;
     }
-    let start = 0, end = max_capacity
-    let ans = end
-    while(start <= end) {
-        let mid = start + parseInt((end - start)/2)
-        if(canShip(mid)) {
-            ans = mid
-            end = mid -1
+    let ans = max_capacity;
+    let low = min_capacity, high = max_capacity;
+    
+    while(low < high) {
+        let mid = low + parseInt( (high - low) / 2 );
+        if(ispossible(mid)) {
+            ans = Math.min(ans, mid);
+            high = mid;
         } else {
-            start = mid + 1
+            low = mid + 1;
         }
     }
-    return ans
+    return ans;
 };
