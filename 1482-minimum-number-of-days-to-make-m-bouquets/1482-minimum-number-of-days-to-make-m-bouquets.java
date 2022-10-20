4c37359adccc91f01b63022fdea39e5cf1226464
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-20 15:53:32 +0530

    Time: 1770 ms (5.14%), Space: 122.3 MB (6.24%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,59 @@
+class Solution {
+   
+    
+    public boolean isPossible(int[] bloomDay, int k, int m, int days) {
+        final int n = bloomDay.length;
+        int c = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        for(int i = 0; i < n; i++) {
+            if(bloomDay[i] <= days) {
+                if(stack.isEmpty()) {
+                    stack.push(1);
+                } else {
+                    int top = stack.peek();
+                    if(top + 1 <= k) {
+                        stack.pop();
+                        stack.push(top+1);
+                    } else {
+                        stack.push(1);
+                    }
+                }
+            } else {
+                while(stack.isEmpty() == false && stack.peek() < k) {
+                    stack.pop();
+                }
+            }
+        }
+        
+        //System.out.println(days + " " + stack);
+        while(stack.isEmpty() == false) {
+            int top = stack.peek();
+            stack.pop();
+            if(top >= k) c++;
+        }
+        //System.out.println(c);
+        return c >= m ? true : false; 
+    }
+    public int minDays(int[] bloomDay, int m, int k) {
+        final int n = bloomDay.length;
+        int min = k, max = 0;
+        for(int bd : bloomDay) {
+            max = Math.max(max, bd);
+        }
+        
+        int low = min, high = max;
+        //System.out.println(min + " " + max);
+        int ans = -1;
+        while(low <= high) {
+            int mid = low + ((high - low) / 2);
+            //System.out.println(mid);
+            if(isPossible(bloomDay, k, m, mid)) {
+                ans = (ans == -1) ? mid : Math.min(ans, mid);
+                high = mid - 1;
+            } else {
+                low = mid  + 1;
+            }
+        }
+        return ans; 
+    }
+}
