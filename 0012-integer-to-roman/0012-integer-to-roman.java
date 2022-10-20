5803de4ef92621c6d264fd8242b1133db0822456
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-20 22:36:14 +0530

    Time: 31 ms (9.70%), Space: 46 MB (43.81%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,10 @@
+class Solution { 
+    private static final String[] thousands = {"", "M", "MM", "MMM"};
+    private static final String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}; 
+    private static final String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    private static final String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    
+    public String intToRoman(int num) { 
+        return thousands[num / 1000] + hundreds[num % 1000 / 100] + tens[num % 100 / 10] + ones[num % 10];
+    }
+}
