f2ef7b96a0ebdc1d0a4685b642e3de3885da68fa
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-21 13:00:36 +0530

    Time: 59 ms (32.96%), Space: 77.6 MB (74.69%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -2,17 +2,21 @@ class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         HashSet<Integer> hset = new HashSet<Integer>();
         final int n = nums.length;
-        for(int i = 0; i < n; i++) {
-            if(i - k - 1 >= 0) {
-                hset.remove(nums[i-k-1]);
-            }
+        //first window
+        for(int i = 0; i < Math.min(k + 1, n); i++) {
             if(hset.contains(nums[i])) {
                 return true;
-            } else {
-                hset.add(nums[i]);
             }
+            hset.add(nums[i]);
+        }
+        for(int i = k + 1; i < n; i++) {
+            int j = i - k - 1;
+            hset.remove(nums[j]);
+            if(hset.contains(nums[i])) {
+                return true;
+            }
+            hset.add(nums[i]);
         }
         return false;
     }
-    
 }
