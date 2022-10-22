0090a23441211c2c639cd00d97fa6f016a68e647
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-22 21:54:35 +0530

    Time: 565 ms (11.11%), Space: 62.7 MB (51.85%) - LeetHub

--- 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.js
+++ 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.js
@@ -5,18 +5,12 @@
  */
 var minProductSum = function(nums1, nums2) {
     
-    let min_product = 0
-    nums1.sort(function(a,b) { return b - a})
-    nums2.sort(function(a,b) { return a - b})
-    let n = nums1.length
-    let taken = new Array(n)
-    taken.fill(false)
-    let j = 0
+    nums1.sort((a,b) => { return a - b});
+    nums2.sort((a,b) => { return  b - a});
+    const n = nums1.length;
+    let ans = 0;
     for(let i = 0; i < n; i++) {
-        let a = nums1[i]
-        let b = nums2[j]
-        j++
-        min_product = min_product + (a*b)
+        ans  += (nums1[i] * nums2[i]);
     }
-    return min_product
+    return ans;
 };
