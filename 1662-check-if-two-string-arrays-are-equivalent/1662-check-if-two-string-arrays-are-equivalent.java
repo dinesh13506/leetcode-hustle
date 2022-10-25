71fd082bb3a8f3606fc6159f69a310e771ec4b27
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-25 10:10:07 +0530

    Time: 3 ms (21.99%), Space: 41.8 MB (70.29%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -3,14 +3,13 @@ class Solution {
         int p1 = 0, p2 = 0;
         int w1 = 0, w2 = 0;
         while(w1 < word1.length && w2 < word2.length) {
-            
-            
             if(word1[w1].charAt(p1) == word2[w2].charAt(p2)) {
                 p1++;
                 p2++;
             } else {
                 return false;
             }
+            
             if(p1 == word1[w1].length()) {
                 w1++;
                 p1 = 0;
@@ -19,6 +18,7 @@ class Solution {
                 w2++;
                 p2 = 0;
             }
+            
         }
         if(w1 == word1.length && w2 == word2.length) {
             return true;
