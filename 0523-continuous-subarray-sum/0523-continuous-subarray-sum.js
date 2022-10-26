c508ce4cd35b27067151ec6f46598d8e97d6c325
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-26 06:46:40 +0530

    Time: 273 ms (10.98%), Space: 85.1 MB (6.50%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkSubarraySum = function(nums, k) {
+    
+    const n = nums.length;
+    if(n <= 1) {
+        return false;
+    }
+    let prefixSum = new Array(n);
+    let map = new Map();
+    for(let i = 0;  i < n; i++) {
+        prefixSum[i] = (i > 0 ? prefixSum[i-1] : 0) + nums[i];
+        if(map.has(prefixSum[i]) == false) {
+            map.set(prefixSum[i], i);
+        } else {
+            let j = map.get(prefixSum[i]);
+            if(i - j > 1) {
+                return true;
+            }
+        }
+    }
+    //console.log(prefixSum)
+    for(let i = 0;  i < n; i++) {
+        //case 1 if sum at ith index is multiple of k
+        if(prefixSum[i] % k == 0 && i > 0) {
+            console.log("hey")
+            return true;
+        } else {
+            let value = prefixSum[i] - k;
+            while(value > 0) {
+               if(map.has(value)) {
+                    let j = map.get(value);
+                    if(i - j > 1) {
+                        return true
+                    }
+                }
+                value = value - k;
+            }
+            
+        }
+    }
+    return false;
+};
