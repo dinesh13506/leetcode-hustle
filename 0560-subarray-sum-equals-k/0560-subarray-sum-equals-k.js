afacf21bfe114c6b584680d37bfb8cc788825572
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-26 07:54:25 +0530

    Time: 126 ms (77.79%), Space: 50.4 MB (68.54%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -8,10 +8,6 @@ var subarraySum = function(nums, k) {
     let count = 0;
     let map = new Map();
     const n = nums.length;
-    let ps = new Array(n);
-    for(let i = 0; i < n; i++) {
-        ps[i] = (i > 0 ? ps[i-1] : 0) + nums[i];
-    }
     let sum = 0;
     map.set(0,1);
     for(let i = 0; i < n; i++) {
