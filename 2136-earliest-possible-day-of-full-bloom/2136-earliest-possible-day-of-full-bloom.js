9a28d5b6926846c33b51338027051c82bc85ddf5
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-10-29 09:23:34 +0530

    Time: 623 ms (16.67%), Space: 93.9 MB (58.33%) - LeetHub

---
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} plantTime
+ * @param {number[]} growTime
+ * @return {number}
+ */
+var earliestFullBloom = function(plantTime, growTime) {
+    const n = plantTime.length;
+    let input = [];
+    for(let i = 0; i < n; i++) {
+        input.push([
+            plantTime[i], growTime[i], growTime[i] / plantTime[i]
+        ]);
+    }
+    //console.log(input);
+    input.sort((a,b) => {
+            if(a[1] == b[1]) {
+                return a[0] - b[0];
+            } else if(a[0] == b[0]) {
+                return b[1] - a[1];
+            } else {
+                return b[1] - a[1];
+            }
+    })
+    //console.log(input);
+    let pt = 0;
+    let ans = 0;
+    for(let i = 0; i < n; i++) {
+        pt += input[i][0];
+        ans = Math.max(ans, pt + input[i][1]);
+    }
+    return ans;
+};
