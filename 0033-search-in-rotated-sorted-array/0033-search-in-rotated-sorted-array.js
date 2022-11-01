fbb28c92219355fe18f8c36058044ea6b9155f20
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-11-01 23:06:39 +0530

    Time: 97 ms (54.58%), Space: 42.4 MB (22.73%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,55 +4,28 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    
     const n = nums.length;
-    let start = 0;
-    let end = n -1;
-    let smallest_el_index = 0;
-    let bs = (start, end) => {
-        while(start <= end) {
-            let mid = start + ( end - start) / 2;
-            mid = parseInt(mid);
-            if(nums[mid] == target) {
-                return mid;
-            } else if(nums[mid] < target) {
+    let start = 0, end = n - 1;
+    while(start <= end) {
+        let mid = start + (end - start)/2;
+        mid = parseInt(mid);
+        if(nums[mid] == target) {
+            return mid;
+        }
+        if(nums[start] <= nums[mid]) {
+            if(nums[start] <= target  && target < nums[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        else {
+            if(target > nums[mid] && target <= nums[end]) {
                 start = mid + 1;
             } else {
                 end = mid - 1;
             }
         }
-        return -1;
     }
-    if(nums[0] < nums[n-1]) {
-        //console.log("hey")
-        return bs(0, n-1);
-    }
-    while(start < end) {
-        let mid = start + ( end - start) / 2;
-        mid = parseInt(mid);
-        if(nums[end] < nums[mid]) {
-            if(nums[end] < nums[smallest_el_index]) {
-                smallest_el_index = end;
-            }
-            start = mid + 1;
-        } else {
-            if(nums[mid] < nums[smallest_el_index]) {
-                smallest_el_index = mid;
-            }
-            end = mid;
-        }
-    }
-    //console.log(smallest_el_index)
-    
-    
-    
-    if(nums[smallest_el_index] == target) {
-        return smallest_el_index;
-    }
-    let res1 = bs(0, smallest_el_index - 1);
-    if(res1 == -1) {
-        return bs(smallest_el_index, n - 1);
-    } else {
-        return res1;
-    }
+    return -1;
 };
