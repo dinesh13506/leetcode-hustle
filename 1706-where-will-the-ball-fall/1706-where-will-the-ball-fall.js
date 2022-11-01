552e553387a1e33067624efb3f21534834ca738a
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-11-01 11:03:40 +0530

    Time: 109 ms (64.90%), Space: 44.7 MB (47.76%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
@@ -4,25 +4,55 @@
  */
 var findBall = function(grid) {
     
-    let m = grid.length, n = grid[0].length
-    let result = new Array(n)
-    result.fill(-1)
-    for(let col = 0; col < n; col++) {
-        let curr_row = 0, curr_col = col
-        while(curr_row < m) {
-            let curr_value = grid[curr_row][curr_col]
-            if(curr_value == 1 && curr_col + 1 < n && grid[curr_row][curr_col+1] == 1) {
-                curr_col++
-                curr_row++
-            }
-            else if(curr_value == -1 && curr_col - 1 >=0 && grid[curr_row][curr_col-1] == -1) {
-                curr_col--
-                curr_row++
-            } else {
-                break
-            }
-        }
-        result[col] = curr_row == m ? curr_col : -1
+    const m = grid.length;
+    const n = grid[0].length;
+    let memo = new Array(n);
+    for(let i = 0; i < m; i++) {
+        memo[i]  = new Array(n);
+        memo[i].fill(null);
     }
-    return result
+    let f = (i, j) => {
+        //console.log(i,j);
+        // if(i < 0 || i >= m || j < 0 || j >= n) {
+        //     return -1;
+        // }
+        if(i >= m) {
+            return j;
+        }
+        if(memo[i][j] != null) return memo[i][j];
+        let value = grid[i][j];
+        if(value == -1 && j - 1 < 0) {
+            memo[i][j] = -1;
+            return memo[i][j];
+        }
+        if(value == 1 && j + 1 >= n) {
+            memo[i][j] = -1;
+            return memo[i][j];
+        }
+        if(value == -1 && grid[i][j-1] == 1) {
+            memo[i][j] = -1;
+            return memo[i][j];
+        }
+        if(value == 1 && grid[i][j+1] == -1) {
+            memo[i][j] = -1;
+            return memo[i][j];
+        }
+        
+        if(value == -1 && grid[i][j-1] == -1) {
+            memo[i][j] = f(i + 1, j - 1);
+            return memo[i][j];
+        }
+        if(value == 1 && grid[i][j+1] == 1) {
+            memo[i][j] = f(i + 1, j + 1);
+            return memo[i][j];
+        }
+    }
+    
+    let ans = [];
+    for(let j = 0 ;j <n; j++) {
+        ans.push(f(0, j));
+    }
+    //console.log(f(0,0));
+    return ans;
+    
 };
