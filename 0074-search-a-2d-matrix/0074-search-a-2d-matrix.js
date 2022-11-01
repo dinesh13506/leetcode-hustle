339836ee26b03285245eb678f5416a4601434bd9
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-11-01 17:33:33 +0530

    Time: 114 ms (15.36%), Space: 41.9 MB (78.18%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -4,24 +4,20 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
+    if(matrix.length <= 0) {
+        return false;
+    }
     const m = matrix.length;
     const n = matrix[0].length;
-    
-    let p1 = 0;
-    let p2 = (m * n) - 1;
-    
-    while(p1 <= p2) {
-        let mid = parseInt( (p1 + p2) / 2);
-        let rowNo = parseInt(mid / n);
-        let colNo = parseInt(mid % n);
-        if(matrix[rowNo][colNo] == target) {
+    let r = 0, c = n - 1;
+    while(r < m && c >=0) {
+        let mid = matrix[r][c];
+        if(target == mid) {
             return true;
-        }
-        else if(target < matrix[rowNo][colNo]) {
-            p2 = mid - 1;
-            
-        } else if(target > matrix[rowNo][colNo]) {
-            p1 = mid + 1;
+        } else if(target < mid) {
+            c--;
+        } else {
+            r++;
         }
     }
     return false;
