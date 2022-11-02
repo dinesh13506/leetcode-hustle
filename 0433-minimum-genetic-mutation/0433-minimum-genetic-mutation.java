060c28e49eb7f475fef51bfcdce5c7937d2ea2f4
dinesh13506 <32204239+dinesh13506@users.noreply.github.com>
2022-11-02 10:47:48 +0530

    Time: 18 ms (5.70%), Space: 43.1 MB (6.42%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -1,34 +1,32 @@
 class Solution {
     public int minMutation(String start, String end, String[] bank) {
-        Queue<String> queue = new LinkedList<>();
-        Set<String> seen = new HashSet<>();
-        queue.add(start);
-        seen.add(start);
-        
         int steps = 0;
+        Queue<String> queue = new LinkedList<String>();
+        Set<String> set = new HashSet<String>();
         
-        while (!queue.isEmpty()) {
-            int nodesInQueue = queue.size();
-            for (int j = 0; j < nodesInQueue; j++) {
-                String node = queue.remove();
-                if (node.equals(end)) {
+        queue.add(start);
+        set.add(start);
+        
+        while(queue.size() > 0) {
+            int size = queue.size();
+            
+            for(int j = 0; j < size; j++) {
+                String curr = queue.remove();
+                if(curr.equals(end)) {
                     return steps;
                 }
-
-                for (char c: new char[] {'A', 'C', 'G', 'T'}) {
-                    for (int i = 0; i < node.length(); i++) {
-                        String neighbor = node.substring(0, i) + c + node.substring(i + 1);
-                        if (!seen.contains(neighbor) && Arrays.asList(bank).contains(neighbor)) {
-                            queue.add(neighbor);
-                            seen.add(neighbor);
+                for(char ch : new char[]{'A','C','G','T'}) {
+                    for(int i = 0; i < curr.length(); i++) {
+                        String newStr = curr.substring(0, i) + ch + curr.substring(i+1);
+                        if(!set.contains(newStr) && Arrays.asList(bank).contains(newStr)) {
+                            queue.add(newStr);
+                            set.add(newStr);
                         }
                     }
                 }
             }
-            
             steps++;
         }
-        
         return -1;
     }
 }
