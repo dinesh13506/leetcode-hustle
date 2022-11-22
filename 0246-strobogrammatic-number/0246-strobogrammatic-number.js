f8833ef1f9028588e9b92bdc87de1259f2cf77e7
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2022-11-22 22:38:10 +0530

    Time: 117 ms (5.98%), Space: 41.8 MB (56.41%) - LeetHub

---
+++ 0246-strobogrammatic-number/0246-strobogrammatic-number.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} num
+ * @return {boolean}
+ */
+var isStrobogrammatic = function(num) {
+    //0
+    //1
+    //6
+    //8
+    //9
+    let map = new Map();
+    map.set("0","0");
+    map.set("1","1");
+    map.set("6","9");
+    map.set("8","8");
+    map.set("9","6");
+    let input = "" + num;
+    let output = [];
+    for(let ch of input) {
+        if(map.has(ch)) {
+            output.push(map.get(ch))
+        } else {
+            output.push("*");
+        }
+    }
+    //console.log(output);
+    return output.reverse().join("") == input;
+    
+};
