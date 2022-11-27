04d883f8f73b21aed36297e19a9ba9e29c4384e8
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2022-11-27 17:15:39 +0530

    Time: 3 ms (65.29%), Space: 40.4 MB (91.86%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,26 +1,25 @@
-import java.io.*;
-import java.util.*;
 class Solution {
     public String customSortString(String order, String s) {
         int[] indexMap = new int[26];
         Arrays.fill(indexMap, Integer.MAX_VALUE);
+        int p1 = 0;
         for(int i = 0; i < order.length(); i++) {
             char ch = order.charAt(i);
-            indexMap[ch - 'a'] = i;
+            int idx = ch - 'a';
+            indexMap[idx] = p1;
+            p1++;
         }
-        
-        ArrayList<Character> list = new ArrayList<Character>();
+        ArrayList<Character> al = new ArrayList<Character>();
         for(int i = 0; i < s.length(); i++) {
-            list.add(s.charAt(i));
+            al.add(s.charAt(i));
         }
-        Collections.sort(list, (a,b) -> {
-            return indexMap[a - 'a'] - indexMap[b- 'a'];
+        Collections.sort(al, (a,b) -> {
+            return indexMap[a - 'a'] - indexMap[b-'a'];
         });
-        
-        StringBuffer bf = new StringBuffer();
-        for(int i = 0; i < list.size(); i++) {
-            bf.append(list.get(i));
+        StringBuilder sb = new StringBuilder("");
+        for(char ch : al) {
+            sb.append(ch);
         }
-        return bf.toString();
+        return sb.toString();
     }
 }
