3ac3c9469c8d5e037954c7faeb0e4dd1c60989f2
Dinesh <32204239+dinesh13506@users.noreply.github.com>
2022-11-29 22:17:11 +0530

    Time: 178 ms (59.57%), Space: 49.3 MB (57.45%) - LeetHub

---
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {boolean}
+ */
+var isValidPalindrome = function(s, k) {
+    
+    
+    let memo = new Array(s.length);
+    for(let i = 0; i < s.length; i++) {
+        memo[i] = new Array(s.length);
+        memo[i].fill(Infinity);
+    }
+    let dp = (i, j) => {
+        if(i == j) return 0;
+        if(i == j - 1) {
+            return s[i] == s[j] ? 0 : 1;
+        }
+        
+        if(memo[i][j] != Infinity) return memo[i][j];
+        if(s[i] == s[j]) {
+            memo[i][j] =  dp(i + 1, j - 1);
+        } else {
+             memo[i][j] =  1 + Math.min(dp(i+1, j), dp(i, j - 1));
+        }
+        return memo[i][j];
+    }
+    
+    return dp(0,s.length - 1) <= k;
+};
